e22b3a690e18c5670985a1ff38a1f4714b233258
Robert Medeiros <robert@crimeminister.org>
2021-12-23 14:00:01 -0500

    chore(www): add development web site

---
+++ www/src/kubelt/routes.cljs
@@ -0,0 +1,43 @@
+(ns kubelt.routes
+  (:require
+   [bidi.bidi :as bidi]
+   [pushy.core :as pushy]
+   [re-frame.core :as re-frame]
+   [kubelt.events :as events]))
+
+(defmulti panels identity)
+(defmethod panels :default [] [:div "No panel found for this route."])
+
+(def routes
+  (atom
+    ["/" {""      :home
+          "about" :about}]))
+
+(defn parse
+  [url]
+  (bidi/match-route @routes url))
+
+(defn url-for
+  [& args]
+  (apply bidi/path-for (into [@routes] args)))
+
+(defn dispatch
+  [route]
+  (let [panel (keyword (str (name (:handler route)) "-panel"))]
+    (re-frame/dispatch [::events/set-active-panel panel])))
+
+(defonce history
+  (pushy/pushy dispatch parse))
+
+(defn navigate!
+  [handler]
+  (pushy/set-token! history (url-for handler)))
+
+(defn start!
+  []
+  (pushy/start! history))
+
+(re-frame/reg-fx
+  :navigate
+  (fn [handler]
+    (navigate! handler)))
