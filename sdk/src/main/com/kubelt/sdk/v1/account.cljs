b82340445aab21b60bd85ac5858227f1cf3d9d26
Robert Medeiros <robert@crimeminister.org>
2022-01-12 13:53:08 -0500

    wip

--- sdk/src/main/com/kubelt/sdk/v1/account.cljs
+++ sdk/src/main/com/kubelt/sdk/v1/account.cljs
@@ -15,11 +15,22 @@
   ;;   account doesn't already exist
   ;;   - if exists, return existing account and/or error
   ;;   - if not exists:
-  ;;     - request naming account creation using naming client
+
+  ;;     - request naming account creation using naming client can we
+  ;;       - store the user's public key as a record? then all
+  ;;         subsequent mutations or reads could be signed and
+  ;;         validated. also look at JWT
+
   ;;     - create initial MeDAG in local quad store
   ;;     - store MeDAG to IPFS to get CID
   ;;     - store CID for public key in naming system
   ;;     - return ??
+
+  ;; a flow for validating keypair ownership:
+  ;; - client -> gateway: send nonce for public key <key>
+  ;; - gateway -> client: nonce
+  ;; - client -> gateway: private.sign(nonce) public key CID
+  ;; - gateway.publish(CID, hash(publickey))
   )
 
 ;; TODO test me
