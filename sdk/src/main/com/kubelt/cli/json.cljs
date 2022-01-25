bcc57bf494a23fb8d548ed89e259c2fe38c3a156
gesamtkunstwerker <alex.flanagan@gmail.com>
2022-01-25 14:58:55 -0500

    feat(cli): JSON command rename. Close #62. (#63)
    * feat(cli): JSON command rename. Close #62.
    
    * doc: update docstring
    
    Co-authored-by: Robert Medeiros <robert@crimeminister.org>

---
+++ sdk/src/main/com/kubelt/cli/json.cljs
@@ -0,0 +1,14 @@
+(ns com.kubelt.cli.json
+  "CLI setup for 'json' sub-command."
+  {:copyright "©2022 Kubelt, Inc." :license "UNLICENSED"}
+  (:require
+   [com.kubelt.cli.json.convert :as json.convert]))
+
+
+(defonce command
+  {:command "json <command>"
+   :desc "Invoke JSON methods"
+   :builder (fn [^js yargs]
+              (-> yargs
+                  (.command (clj->js json.convert/command))
+                  (.demandCommand)))})
