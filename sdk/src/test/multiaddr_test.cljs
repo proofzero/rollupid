6dd94a894516ba7c3df7dd4d254d37ac8988057f
Robert Medeiros <robert@crimeminister.org>
2022-01-26 10:30:27 -0500

    feat(sdk): set up GitHub package publishing (#64)
    * feat(sdk): set up GitHub package publishing
    
    * feat(cli): JSON command rename. Close #62. (#63)
    
    * feat(cli): JSON command rename. Close #62.
    
    * doc: update docstring
    
    Co-authored-by: Robert Medeiros <robert@crimeminister.org>
    
    Co-authored-by: gesamtkunstwerker <alex.flanagan@gmail.com>

--- sdk/src/test/multiaddr_test.cljs
+++ sdk/src/test/multiaddr_test.cljs
@@ -8,11 +8,24 @@
   (:require
    [com.kubelt.sdk.impl.multiaddr :as multiaddr]))
 
+(deftest vec->multiaddr-str-test
+  (testing "convert valid vector"
+    (let [host "127.0.0.1"
+          port 8080
+          v [:ip4 host :tcp port]
+          s (multiaddr/vec->str v)]
+      (is (string? s)
+          "output is a string")
+      (is (str/includes? s host)
+          "host IP address is included")
+      (is (str/includes? s port)
+          "host port is included"))))
+
 (deftest str->map-test
   (testing "multiaddr string conversion"
     (let [host "127.0.0.1"
           port 8080
-          maddr-str (str "/ip4/" host "/tcp/" port)
+          maddr-str (multiaddr/vec->str [:ip4 host :tcp port])
           maddr-map (multiaddr/str->map maddr-str)]
       (is (map? maddr-map)
           "conversion result must be a map")
@@ -25,4 +38,13 @@
 
       ;; TODO more tests
       (is (= host (get maddr-map :address/host)))
-      (is (= port (get maddr-map :address/port))))))
+      (is (= port (get maddr-map :address/port)))))
+
+  #_(testing "invalid host address"
+    (let [host "localhost"
+          port 8080
+          maddr-str (multiaddr/vec->str [:ip4 host :tcp port])
+          maddr-map (multiaddr/str->map maddr-str)]
+      ;; This is an error; how to handle? Fail spec validation? Return
+      ;; error map?
+      )))
