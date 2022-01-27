39ad03de8de1284453da15e0c68494536ca1b40c
billk <bill@binaryaces.ca>
2022-01-27 16:03:15 -0500

    stubbing in interceptors for p2p endpoints

--- src/main/com/kubelt/p2p/interceptor.cljs
+++ src/main/com/kubelt/p2p/interceptor.cljs
@@ -21,3 +21,41 @@
   {:name ::status-ok
    :leave (fn [ctx]
             (assoc-in ctx [:response :http/status] 200))})
+
+(def get-value-for-kbtname
+  {:name ::get-value-for-kbtname
+   :enter (fn [ctx]
+            (log/info {:log/msg "enter get-value-for-kbtname"})
+            ctx)
+ 
+   ;; TODO lookup value in hyperbee for kbtname in request
+   ;; TODO return value
+   
+   :leave (fn [ctx]
+            (log/info {:log/msg "leaving get-value-for-kbtname"})
+            ctx)
+   :error (fn [{:keys [error] :as ctx}]
+            (log/error {:log/error error})
+            ctx)})
+
+
+(def update-kbt-value
+  {:name ::update-kbt-value
+   :enter (fn [ctx]
+            (log/info {:log/msg "enter update-kbt-value"})
+            ctx)
+ 
+   ;; TODO verify JWT 
+   ;; TODO extract payload from JWT
+   ;; TODO update value in hyperbee for kbtname
+   ;; TODO return response
+   
+   :leave (fn [ctx]
+            (log/info {:log/msg "leaving update-kbt-value"})
+            ctx)
+   :error (fn [{:keys [error] :as ctx}]
+            (log/error {:log/error error})
+            ctx)})
+
+
+
