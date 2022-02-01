7bc8cc892a6e3ac63a8c6db81ccc0d3d989a58ce
Adrian Maurer <adrianmaurer+github@fastmail.com>
2022-02-01 13:16:02 -0500

    wip

---
+++ packages/sdk-module/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+module.exports = {
+  entry: "../sdk-js/lib/sdk.js",
+  mode: "development",
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "lib"),
+    filename: "index.js",
+  },
+  plugins: [new NodePolyfillPlugin()],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
