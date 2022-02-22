a933d6aad56224d98381c022c93fd73491616a94
billk <bill@kubelt.com>
2022-02-22 09:51:35 -0500

    removing old p2p project

--- p2p/src/api/services/__tests__/kbtname.service.test.ts
+++
@@ -1,9 +0,0 @@
-//TODO: test kbt service here 
-//
-
-describe("kbt service test", () => {
-	it("should always pass", () => {
-
-		expect(true).toEqual(true);
-	});
-});
--- p2p/test/generate_test_keys.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-openssl genrsa  -out private.key 2048
-openssl rsa -in private.key -outform PEM -pubout -out public.key
-
-
-openssl genrsa  -out private_bad.key 2048
-openssl rsa -in private_bad.key -outform PEM -pubout -out public_bad.key
