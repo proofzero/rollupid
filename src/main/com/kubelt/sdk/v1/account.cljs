cea2cb89e3b7529358303bdab396983f04b81e21
Adrian Maurer <adrianmaurer+github@fastmail.com>
2022-03-07 19:39:18 -0500

    wip: address pr comments

--- src/main/com/kubelt/sdk/v1/account.cljs
+++ src/main/com/kubelt/sdk/v1/account.cljs
@@ -17,41 +17,31 @@
 ;; TODO test me
 (defn authenticate!
   "Create an account or authorize an existing account. User identity is
-  centered around an ETH account and is also the name linked to the user's
-  Kubelt application data (aka the 'me dag'). 
+  centered around an wallet accounts and is also the name linked to the
+  user's Kubelt application data (aka the 'me dag'). 
   
-  A public key is requested from the wallet and must be provided as a
-  hex-encoded string along with the account id. These two pieces of information 
-  together kick off a zero-knowledge authentication request."
+  The public key from the wallet param must be available as a
+  hex-encoded string along with the account id. These two pieces of 
+  information together kick off a zero-knowledge authentication request.
+  
+  The decryption function stored in the wallet will be used to decrypt
+  a nonce to complete the proof"
   [sys wallet]
   ;; TODO validate system map
   ;; TODO validate public-key
   ;; TODO validate wallet map
-  (let [{:keys [wallet/sign-fn]} wallet
-        message-to-sign "Foo"
-        signed-message (sign-fn message-to-sign)]
-    ;; Wallet is a collection of crypto fns that hide the private key
-    ;; from SDK by closing over it, while enabling crypto operations
-    ;; within the SDK.
+  (let [{:keys [wallet/sign-fn]} wallet]
+        
+    ;; Send the public key and the account id to kubelt api "/auth"
+    ;; This kicks off a zero-knowledge proof authentication
 
-    ;; a flow for validating keypair ownership:
-    ;; - sdk -> p2p: send nonce for public key <key>
-    ;; - p2p -> sdk: <encrypted nonce>
-    ;; - sdk -> p2p: <decrypted, signed nonce>
+    ;; kubelt api returns an encrypted nonce created using the public key
+    ;; decrypt this nonce and send the result back to the api
 
-    ;; - p2p: lookup(pub-key) => cid / nil
-    ;;   - if cid, user's me-dag already exists; hurray
-    ;;   - if nil, we have a new user; create p2p context
-    ;;     - store public key?
-    ;;     - store registration timestamp?
-    ;;     - set up billing context?
-    ;; - p2p -> sdk: JWT session (include me-dag root CID in body)
-    ;; - sdk: if nil, create and store me-dag:
-    ;;   - populate triple store with initial user account info
-    ;;   - sdk -> ipfs: store(me-dag) => cid
-    ;; - sdk -> p2p: update(jwt, me-dag cid)
-    ;;   => refreshed JWT session token
-    ;; <= return updated system map
+    ;; receive a JWT and store in system map
+    ;; the JWT has an expiry and encodes client IP
+    ;; to restrict renewing JWTs for other clients
+
     sys))
 
 ;; TODO test me
