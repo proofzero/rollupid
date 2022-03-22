6e798d1a87198cfef85f6b5c9af0879127a04f38
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-03-22 17:54:46 +0200

    Fixed hook

--- packages/sanity-plugin-kubelt/src/components/KubeltAuth.tsx
+++ packages/sanity-plugin-kubelt/src/components/KubeltAuth.tsx
@@ -1,8 +1,8 @@
 import {Badge, Box, Card, Flex, Inline} from '@sanity/ui'
 import React from 'react'
 
-import {useAccount} from '../hooks'
 import {requestWalletAuth, requestKubeltAuth} from '../domain/kubeltWallet'
+import useAccount from '../hooks/useAccount'
 
 function KubeltAuth() {
   const account = useAccount()
