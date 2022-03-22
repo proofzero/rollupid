13a25474030dd5ac51583c03f8a50e692959ab61
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-03-22 17:54:21 +0200

    Updated webpack config

--- packages/sdk-web/webpack.config.js
+++ packages/sdk-web/webpack.config.js
@@ -1,31 +1,45 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
-const paths = {
-  // Source files
-  src: path.resolve(__dirname, "../sdk-js/lib/sdk.js"),
+const isProduction = process.env.NODE_ENV == 'production';
 
-  // Production build files
-  build: path.resolve(__dirname, "./lib"),
+
+const config = {
+    entry: path.resolve(__dirname, "../sdk-js/lib/sdk.js"),
+    output: {
+        path: path.resolve(__dirname, 'lib'),
+    },
+    resolve: {
+        fallback: {
+            "fs": false
+        },
+    },
+    plugins: [
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+        new NodePolyfillPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
 };
 
-// TODO: make a production and dev build target
-module.exports = {
-  entry: paths.src,
-  mode: "development",
-  output: {
-    path: paths.build,
-    filename: "index.js",
-    library: "sdk",
-    libraryTarget: "commonjs2",
-  },
-  plugins: [
-    new NodePolyfillPlugin(),
-    new webpack.ProvidePlugin({
-      Buffer: ["buffer", "Buffer"],
-      process: "process/browser",
-    }),
-  ],
-  devtool: "inline-source-map",
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+        config.devtool = "inline-source-map";
+    }
+    return config;
 };
