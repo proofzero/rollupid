c5ee57d2493183d757dbf0bef324e98127f5535f
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-03-24 16:05:59 +0200

    Updated sdk-web export module type to AMD (browser)

--- packages/sdk-web/webpack.config.js
+++ packages/sdk-web/webpack.config.js
@@ -3,11 +3,13 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == 'production';
 
-
 const config = {
     entry: path.resolve(__dirname, "../sdk-js/lib/sdk.js"),
     output: {
-        path: path.resolve(__dirname, 'lib'),
+        path: path.resolve(__dirname, "./lib/"),
+        filename: "index.js",
+        library: "sdk",
+        libraryTarget: "amd",
     },
     resolve: {
         fallback: {
@@ -19,27 +21,16 @@ const config = {
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
         new NodePolyfillPlugin()
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
 };
 
 module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
-        
-        
+
+
     } else {
         config.mode = 'development';
-        config.devtool = "inline-source-map";
+        config.devtool = 'inline-source-map'
     }
     return config;
 };
