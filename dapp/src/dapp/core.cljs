a8e6f45a09a5faadcd84d411839ff956cf84dd17
billk <bill@kubelt.com>
2022-03-30 09:43:27 -0400

    browser http client wip using goog xhrio

--- dapp/src/dapp/core.cljs
+++ dapp/src/dapp/core.cljs
@@ -1,12 +1,12 @@
 (ns dapp.core
   (:require
-   [ajax.core :as ajax] 
-   [reagent.dom :as rdom]
-   [re-frame.core :as re-frame]
-   [day8.re-frame.http-fx]
-   [dapp.config :as config]
-   [dapp.views :as views]
-   [dapp.routes :as routes]))
+    [com.kubelt.sdk.v1 :as sdk.v1]
+    [com.kubelt.sdk.v1.core :as sdk.core]
+    [reagent.dom :as rdom]
+    [re-frame.core :as re-frame]
+    [dapp.config :as config]
+    [dapp.views :as views]
+    [dapp.routes :as routes]))
 
 
 (defn dev-setup []
@@ -27,40 +27,16 @@
 
 ;;; Events ;;;
 
-
-(re-frame/reg-event-fx                             ;; note the trailing -fx
-              :handler-with-http                      ;; usage:  (dispatch [:handler-with-http])
-              (fn [{:keys [db]} _]                    ;; the first param will be "world"
-                  {:db   (assoc db :show-twirly true)   ;; causes the twirly-waiting-dialog to show??
-                     :http-xhrio {:method          :get
-                                  :uri             "https://api.github.com/orgs/day8"
-                                  :timeout         8000                                           ;; optional see API docs
-                                  :response-format (ajax/json-response-format {:keywords? true})  ;; IMPORTANT!: You must provide this.
-                                  :on-success      [::success-http-result]
-                                  :on-failure      [:bad-http-result]}}))
-
-(re-frame/reg-event-db
-    ::success-http-result
-      (fn [db [_ result]]
-        (prn {:hereiam "success" :result result})
-        (assoc db :success-http-result result)))
-
-
-       ;; I.e (re-frame/dispatch [::events/load-something-with-ajax])
-
 (re-frame/reg-event-db ::initialize-db
-  (fn [db _]
-    (re-frame/dispatch [:handler-with-http])
-    (if db
-      db
-      kubelt-db)))
-
+                       (fn [db _]
+                         (sdk.v1/init)))
 
 (re-frame/reg-sub ::current-user
-  (fn [db]
-    (:current-user db)))
+                  (fn [db]
+                    (:current-user db)))
 
 (defn init []
+  (prn "hereiam starting up")
   (re-frame/clear-subscription-cache!)
   (re-frame/dispatch-sync [::initialize-db])
   (dev-setup)
