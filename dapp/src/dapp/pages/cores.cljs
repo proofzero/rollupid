e4ae416d9f68f2e961b621e143f8a2b982364f29
Dhruvil Patel <dhruvil.h.patel@gmail.com>
2022-04-05 14:04:02 -0600

    feat(dapp): Update cores and reports pages with header component

--- dapp/src/dapp/pages/cores.cljs
+++ dapp/src/dapp/pages/cores.cljs
@@ -1,7 +1,19 @@
-(ns dapp.pages.cores)
+(ns dapp.pages.cores
+  (:require
+   [dapp.components.header :as header]))
+
+(defn cores-content
+  []
+  [:div.cores-content
+   {:class "bg-gray-200 h-full flex flex-col"}
+   [:h1
+    {:class "mt-6 ml-6 text-xl w-auto"}
+    "Cores"]])
 
 (defn render
   []
-  [:div.cores-container
-   {:class "w-4/5 h-screen bg-gray-200"}
-   [:h1 "Cores"]])
+  (fn []
+    [:div.cores-container
+     {:class "w-4/5 flex flex-col"}
+     [header/render]
+     [cores-content]]))
