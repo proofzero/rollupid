f7f02766294f60488758129d8b33f6c11033c8bf
Drew Patel <dhruvil.h.patel@gmail.com>
2022-04-12 14:14:42 -0600

    feat(dapp): Update web3modal style to match mocks + refactor into own component ns (#305)
    * feat(dapp): Move web3-modal to its own component namespace
    
    * feat(dapp): Parameterize web3modal config
    
    To be used later for testing or connecting to another
    network.
    
    * feat(dapp): Remove unused re-frame events from wallet ns
    
    * feat(dapp): Update authenticate! promise to resolve entire chain

--- dapp/src/dapp/pages/dashboard.cljs
+++ dapp/src/dapp/pages/dashboard.cljs
@@ -1,54 +1,8 @@
 (ns dapp.pages.dashboard
   (:require
-   [com.kubelt.lib.promise :refer [promise]]
-   [com.kubelt.lib.crypto.hexify :as hexify]
    [dapp.components.button :as button]
    [dapp.components.header :as header]
-   [dapp.wallet :as wallet]
-   [ethers :as ethers]
-   [re-frame.core :as re-frame]
-   [taoensso.timbre :as log])
-  (:require
-    ["web3modal$default" :as Web3Modal]
-    ["@coinbase/wallet-sdk" :as CoinbaseWalletSDK]))
-
-(def provider-options
-  {:network "mainnet"
-   :cacheProvider true
-   :theme "dark"
-   :providerOptions {:walletlink {:package CoinbaseWalletSDK :options {:appName "Kubelt"}}}})
-
-(defn make-sign-fn
-  [provider wallet-address]
-  (fn [signable]
-    (promise
-     (fn [resolve _reject]
-       (let [signable-buffer (hexify/hex-string signable)]
-         (-> (.request provider (clj->js {:method "personal_sign"
-                                          :params [signable-buffer wallet-address]}))
-             (.then (fn [digest]
-                      (resolve digest)))))))))
-
-(defn open-modal []
-  (log/trace "open the modal")
-  (let [modal (Web3Modal. (clj->js provider-options))]
-    ;(.clearCachedProvider modal)
-    (-> (.connect modal)
-        ;; TODO: figure out why this won't re-prompt wallet if password was not entered at prompt
-        (.then (fn [provider]
-                 ;; dispatch the provider
-                 (-> (.request provider (clj->js {:method "eth_requestAccounts"}))
-                     (.then (fn [account]
-                              (let [raw-address (first (js->clj account))
-                                    wallet-address (ethers/utils.getAddress raw-address)
-                                    sign-fn (make-sign-fn provider wallet-address)
-                                    new-wallet {:com.kubelt/type :kubelt.type/wallet
-                                                :wallet/address wallet-address
-                                                :wallet/sign-fn sign-fn}]
-                                (re-frame/dispatch [::wallet/set-current-wallet new-wallet])))))))
-        (.catch (fn [error]
-                  (.clearCachedProvider modal)
-                  (log/error {:msg "web3 modal error" :error error}))))))
+   [dapp.components.web3-modal :as web3-modal]))
 
 (defn connect-wallet
   []
@@ -69,7 +23,7 @@
                      :text "Connect a Wallet"
                      :on-click (fn [e]
                                  (.preventDefault e)
-                                 (open-modal))
+                                 (web3-modal/open-modal))
                      :variant :primary}]]))
 
 (defn dashboard-content
