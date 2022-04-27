64dfd43b58419b013c237849c08b4ce1a1419b7b
Dhruvil Patel <dhruvil.h.patel@gmail.com>
2022-04-27 07:43:16 -0600

    chore(dapp): Remove unused namespaces and references

--- dapp/src/dapp/utils.cljs
+++
@@ -1,9 +0,0 @@
-(ns dapp.utils
-  (:require 
-    [taoensso.timbre :as log]))
-
-(defn classnames 
-  "Selects and creates a set of html classes"
-  [& classes]
-  (clojure.string/join " " (remove nil? classes)))
-
