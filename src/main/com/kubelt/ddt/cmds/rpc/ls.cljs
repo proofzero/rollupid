efaaad15697bf4cf86194fd0b3bf604f375a1252
tangrammer <juanantonioruz@gmail.com>
2022-05-20 12:44:54 +0200

    Adapt ddt rpc ls

--- src/main/com/kubelt/ddt/cmds/rpc/ls.cljs
+++ src/main/com/kubelt/ddt/cmds/rpc/ls.cljs
@@ -2,12 +2,15 @@
   "List available RPC methods."
   {:copyright "ⓒ2022 Proof Zero Inc." :license "Apache 2.0"}
   (:require
+   [clojure.string :as cstr]
    [com.kubelt.ddt.auth :as ddt.auth]
    [com.kubelt.ddt.options :as ddt.options]
    [com.kubelt.ddt.prompt :as ddt.prompt]
    [com.kubelt.ddt.util :as ddt.util]
+   [com.kubelt.lib.http.node :as http.node]
    [com.kubelt.lib.promise :as lib.promise]
    [com.kubelt.rpc :as rpc]
+   [com.kubelt.rpc.schema :as rpc.schema]
    [com.kubelt.sdk.v1.core :as sdk.core]))
 
 (defonce command
@@ -30,12 +33,15 @@
                   (.-password result)
                   (fn [sys]
                     (-> (sdk.core/rpc-api sys (-> sys :crypto/wallet :wallet/address))
-                        (lib.promise/then
-                         (fn [api]
-                           (println (->> (update-in api [:methods 1 :result] assoc :name "pong" :schema {:type "string"})
-                                         (rpc/init)
-                                         (rpc/available)))))
+                        (lib.promise/then (fn [api]
+                                            (let [client (-> {:uri/domain (-> sys :client/p2p :http/host)
+                                                              :uri/port (-> sys :client/p2p :http/port)
+                                                              :uri/path (cstr/join "" ["/@" (-> sys :crypto/wallet :wallet/address) "/jsonrpc"])
+                                                              :http/client (:client/http sys)}
+                                                             rpc/init
+                                                             (rpc.schema/schema api))]
+                                              (println (rpc/available client)))))
                         (lib.promise/catch
-                            (fn [e]
-                              (println (ex-message e))
-                              (prn (ex-data e))))))))))})
+                         (fn [e]
+                           (println (ex-message e))
+                           (prn (ex-data e))))))))))})
