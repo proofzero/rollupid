3e713fe6699dba23275bf2b07968adf01f92a4ec
tangrammer <juanantonioruz@gmail.com>
2022-05-24 18:05:28 +0200

    Refactor

--- src/main/com/kubelt/ddt/cmds/rpc/core/config/get.cljs
+++ src/main/com/kubelt/ddt/cmds/rpc/core/config/get.cljs
@@ -2,25 +2,22 @@
   "RPC core config get"
   {:copyright "ⓒ2022 Proof Zero Inc." :license "Apache 2.0"}
   (:require
-   [com.kubelt.ddt.cmds.rpc.core.config :as core.config]
    [com.kubelt.ddt.util :as ddt.util]
    [com.kubelt.ddt.options :as ddt.options]
    [com.kubelt.ddt.cmds.rpc.call :as rpc.call]))
 
 
 (defonce command
-  {:command "get"
+  {:command "get [path]"
    :desc "Make an RPC call."
    :requiresArg true
    :builder (fn [^Yargs yargs]
-              ;; Include the common options.
               (ddt.options/options yargs)
-              (.option yargs rpc.call/ethers-rpc-name rpc.call/ethers-rpc-config)
-              (.option yargs core.config/json-path-name core.config/json-path-config))
+              (.option yargs rpc.call/ethers-rpc-name rpc.call/ethers-rpc-config))
    :handler (fn [args]
               (let [args-map (ddt.options/to-map args)
-                    path  (get args-map (keyword core.config/json-path-name) "")
+                    path  (get args-map :path "")
                     path* (ddt.util/rpc-name->path path)
-                    handler (rpc.call/call-handler #(println (str "Selecting config-path (" path "): "(get-in % path*))))]
+                    handler (rpc.call/call-handler #(println (str "Selecting config-path (" path "): " (get-in % path*))))]
                 (aset args "method" ":kb:get:config")
                 (handler args)))})
