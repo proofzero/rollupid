f9e966682d522581fd733123bf59c939adb8b706
tangrammer <juanantonioruz@gmail.com>
2022-06-03 11:47:04 +0200

    Adap: timeout logic is now defined as delay,
    and timeout follows promesa lib logic
    
    "Returns a cancellable promise that will be fulfilled with this
      promise's fulfillment value or rejection reason.  However, if this
      promise is not fulfilled or rejected within `ms` milliseconds, the
      returned promise is cancelled with a TimeoutError"

--- src/main/com/kubelt/lib/promise.cljs
+++ src/main/com/kubelt/lib/promise.cljs
@@ -50,20 +50,42 @@
   (p/rejected x))
 
 (defn timeout
-  "Returns a promise that resolves after the given millisecond interval."
+  "Returns a cancellable promise that will be fulfilled with this
+  promise's fulfillment value or rejection reason.  However, if this
+  promise is not fulfilled or rejected within `ms` milliseconds, the
+  returned promise is cancelled with a TimeoutError"
+  [p ms]
+  (p/timeout p ms))
+
+(defn delay
+  "Given a timeout in miliseconds and optional value, returns a promise
+  that will fulfilled with provided value (or nil) after the time is
+  reached."
   [ms]
-  (promise (fn [resolve _]
-             (js/setTimeout resolve ms))))
+  (p/delay ms))
 
 (defn any
-  "Takes an iterable of promises and returns a single promise that
-  resolves as soon as any of the promises in the iterable fulfills, with
-  the value of the fulfilled promise."
+  "Given an array of promises, return a promise that is fulfilled when
+  first one item in the array is fulfilled."
   [coll]
   (p/any coll))
 
 (defn all
-  "Takes an iterable of promises and returns a single promise that
-  resolves to an array of the results of the input promises."
+  "Given an array of promises, return a promise
+  that is fulfilled  when all the items in the
+  array are fulfilled.
+
+  Example:
+
+  (-> (all [(promise :first-promise)
+            (promise :second-promise)]
+      (then (fn [[first-result second-result]]))
+       (println (str first-result \", \" second-result)
+
+  Will print out
+  :first-promise, :second-promise.
+
+  If at least one of the promises is rejected, the resulting promise will be
+  rejected."
   [coll]
   (p/all coll))
