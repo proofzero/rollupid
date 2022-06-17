6294671be05bebb8f3eee4dacbb0d4e4ecbc53c9
tangrammer <juanantonioruz@gmail.com>
2022-06-17 12:06:40 +0200

    Adapt to com.kubelt.lib.vault/vault? expectations
    Internally used by v1/store

--- src/main/com/kubelt/sdk/v1/oort.cljs
+++ src/main/com/kubelt/sdk/v1/oort.cljs
@@ -6,6 +6,8 @@
    [com.kubelt.lib.http.status :as http.status]
    [com.kubelt.lib.jwt :as lib.jwt]
    [com.kubelt.lib.oort :as lib.oort]
+   [com.kubelt.lib.vault :as lib.vault]
+   [clojure.set :refer [rename-keys]]
    [com.kubelt.lib.promise :as lib.promise :refer [promise]]
    [com.kubelt.lib.wallet :as lib.wallet]))
 
@@ -73,8 +75,9 @@
                      ;; subset, e.g. tokens) to be frozen and thawed
                      ;; back out
                      (-> sys
-                         (assoc-in [:crypto/session :vault/tokens core] decoded-jwt)
-                         (assoc-in [:crypto/session :vault/tokens* core] verify-result)))))))))))))
+                         (assoc-in [:crypto/session] (lib.vault/vault {core decoded-jwt}))
+                         (assoc-in [:crypto/session] (-> (lib.vault/vault {core verify-result})
+                                                         (rename-keys {:vault/tokens :vault/tokens*})))))))))))))))
 
 (defn authenticate-js!
   "Create an account from a JavaScript context."
