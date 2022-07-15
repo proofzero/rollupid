f5d03795666f622b054d919542b12efcfeafd4b9
tangrammer <juanantonioruz@gmail.com>
2022-07-15 19:16:34 +0200

    Automate Typescript type generation

--- src/dev/jsonspec/utils.cljs
+++ src/dev/jsonspec/utils.cljs
@@ -1,24 +1,55 @@
 (ns jsonspec.utils
   (:require
    [malli.json-schema :as json-schema]
-   [malli.generator :as mg]
    [com.kubelt.spec.profile :as spec.profile]
    [com.kubelt.lib.io.node :as io]
    [com.kubelt.lib.promise :as lib.promise]
-   [com.kubelt.lib.json :as json]))
+   [com.kubelt.lib.json :as json]
+   [clojure.string :as str])
+  (:require
+   ["process" :as process]
+   ["json-schema-to-typescript" :refer [compileFromFile]]))
 
-
-(def local-path "/Users/tangrammer/git/kubelt/kubelt/")
+(def local-path (str/replace (.cwd process) "packages/sdk-js/lib" ""))
 
 (defn json-spec-path [filename]
   (str local-path "src/main/com/kubelt/jsonspec/" filename ".json"))
 
-(comment
+(defn ts-types-path [filename]
+  (str local-path "three-id/src/types/" filename ".d.ts"))
 
- (-> (io/write-to-file& (json/edn->json-forjs-str (json-schema/transform spec.profile/profile))
-                        (json-spec-path "Profile") )
-     (lib.promise/then  (fn [x] (println "done!" x)))
-     (lib.promise/catch  (fn [e] (println "error! " e)))
-     )
+(defn generate-ts-type& [malli-spec type-name]
+  (lib.promise/promise
+   (fn [resolve reject]
+     (-> (io/write-to-file&
+          (json-spec-path type-name)
+          (.stringify js/JSON (clj->js (json-schema/transform malli-spec)) nil 2))
+         (lib.promise/then
+          (fn [_]
+            (-> (compileFromFile (json-spec-path type-name)
+                                 (clj->js {"additionalProperties" false
+                                           "bannerComment" ""
+                                           "unknownAny" false}))
+                (lib.promise/then  (fn [x]
+                                     (io/write-to-file&
+                                      (ts-types-path type-name)
+                                      x)))
+                (lib.promise/then  (fn [_] (resolve (str "[SUCCESS] " type-name))))
+                (lib.promise/catch (fn [e] (reject (str "[ERROR] " e)))))))
+         (lib.promise/catch  (fn [e] (reject (str "[ERROR] " e))))))))
+
+
+(defn generate-all& [col]
+  (->>
+   col
+   (mapv (fn [[malli-spec ts-name]]
+           (generate-ts-type& malli-spec ts-name)))
+   (lib.promise/all)))
+
+(comment
+  (-> (generate-all& [[spec.profile/profile "Profile"]])
+      (lib.promise/then #(println "YIKES! .... " %))
+      (lib.promise/catch #(println "ERROR!.... " %)))
+
 ;;
- )
+  )
