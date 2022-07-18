061e9af43b1ef3086f9683918dfe28d86306bb82
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-07-18 12:35:06 +0300

    Format, lint & some todos

--- three-id/src/provider/kubelt.ts
+++ three-id/src/provider/kubelt.ts
@@ -78,35 +78,38 @@ export const isAuthenticated = async (address: string | null | undefined) => {
 };
 
 export const kbGetProfile = async (core: string) => {
-
   const profile: Profile = await new Promise((resolve, reject) => {
-    sdkWeb?.node_v1?.oort.rpcApi(sdk, core)
-      .then((api: any) => {
-        console.log(api);
-        sdkWeb?.node_v1?.oort.callRpcClient(sdk, api, {"method": ["kb", "get", "profile"],
-                                                       "params":[]})
-          .then ((x: any) => {
-            resolve(x?.body?.result);})
-        // TODO check if not 200 status response
+    sdkWeb?.node_v1?.oort.rpcApi(sdk, core).then((api: any) => {
+      console.log(api);
+      // TODO: async / await? 🤔
+      sdkWeb?.node_v1?.oort
+        .callRpcClient(sdk, api, {
+          method: ["kb", "get", "profile"],
+          params: [],
+        })
+        .then((x: any) => {
+          resolve(x?.body?.result);
+        });
+      // TODO check if not 200 status response
     });
-
   });
 
-  console.log('get_profile_response', profile);
+  console.log("get_profile_response", profile);
   return profile;
-
-}
+};
 
 export const kbSetProfile = async (core: string, updatedProfile: Profile) => {
   const profile = await new Promise((resolve, reject) => {
-    sdkWeb?.node_v1?.oort.rpcApi(sdk, core)
-      .then((api: any) => {
-        sdkWeb?.node_v1?.oort.callRpcClient(sdk, api, {"method": ["kb", "set", "profile"],
-                                                       "params": {"profile": updatedProfile}})
-          .then ((x: any) => resolve(x?.body?.result))
-        // TODO check if not 200 status response
+    sdkWeb?.node_v1?.oort.rpcApi(sdk, core).then((api: any) => {
+      sdkWeb?.node_v1?.oort
+        .callRpcClient(sdk, api, {
+          method: ["kb", "set", "profile"],
+          params: { profile: updatedProfile },
+        })
+        .then((x: any) => resolve(x?.body?.result));
+      // TODO check if not 200 status response
     });
   });
-  console.log('set_profile response ', profile);
+  console.log("set_profile response ", profile);
   return profile;
 };
