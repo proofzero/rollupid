9f450e30ddea6fc6d970f19ec2d102709fe7f28e
Dhruvil Patel <dhruvil.h.patel@gmail.com>
2022-07-21 09:14:57 -0400

    feat(three-id): Add test to sign metamask request

--- three-id/tests/e2e/specs/gate-spec.js
+++ three-id/tests/e2e/specs/gate-spec.js
@@ -1,10 +1,16 @@
 describe('Metamask', () => {
-  context('Test commands', () => {
+  context('Gate commands', () => {
     it(`acceptMetamaskAccess should accept connection request to metamask`, () => {
       cy.visit('/');
       cy.findByTestId('connect-wallet').click();
       cy.acceptMetamaskAccess().then(connected => {
         expect(connected).to.be.true;
+      });
+    });
+
+    it(`confirmMetamaskSignatureRequest should succeed`, () => {
+      cy.confirmMetamaskSignatureRequest().then(signed => {
+        expect(signed).to.be.true;
       });
     });
   });
