978e8dd030b8f0ac82aaf7fa9b2a4c586b24e7d9
Robert Medeiros <robert@crimeminister.org>
2022-07-25 15:48:52 -0400

    chore: remove unused ext for hygiene (#603)

--- ext/src/main/kubelt/popup/script.cljs
+++
@@ -1,8 +0,0 @@
-(ns kubelt.popup.script
-  "Business logic for the popup."
-  {:copyright "©2022 Proof Zero Inc." :license "Apache 2.0"})
-
-
-(defn init
-  []
-  (js/console.log "popup"))
--- ext/src/main/kubelt/worker/core.cljs
+++
@@ -1,9 +0,0 @@
-(ns kubelt.worker.core
-  "Kubelt web worker implementation. Responsible for content state
-  and metrics as well as running user-supplied queries."
-  {:copyright "©2022 Proof Zero Inc." :license "Apache 2.0"})
-
-(defn init []
-  (js/self.addEventListener "message"
-   (fn [^js e]
-     (js/postMessage (.. e -data)))))
