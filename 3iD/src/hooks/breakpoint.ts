e8c040c1d7ccc4df1eae46368d0e71b61ad77838
Adrian Maurer <adrian@kubelt.com>
2022-08-26 13:54:31 -0400

    480

--- 3iD/src/hooks/breakpoint.ts
+++ 3iD/src/hooks/breakpoint.ts
@@ -1,6 +1,6 @@
 import { useWindowDimensions } from "react-native";
 
-const useBreakpoint = <T>(wide: T, tall: T, breakpoint = 1600): T => {
+const useBreakpoint = <T>(wide: T, tall: T, breakpoint = 480): T => {
   const window = useWindowDimensions();
 
   return window.width >= breakpoint ? wide : tall;
