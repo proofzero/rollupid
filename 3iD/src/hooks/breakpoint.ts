0f8b286dfacdb4472253deea688cbf0ed167410b
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-08-26 16:41:30 +0300

    Made default breakpoint 1600

--- 3iD/src/hooks/breakpoint.ts
+++ 3iD/src/hooks/breakpoint.ts
@@ -1,6 +1,6 @@
 import { useWindowDimensions } from "react-native";
 
-const useBreakpoint = <T>(wide: T, tall: T, breakpoint = 800): T => {
+const useBreakpoint = <T>(wide: T, tall: T, breakpoint = 1600): T => {
   const window = useWindowDimensions();
 
   return window.width >= breakpoint ? wide : tall;
