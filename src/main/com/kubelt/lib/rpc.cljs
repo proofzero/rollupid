b9712bb325636eb29ff64fb543a3cf7f0723e4a0
Adrian Maurer <adrian@kubelt.com>
2022-08-29 17:27:26 -0400

    chore(sdk): expire test and remove @

--- src/main/com/kubelt/lib/rpc.cljs
+++ src/main/com/kubelt/lib/rpc.cljs
@@ -17,7 +17,7 @@
            client (-> {:uri/domain (-> sys :client/oort :http/host)
                        :uri/port (-> sys :client/oort :http/port)
                        :uri/scheme (-> sys :client/oort :http/scheme)
-                       :uri/path (cstr/join "" ["/@" wallet-address "/jsonrpc"])
+                       :uri/path (cstr/join "" ["/" wallet-address "/jsonrpc"])
                        :http/client (:client/http sys)
                        :rpc/jwt (get-in sys [:crypto/session :vault/tokens* wallet-address])}
                       rpc/init
