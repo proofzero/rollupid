dbe161a2113900a5febf9a04a92f1b55b8e8ab94
maurerbot <adrianmaurer+github@fastmail.com>
2022-08-29 08:34:26 -0400

    preview oauth3

---
+++ threeid/app/routes/oAuth3.tsx
@@ -0,0 +1,37 @@
+import type { 
+    LoaderFunction,
+    ActionFunction,
+    json
+} from "@remix-run/cloudflare";
+
+export const loader: LoaderFunction = async ({
+    request,
+  }) => {
+    const url = new URL(request.url);
+    const identity = url.searchParams.get("identity");
+    const claims = url.searchParams.get("claims");
+    const appId = url.searchParams.get("appId");
+    const redirectUrl = url.searchParams.get("redirectUrl");
+
+    // TODO: call oort to resolve super core
+
+    // TODO: render on client an oauth box
+    return json({
+        identity,
+        claims,
+        appId,
+        redirectUrl,
+    });
+  };
+
+  export const action: ActionFunction = async ({
+    request,
+  }) => {
+    // TODO: accept from client accept or deny request
+    // TODO: genearte jwt
+    // TODO: redirect to app
+  };
+
+export default function OAuth3 ()  {
+    
+}
