ca67f7f8a80c1b22086b459d9aa39c276bccf24e
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-09-09 13:44:22 +0300

    Updated side-nav font family...

--- threeid/app/components/side-nav/SideNavItem.tsx
+++ threeid/app/components/side-nav/SideNavItem.tsx
@@ -1,4 +1,5 @@
 import ConditionalTooltip from "../conditional-tooltip";
+import Text, { TextColor, TextSize, TextWeight } from "../typography/Text";
 
 type SideNavItemProps = {
   item: {
@@ -13,22 +14,43 @@ const SideNavItem = ({ item }: SideNavItemProps) => {
   return (
     <div
       className={
-        item.current
-          ? "bg-gray-100 text-gray-600"
-          : "bg-transparent text-gray-400 hover:bg-gray-100 hover:text-gray-600"
+        item.current ? "bg-gray-100" : "bg-transparent hover:bg-gray-100"
       }
     >
       <a
         href={item.href}
         aria-current={item.current ? "page" : undefined}
-        className="group border-l-4 px-3 py-2 flex items-center text-sm font-medium"
+        className="group border-l-4 px-3 py-2 flex items-center"
       >
         <item.icon
-          className="flex-shrink-0 -ml-1 mr-3 h-6 w-6 side-nav-item-font"
+          className="flex-shrink-0 -ml-1 mr-3 h-6 w-6"
+          style={{
+            color: item.current ? "#4B5563" : "#9CA3AF",
+          }}
           aria-hidden="true"
         />
+
         <ConditionalTooltip content="Coming Soon" condition={!item.exists}>
-          <span className="truncate side-nav-item-font">{item.name}</span>
+          {item.current && (
+            <Text
+              className="truncate"
+              size={TextSize.Base}
+              weight={TextWeight.Medium500}
+              color={TextColor.Gray600}
+            >
+              {item.name}
+            </Text>
+          )}
+          {!item.current && (
+            <Text
+              className="truncate"
+              size={TextSize.Base}
+              weight={TextWeight.Medium500}
+              color={TextColor.Gray400}
+            >
+              {item.name}
+            </Text>
+          )}
         </ConditionalTooltip>
       </a>
     </div>
