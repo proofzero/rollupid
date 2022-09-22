d762275397688e3ec6eaa6ed6ef6be83a47834c3
Alex Flanagan <alex@kubelt.com>
2022-09-22 15:56:58 -0400

    Updated tests

--- eth/bin/local_profile_smoke.sh
+++ eth/bin/local_profile_smoke.sh
@@ -2,7 +2,7 @@
 set -ex
 npx hardhat profile:deploy
 npx hardhat profile:next
-npx hardhat profile:mint --account 0x3DaC36FE079e311489c6cF5CC456a6f38FE01A52
+npx hardhat profile:mint --account account5
 npx hardhat profile:owner --profile-id 0
 npx hardhat profile:image --profile-id 0
 npx hardhat profile:metadata --profile-id 0
