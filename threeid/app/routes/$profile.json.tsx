fbaeede44bb4c92f6c1854e3b7a50f48a1d72339
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-09-30 15:23:08 +0300

    Replaced Oort getData calls with profile endpoint

--- threeid/app/routes/$profile.json.tsx
+++ threeid/app/routes/$profile.json.tsx
@@ -3,30 +3,32 @@ import { oortSend } from "~/utils/rpc.server";
 import { getUserSession } from "~/utils/session.server";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
-  const session = await getUserSession(request);
-  const jwt = session.get("jwt");
+  // const session = await getUserSession(request);
+  // const jwt = session.get("jwt");
 
-  if (!jwt) {
-    throw new Error("JWT required for Oort operations");
+  // if (!jwt) {
+  //   throw new Error("JWT required for Oort operations");
+  // }
+
+  // const oortOptions = {
+  //   jwt: jwt,
+  //   cookie: request.headers.get("Cookie") as string | undefined,
+  // };
+
+  // const [displayname, pfp] = await Promise.all([
+  //   oortSend("kb_getData", ["3id.profile", "displayname"], oortOptions),
+  //   oortSend("kb_getData", ["3id.profile", "pfp"], oortOptions),
+  // ]);
+
+  // @ts-ignore
+  const url = `${OORT_SCHEMA}://${OORT_HOST}:${OORT_PORT}/@${params.profile}/3id/profile`;
+
+  const publicProfile = await fetch(url);
+  const publicProfileJson = await publicProfile.json();
+
+  if (publicProfileJson.error) {
+    throw new Error(publicProfileJson.error);
   }
 
-  const oortOptions = {
-    jwt: jwt,
-    cookie: request.headers.get("Cookie") as string | undefined,
-  };
-
-  const [displayname, pfp] = await Promise.all([
-    oortSend("kb_getData", ["3id.profile", "displayname"], oortOptions),
-    oortSend("kb_getData", ["3id.profile", "pfp"], oortOptions),
-  ]);
-
-  let profile: {
-    displayname?: string;
-    pfp?: string;
-  } = {
-    displayname: displayname.result?.value ?? undefined,
-    pfp: pfp.result?.value ?? undefined,
-  };
-
-  return json(profile);
+  return json(publicProfileJson);
 };
