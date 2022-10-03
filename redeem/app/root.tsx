fe6229512c2ed146debf4dca80ccd2fc5daee602
Adrian Maurer <adrian@kubelt.com>
2022-10-03 18:01:17 -0400

    feat(redeem): remix version

---
+++ redeem/app/root.tsx
@@ -0,0 +1,210 @@
+import type { MetaFunction, LoaderFunction, LinksFunction } from "@remix-run/cloudflare";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData, useCatch, useNavigate  } from "@remix-run/react";
+
+// import styles from "~/assets/styles.css";
+import styles from "./tailwind.css";
+
+import blankCard from "~/assets/blankcard.png";
+import logo from "~/assets/logo.png";
+import sad from "~/assets/sad.png";
+
+import { startSession } from "~/datadog.client";
+
+export const loader: LoaderFunction = () => {
+  return json({
+    ENV: {
+      // @ts-ignore
+      DATADOG_APPLICATION_ID: DATADOG_APPLICATION_ID,
+      // @ts-ignore
+      DATADOG_CLIENT_TOKEN: DATADOG_CLIENT_TOKEN,
+      // @ts-ignore
+      DATADOG_SERVICE_NAME: DATADOG_SERVICE_NAME,
+      // @ts-ignore
+      DATADOG_ENV: DATADOG_ENV,
+      // @ts-ignore
+      INVITE_CONTRACT_ADDRESS: INVITE_CONTRACT_ADDRESS,
+    },
+  });
+};
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "3ID Redeem",
+  viewport: "width=device-width,initial-scale=1",
+  "og:title": "Get 3ID - Decentralized Web Passport",
+  "og:site_name": "Get 3ID",
+  "og:url": "https://get.threeid.xyz",
+  "og:description":
+    "Claim your 3ID Invite.",
+  "og:image": blankCard,
+  "theme-color": "#673ab8",
+  "mobile-web-app-capable": "yes",
+  "apple-mobile-web-app-capable": "yes",
+});
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
+
+export default function App() {
+  const browserEnv = useLoaderData();
+  typeof window !== 'undefined' && startSession();
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div
+          style={{
+            display: "flex",
+            // flexDirection: "column",
+            minHeight: "100vh",
+            alignItems: "center",
+          }}
+        >
+          
+
+          <main
+            className="container-fluid"
+            style={{
+              // display: "flex",
+              // flex: 1,
+              // maxWidth: "740px"
+              marginTop: "70px"
+            }}
+          >
+            <div className="row d-flex align-self-center" style={{
+              // display: "flex",
+              // flex: 1,
+              // maxWidth: "740px"
+              marginTop:-40,
+            }}
+          >
+              <div className="col-12 mx-auto">
+                <Outlet />
+
+              </div>
+            </div>
+            <div
+              className="row d-flex align-self-center "
+              style={{
+                // display: "flex",
+                // flexDirection: "row",
+                // justifyContent: "center",
+                marginTop: "2em",
+              }}>
+              <div className="col-12 mx-auto" >
+                <p className="gray-text text-center">
+                  3ID is non-custodial and secure. We will never request access to your assets.
+                </p>
+              </div>
+            </div>
+          </main>
+        </div>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(browserEnv.ENV)}`,
+          }}
+        />
+      </body>
+    </html>
+  );
+}
+
+// https://remix.run/docs/en/v1/guides/errors
+
+export function ErrorBoundary({ error }) {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="error-screen">
+        <div className="wrapper grid grid-row-3 gap-4">
+          <nav className="col-span-3">
+            <img src={logo} alt="threeid" />
+          </nav>
+          <article className="content col-span-3">
+            <div className="error justify-center items-center">
+              <img className="m-auto pb-12" src={sad}/>
+              <p className="error-message">
+                Something went terribly wrong!
+              </p>
+              <p className="error-secondary-message">{error.message}</p>
+              <p className="error-secondary-message">
+                If this problem persists please join Discord for help
+              </p>
+              
+              <div className="error-buttons grid grid-rows-1 text-center">
+                  <BaseButtonAnchor text={"Go to Discord"} color={"dark"} href={"https://discord.gg/threeid"} />
+              </div>
+            </div>
+          </article>
+          <article className="content col-span-3 m-12">
+          <div className="error justify-center items-center">
+              <p>The stack trace is:</p>
+              <pre>{error.stack}</pre>
+            </div>
+          </article>
+        </div>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload port={8002} />
+      </body>
+    </html>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  const navigate = useNavigate()
+  const goBack = () => { 
+    navigate(-1) 
+  }
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="error-screen">
+        <div className="wrapper grid grid-row-3 gap-4">
+          <nav className="col-span-3">
+            <img src={logo} alt="threeid" />
+          </nav>
+          <article className="content col-span-3">
+            <div className="error justify-center items-center">
+              <img className="m-auto pb-12" src={sad}/>
+              <p className="error-message">
+                Oops!
+              </p>
+              <p className="error-secondary-message">{caught.status} {caught.statusText}</p>
+              <div className="error-buttons grid grid-rows-1 text-center">
+                  <BaseButton onClick={goBack} text={"Go Back"} color={"dark"} />
+              </div>
+            </div>
+          </article>
+          <div>{" "}</div>
+        </div>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload port={8002} />
+      </body>
+    </html>
+  );
+}
