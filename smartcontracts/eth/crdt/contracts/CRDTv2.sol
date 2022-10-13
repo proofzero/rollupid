5352ff67e850359b1508c28ae83442feea158e2f
Alex Flanagan <alex@kubelt.com>
2022-10-13 13:58:22 -0400

    wip v2 not upgradable

--- smartcontracts/eth/crdt/contracts/CRDTv2.sol
+++ smartcontracts/eth/crdt/contracts/CRDTv2.sol
@@ -2,23 +2,25 @@
 pragma solidity ^0.8.12;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts-upgradeable/utils/introspection/ERC165Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+// import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20SnapshotUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20FlashMintUpgradeable.sol";
+// import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+// import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+// import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+// import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20FlashMintUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 /// @custom:security-contact admin@kubelt.com
-contract KubeltPlatformCreditsV2 is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, AccessControlUpgradeable, PausableUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, ERC20FlashMintUpgradeable, UUPSUpgradeable {
+// , ERC20BurnableUpgradeable, PausableUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, ERC20FlashMintUpgradeable
+contract KubeltPlatformCreditsV2 is Initializable, ERC20Upgradeable, AccessControlUpgradeable, UUPSUpgradeable, ERC20SnapshotUpgradeable {
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
     bytes32 public constant SNAPSHOT_ROLE = keccak256("SNAPSHOT_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -27,42 +29,44 @@ contract KubeltPlatformCreditsV2 is Initializable, ERC20Upgradeable, ERC20Burnab
 
     function initialize() initializer public {
         __ERC20_init("Kubelt Platform Credits", "CRDT");
-        __ERC20Burnable_init();
-        __ERC20Snapshot_init();
         __AccessControl_init();
-        __Pausable_init();
-        __ERC20Permit_init("Kubelt Platform Credits");
-        __ERC20Votes_init();
-        __ERC20FlashMint_init();
         __UUPSUpgradeable_init();
 
+        _mint(msg.sender, 100000000 * 10 ** decimals());
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
+
+        // __ERC20Burnable_init();
+        __ERC20Snapshot_init();
+        // __Pausable_init();
+        // __ERC20Permit_init("Kubelt Platform Credits");
+        // __ERC20Votes_init();
+        // __ERC20FlashMint_init();
+
         _grantRole(SNAPSHOT_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
-        _mint(msg.sender, 100000000 * 10 ** decimals());
         _grantRole(MINTER_ROLE, msg.sender);
-        _grantRole(UPGRADER_ROLE, msg.sender);
     }
 
     function snapshot() public onlyRole(SNAPSHOT_ROLE) {
         _snapshot();
     }
 
-    function pause() public onlyRole(PAUSER_ROLE) {
-        _pause();
-    }
+    // function pause() public onlyRole(PAUSER_ROLE) {
+    //     _pause();
+    // }
 
-    function unpause() public onlyRole(PAUSER_ROLE) {
-        _unpause();
-    }
+    // function unpause() public onlyRole(PAUSER_ROLE) {
+    //     _unpause();
+    // }
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
-    }
+    // function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    //     _mint(to, amount);
+    // }
 
+        // whenNotPaused
     function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal
-        whenNotPaused
         override(ERC20Upgradeable, ERC20SnapshotUpgradeable)
     {
         super._beforeTokenTransfer(from, to, amount);
@@ -76,26 +80,26 @@ contract KubeltPlatformCreditsV2 is Initializable, ERC20Upgradeable, ERC20Burnab
 
     // The following functions are overrides required by Solidity.
 
-    function _afterTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._afterTokenTransfer(from, to, amount);
-    }
+    // function _afterTokenTransfer(address from, address to, uint256 amount)
+    //     internal
+    //     override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    // {
+    //     super._afterTokenTransfer(from, to, amount);
+    // }
 
-    function _mint(address to, uint256 amount)
-        internal
-        override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._mint(to, amount);
-    }
+    // function _mint(address to, uint256 amount)
+    //     internal
+    //     override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    // {
+    //     super._mint(to, amount);
+    // }
 
-    function _burn(address account, uint256 amount)
-        internal
-        override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._burn(account, amount);
-    }
+    // function _burn(address account, uint256 amount)
+    //     internal
+    //     override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    // {
+    //     super._burn(account, amount);
+    // }
 
     function getMessage() public view returns (uint) {
         uint message = 1;
