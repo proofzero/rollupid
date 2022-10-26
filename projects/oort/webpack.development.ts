8606c9ab046604beccdc3e8c1fc29e610eeb565d
Adrian Maurer <adrian@kubelt.com>
2022-10-26 18:01:52 -0400

    fix: test

--- projects/oort/webpack.development.ts
+++ projects/oort/webpack.development.ts
@@ -5,8 +5,8 @@ export default merge(base, {
   mode: 'development',
   devtool: 'inline-source-map',
   output: {
-    filename: 'index.js',
-    libraryTarget: 'commonjs',
+    // filename: 'index.js',
+    // libraryTarget: 'commonjs',
     sourceMapFilename: 'index.js.map',
   },
   optimization: {
