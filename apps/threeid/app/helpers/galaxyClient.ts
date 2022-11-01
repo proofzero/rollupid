1abc3610d11ce4c254c2004c03d48ea0ae1a1c65
Adrian Maurer <adrianmaurer+github@fastmail.com>
2022-11-01 12:42:47 -0400

    wip: bind

--- apps/threeid/app/helpers/galaxyClient.ts
+++ apps/threeid/app/helpers/galaxyClient.ts
@@ -1,16 +1,9 @@
 import { GraphQLClient } from "graphql-request";
 import { getSdk } from "~/utils/galaxy.server";
 
-const serviceBindingFetchWrapper = function (
-  input: RequestInfo,
-  init?: RequestInit
-) {
-  // @ ts-ignore
-  return GALAXY.fetch(input, init);
-};
-
 const gqlClient = new GraphQLClient("http://127.0.0.1", {
-  fetch: serviceBindingFetchWrapper,
+  // @ts-ignore
+  fetch: GALAXY.fetch.bind(GALAXY),
 });
 
 export default getSdk(gqlClient);
