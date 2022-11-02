138213815675651714abd290e987a29e6a89ef17
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-11-02 12:51:44 +0200

    Changed modal opacity to better match design

--- apps/threeid/app/components/modal/Modal.tsx
+++ apps/threeid/app/components/modal/Modal.tsx
@@ -34,7 +34,7 @@ const Modal = ({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
