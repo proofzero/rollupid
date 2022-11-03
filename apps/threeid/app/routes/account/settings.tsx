1c41be011d3faf614f749d4c3fe8073964e4c751
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-11-03 11:55:55 +0200

    Added notify pill context for settings layout

--- apps/threeid/app/routes/account/settings.tsx
+++ apps/threeid/app/routes/account/settings.tsx
@@ -1,34 +1,77 @@
-import { Outlet } from "@remix-run/react";
-import { NavLink, Link } from "react-router-dom";
+import { Outlet } from '@remix-run/react'
+import { useState } from 'react'
+import { HiCheckCircle, HiXCircle } from 'react-icons/hi'
 
 import Text, {
   TextColor,
   TextSize,
   TextWeight,
-} from "~/components/typography/Text";
+} from '~/components/typography/Text'
 
 const tabs = [
-  { name: "Profile", to: "profile" },
-  { name: "Integrations", to: "integrations", disabled: true },
-  { name: "Connected Accounts", to: "connections", disabled: true },
-];
+  { name: 'Profile', to: 'profile' },
+  { name: 'Integrations', to: 'integrations', disabled: true },
+  { name: 'Connected Accounts', to: 'connections', disabled: true },
+]
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(' ')
 }
 
 export default function AccountSetting() {
-  const activeStyle = {};
+  const [notifyPillVal, setNotifyPillVal] = useState<{
+    message: string
+    success: boolean
+  } | null>(null)
+
+  const notify = (success: boolean = true) => {
+    setNotifyPillVal({
+      message: success ? 'Saved' : 'Save Failed - Please try again',
+      success,
+    })
+
+    setTimeout(() => {
+      setNotifyPillVal(null)
+    }, 2500)
+  }
+
   return (
     <div>
-      <div className="mb-4">
+      <div className="mb-4 flex flex-row justify-between">
         <Text
           size={TextSize.XL}
           weight={TextWeight.Bold700}
           color={TextColor.Gray900}
+          className="my-4"
         >
           Settings
         </Text>
+
+        {notifyPillVal && (
+          <div
+            className={`p-4 flex flex-row items-center bg-${
+              notifyPillVal.success ? 'green' : 'red'
+            }-50 rounded-md`}
+          >
+            <span className="mr-3.5">
+              {notifyPillVal.success ? (
+                <HiCheckCircle className="text-green-800" />
+              ) : (
+                <HiXCircle className="text-red-800" />
+              )}
+            </span>
+
+            <Text
+              size={TextSize.SM}
+              weight={TextWeight.Medium500}
+              color={
+                notifyPillVal.success ? TextColor.Green800 : TextColor.Red800
+              }
+            >
+              {notifyPillVal.message}
+            </Text>
+          </div>
+        )}
       </div>
       <div className="sm:hidden">
         <label htmlFor="tabs" className="sr-only">
@@ -52,14 +95,14 @@ export default function AccountSetting() {
             {tabs.map((tab) => (
               <a
                 key={tab.name}
-                href={!tab.disabled ? tab.to : "#"}
+                href={!tab.disabled ? tab.to : '#'}
                 // prefetch="render"
                 className={classNames(
                   tab?.current
-                    ? "border-indigo-500 text-indigo-600"
-                    : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                  tab.disabled ? "cursor-not-allowed opacity-50" : "",
-                  "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
+                    ? 'border-indigo-500 text-indigo-600'
+                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+                  tab.disabled ? 'cursor-not-allowed opacity-50' : '',
+                  'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm'
                 )}
               >
                 {tab.name}
@@ -69,8 +112,12 @@ export default function AccountSetting() {
         </div>
       </div>
       <div>
-        <Outlet />
+        <Outlet
+          context={{
+            notificationHandler: notify,
+          }}
+        />
       </div>
     </div>
-  );
+  )
 }
