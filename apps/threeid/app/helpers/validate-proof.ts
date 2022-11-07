3982bae86fd0f202220e2cef614ed944847ad0ef
Adrian Maurer <adrian@kubelt.com>
2022-11-07 07:56:44 -0500

    fix(threeid): reverse order

--- apps/threeid/app/helpers/validate-proof.ts
+++ apps/threeid/app/helpers/validate-proof.ts
@@ -2,5 +2,5 @@ export default async (address: string): Promise<boolean> => {
   // @ts-ignore
   const proof = await PROOFS.get(address)
   // @ts-ignore
-  return !!TEST_ENV || !!proof
+  return !!proof || !!TEST_ENV
 }
