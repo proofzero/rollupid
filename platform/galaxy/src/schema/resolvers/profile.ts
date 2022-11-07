3dc30cae99abfa9ea08ceee9b48b14e8c2b60625
Adrian Maurer <adrian@kubelt.com>
2022-11-07 12:50:57 -0500

    closes: #961

--- platform/galaxy/src/schema/resolvers/profile.ts
+++ platform/galaxy/src/schema/resolvers/profile.ts
@@ -1,93 +1,93 @@
-import { composeResolvers } from "@graphql-tools/resolvers-composition";
-import Env from "../../env";
-import OortClient from "./clients/oort";
+import { composeResolvers } from '@graphql-tools/resolvers-composition'
+import Env from '../../env'
+import OortClient from './clients/oort'
 import {
   setupContext,
   isAuthorized,
   checkHTTPStatus,
   getRPCResult,
-} from "./utils";
+} from './utils'
 
-import { Resolvers } from "./typedefs";
+import { Resolvers } from './typedefs'
 
 type ResolverContext = {
-  env: Env;
-  jwt?: string;
-  coreId?: string;
-};
+  env: Env
+  jwt?: string
+  coreId?: string
+}
 
 const threeIDResolvers: Resolvers = {
   Query: {
     profile: async (_parent: any, {}, { env, jwt }: ResolverContext) => {
-      const oortClient = new OortClient(env.OORT, jwt);
-      const profileResponse = await oortClient.getProfile();
-      checkHTTPStatus(profileResponse);
-      const res = getRPCResult(profileResponse);
-      return await res;
+      const oortClient = new OortClient(env.OORT, jwt)
+      const profileResponse = await oortClient.getProfile()
+      checkHTTPStatus(profileResponse)
+      const res = getRPCResult(profileResponse)
+      return await res
     },
     profileFromAddress: async (
       _parent: any,
       { address }: { address: string },
       { env }: ResolverContext
     ) => {
-      const oortClient = new OortClient(env.OORT);
-      const profileResponse = await oortClient.getProfileFromAddress(address);
-      checkHTTPStatus(profileResponse);
-      return await getRPCResult(profileResponse);
+      const oortClient = new OortClient(env.OORT)
+      const profileResponse = await oortClient.getProfileFromAddress(address)
+      checkHTTPStatus(profileResponse)
+      return await getRPCResult(profileResponse)
     },
   },
   Mutation: {
     updateThreeIDProfile: async (
       _parent: any,
-      { profile, visibility = "PRIVATE" },
+      { profile, visibility = 'PRIVATE' },
       { env, jwt, coreId }: ResolverContext
     ) => {
-      const oortClient = new OortClient(env.OORT, jwt);
-      const profileResponse = await oortClient.getProfile();
-      checkHTTPStatus(profileResponse);
-      const currentProfile = await getRPCResult(profileResponse);
+      const oortClient = new OortClient(env.OORT, jwt)
+      const profileResponse = await oortClient.getProfile()
+      checkHTTPStatus(profileResponse)
+      const currentProfile = await getRPCResult(profileResponse)
 
       const newProfile = {
         ...currentProfile,
         ...profile,
-      };
+      }
 
       // console.log("newProfile posted to oort", newProfile);
 
       const updateResponse = await oortClient.updateProfile(
         newProfile,
         visibility
-      );
-      checkHTTPStatus(updateResponse);
-      return !!(await getRPCResult(updateResponse));
+      )
+      checkHTTPStatus(updateResponse)
+      return !!(await getRPCResult(updateResponse))
     },
   },
   Profile: {
     __resolveType: (obj: any) => {
-      if (obj.cover) {
+      if (obj.pfp) {
         // TODO: what makes a ThreeIDProfile unique from others?
-        return "ThreeIDProfile";
+        return 'ThreeIDProfile'
       }
-      return "DefaultProfile";
+      return 'DefaultProfile'
     },
   },
   PFP: {
     __resolveType: (obj: any) => {
       if (obj.isToken) {
-        return "NFTPFP";
+        return 'NFTPFP'
       }
-      return "StandardPFP";
+      return 'StandardPFP'
     },
   },
-};
+}
 
 const ThreeIDResolverComposition = {
-  "Query.address": [setupContext()],
-  "Query.addresses": [setupContext()],
-  "Query.profile": [setupContext()],
-  "Mutation.updateThreeIDAddress": [setupContext(), isAuthorized()],
-  "Query.profileFromAddress": [setupContext()],
-  "Mutation.updateThreeIDProfile": [setupContext(), isAuthorized()],
-};
+  'Query.address': [setupContext()],
+  'Query.addresses': [setupContext()],
+  'Query.profile': [setupContext()],
+  'Mutation.updateThreeIDAddress': [setupContext(), isAuthorized()],
+  'Query.profileFromAddress': [setupContext()],
+  'Mutation.updateThreeIDProfile': [setupContext(), isAuthorized()],
+}
 
-export default composeResolvers(threeIDResolvers, ThreeIDResolverComposition);
+export default composeResolvers(threeIDResolvers, ThreeIDResolverComposition)
