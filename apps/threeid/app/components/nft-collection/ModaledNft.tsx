9b770af97dd7c861244ec675b148ab43c15f0425
Cosmin Pârvulescu <Cosmin-Parvulescu@users.noreply.github.com>
2022-11-08 15:20:52 +0200

    bug(threeid): nft load fail placeholder (#979)

--- apps/threeid/app/components/nft-collection/ModaledNft.tsx
+++ apps/threeid/app/components/nft-collection/ModaledNft.tsx
@@ -7,10 +7,15 @@ import Text, {
   TextSize,
   TextWeight,
 } from '~/components/typography/Text'
+
 import { gatewayFromIpfs } from '~/helpers/gateway-from-ipfs'
+
+import missingNftSvg from '~/assets/missing-nft.svg'
 
 const ModaledNft = ({ nft }: any) => {
   const [showModal, setShowModal] = useState(false)
+
+  const [loadFail, setLoadFail] = useState(false)
 
   return (
     <>
@@ -23,7 +28,9 @@ const ModaledNft = ({ nft }: any) => {
       <div className="relative overlay-img-wrapper">
         <div
           onClick={() => {
-            setShowModal(true)
+            if (!loadFail) {
+              setShowModal(true)
+            }
           }}
           className="absolute left-0 right-0 top-0 bottom-0 p-4 flex flex-col justify-end transition-all duration-300"
         >
@@ -43,7 +50,11 @@ const ModaledNft = ({ nft }: any) => {
           </Text>
         </div>
 
-        <img className="w-full" src={gatewayFromIpfs(nft.url)} />
+        <img
+          className="w-full"
+          src={loadFail ? missingNftSvg : gatewayFromIpfs(nft.url)}
+          onError={(e) => setLoadFail(true)}
+        />
       </div>
     </>
   )
