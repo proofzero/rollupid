08ec431a86f9edd265de273cd4abb38aeb7242e7
Cosmin Pârvulescu <Cosmin-Parvulescu@users.noreply.github.com>
2022-11-08 18:55:19 +0200

    feat(3id): nft collection filtering (#983)

--- apps/threeid/app/utils/alchemy.server.tsx
+++ apps/threeid/app/utils/alchemy.server.tsx
@@ -24,24 +24,28 @@ export type GetNFTsResponse = {
       name: string
       description: string
       properties?: any
-      attributes: Array<{ value: string; trait_type: string; display_type?: string }>
+      attributes: Array<{
+        value: string
+        trait_type: string
+        display_type?: string
+      }>
       media: NFTMedia | NFTMedia[]
       timeLastUpdated: number
-    },
+    }
     contractMetadata: {
-      name: string,
-      symbol: string,
-      totalSupply: string,
-      tokenType: string,
+      name: string
+      symbol: string
+      totalSupply: string
+      tokenType: string
       openSeaObject: {
-        floorPrice: string,
-        collectionName: string,
-        safeListRequestStatus: string,
-        imageUrl: string,
-        description: string,
-        externalUrl: string,
-        twitterUsername: string,
-        discordUrl: string,
+        floorPrice: string
+        collectionName: string
+        safeListRequestStatus: string
+        imageUrl: string
+        description: string
+        externalUrl: string
+        twitterUsername: string
+        discordUrl: string
       }
     }
   }[]
@@ -56,22 +60,38 @@ export class AlchemyClient {
   async getNFTsForOwner(
     address: string,
     options?: {
-      contracts?: string[] | null,
-      pageKey?: string | null,
-      withMetadata?: boolean | null,
+      contracts?: string[] | null
+      pageKey?: string | null
+      withMetadata?: boolean | null
+      pageSize?: number | null
     }
   ): Promise<GetNFTsResponse> {
     // @ts-ignore
     const reqUrl = new URL(`${ALCHEMY_NFT_API_URL}/getNFTs`)
     reqUrl.searchParams.set('owner', address)
-    options?.contracts && (options.contracts.forEach((contract, idx) => reqUrl.searchParams.append("contractAddresses\[\]", contract)))
+    options?.contracts &&
+      options.contracts.forEach((contract, idx) =>
+        reqUrl.searchParams.append('contractAddresses[]', contract)
+      )
     options?.pageKey && reqUrl.searchParams.set('pageKey', options.pageKey)
-    options?.withMetadata && reqUrl.searchParams.set('withMetadata', options.withMetadata.toString())
+    options?.pageSize &&
+      reqUrl.searchParams.set('pageSize', '' + options.pageSize)
+    options?.withMetadata &&
+      reqUrl.searchParams.set('withMetadata', options.withMetadata.toString())
 
-    const response = await fetch(reqUrl.toString(), {headers: {'accept': 'application/json'}})
+    reqUrl.searchParams.append('filters[]', 'SPAM')
+
+    // Adding AIRDROPS to filter removes 3iD invites
+    // reqUrl.searchParams.append('filters[]', 'AIRDROPS');
+
+    const response = await fetch(reqUrl.toString(), {
+      headers: { accept: 'application/json' },
+    })
 
     if (response.status !== 200) {
-      throw new Error(`Failed to fetch NFTs with request: ${await response.text()}`)
+      throw new Error(
+        `Failed to fetch NFTs with request: ${await response.text()}`
+      )
     }
 
     return response.json()
