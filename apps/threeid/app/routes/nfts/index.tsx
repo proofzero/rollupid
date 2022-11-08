d33b15f1809ff2cdfbaba673dd4687436fc7e311
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-11-08 18:08:21 +0200

    fix(3id): nft page size and opensea schema

--- apps/threeid/app/routes/nfts/index.tsx
+++ apps/threeid/app/routes/nfts/index.tsx
@@ -6,60 +6,66 @@ export const loader: LoaderFunction = async ({ request }) => {
   const srcUrl = new URL(request.url)
 
   // @ts-ignore
-  if(!ALCHEMY_NFT_API_URL) {
+  if (!ALCHEMY_NFT_API_URL) {
     throw new Error("Make sure 'ALCHEMY_NFT_API_URL' env variable is set.")
   }
-  
+
   const owner = srcUrl.searchParams.get('owner')
   if (!owner) {
     throw new Error('Owner required')
   }
 
   const pageKey = srcUrl.searchParams.get('pageKey')
-  
+
   const alchemy = new AlchemyClient()
-  const res = await alchemy.getNFTsForOwner(owner, {pageKey})
-  const ownedNfts = res.ownedNfts.map(
-    (nft) => {
-      let properties: {
-        name: string
-        value: any
-        display: string
-      }[] = []
+  const res = await alchemy.getNFTsForOwner(owner, { pageKey })
+  const ownedNfts = res.ownedNfts.map((nft) => {
+    let properties: {
+      name: string
+      value: any
+      display: string
+    }[] = []
 
-      // TODO: is this here b/c pfp does not conform to standard?
-      if (nft.metadata.properties) {
-        const validProps = Object.keys(nft.metadata.properties)
-          .filter((k) => typeof nft.metadata.properties[k] !== 'object')
-          .map((k) => ({
-            name: k,
-            value: nft.metadata.properties[k],
-            display: typeof nft.metadata.properties[k],
-          }))
-
-        properties = properties.concat(validProps)
-      }
-
-      if (nft.metadata.attributes?.length) {
-        const mappedAttributes = nft.metadata.attributes.map((a) => ({
-          name: a.trait_type,
-          value: a.value,
-          display: a.display_type || "string", // TODO: @Cosmin this field is not in the alchemy schema. Is it needed at all?
+    // TODO: is this here b/c pfp does not conform to standard?
+    if (nft.metadata.properties) {
+      const validProps = Object.keys(nft.metadata.properties)
+        .filter((k) => typeof nft.metadata.properties[k] !== 'object')
+        .map((k) => ({
+          name: k,
+          value: nft.metadata.properties[k],
+          display: typeof nft.metadata.properties[k],
         }))
-        
-        properties = properties.concat(mappedAttributes)
-      }
 
-      const media = Array.isArray(nft.media) ? nft.media[0] : nft.media
-
-      return {
-        url: gatewayFromIpfs(media.raw),
-        title: nft.title,
-        collectionTitle: nft.contractMetadata?.name,
-        properties,
-      }
+      properties = properties.concat(validProps)
     }
-  )
 
-  return json({ownedNfts})
+    if (nft.metadata.attributes?.length) {
+      const mappedAttributes = nft.metadata.attributes.map((a) => ({
+        name: a.trait_type,
+        value: a.value,
+        display: a.display_type || 'string', // TODO: @Cosmin this field is not in the alchemy schema. Is it needed at all?
+      }))
+
+      properties = properties.concat(mappedAttributes)
+    }
+
+    const media = Array.isArray(nft.media) ? nft.media[0] : nft.media
+
+    let mediaUri = media?.raw
+    if (nft.contractMetadata?.openSeaObject) {
+      mediaUri = nft.contractMetadata.openSeaObject.imageUrl
+    }
+
+    return {
+      url: gatewayFromIpfs(mediaUri),
+      title: nft.title,
+      collectionTitle: nft.contractMetadata?.name,
+      properties,
+    }
+  })
+
+  return json({
+    ownedNfts,
+    pageKey: res.pageKey,
+  })
 }
