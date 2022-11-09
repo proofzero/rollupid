13398cab32656af7f19536027f294b1f3fe69faf
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-11-09 15:16:52 +0200

    Added boldness and color to side-nav menu

--- apps/threeid/app/routes/account.tsx
+++ apps/threeid/app/routes/account.tsx
@@ -145,6 +145,8 @@ const SideNavItem = ({ item }: SideNavItemProps) => {
   const activeStyle = {
     backgroundColor: 'rgb(243 244 246)',
     borderColor: '#6366f1',
+    fontWeight: 600,
+    color: '#1f2937'
   }
   return (
     <div className={'basis-1/4 lg:w-100 content-center self-center z-50'}>
@@ -154,7 +156,7 @@ const SideNavItem = ({ item }: SideNavItemProps) => {
         style={({ isActive }) => {
           return isActive && item.href != '#' ? activeStyle : undefined
         }}
-        className="text-sm group lg:border-l-2 hover:border-indigo-500 px-4 py-4 flex self-center justify-center items-center flex-row lg:justify-start lg:items-start"
+        className="text-sm group lg:border-l-2 hover:border-indigo-500 px-4 py-4 flex self-center justify-center items-center flex-row lg:justify-start lg:items-start hover:font-semibold hover:text-gray-800"
       >
         <item.icon
           className={classNames(
