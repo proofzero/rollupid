f176f2811f5c5d490c3cbdb511ae5c0da1cf03db
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-11-09 16:06:27 +0200

    Rounded sign out container

--- apps/threeid/app/components/head-nav/index.tsx
+++ apps/threeid/app/components/head-nav/index.tsx
@@ -161,7 +161,7 @@ export default function HeadNav({
                         leaveFrom="transform opacity-100 scale-100"
                         leaveTo="transform opacity-0 scale-95"
                       >
-                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none rounded-md">
                           {loggedIn &&
                             userNavigation.map((item) => (
                               <Menu.Item key={item.name}>
@@ -169,7 +169,7 @@ export default function HeadNav({
                                   <item.component
                                     className={classNames(
                                       active ? 'bg-gray-100' : '',
-                                      'block px-4 py-2 text-sm text-gray-700'
+                                      'block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
                                     )}
                                   />
                                 )}
