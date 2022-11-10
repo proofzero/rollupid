4c2c340e6a8fed8b3c2b61cd214c8b4d62506eb0
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-11-10 17:23:04 +0200

    Added edge case for when IPFS links also start with /ipfs

--- apps/threeid/app/helpers/gateway-from-ipfs.ts
+++ apps/threeid/app/helpers/gateway-from-ipfs.ts
@@ -1,9 +1,11 @@
 export const gatewayFromIpfs = (
   ipfsUrl: string | undefined
 ): string | undefined => {
-  if (!ipfsUrl?.startsWith("ipfs://")) return ipfsUrl;
+  if (!ipfsUrl?.startsWith('ipfs://')) return ipfsUrl
 
-  const resLocation = ipfsUrl.replace("ipfs://", "");
+  let resLocation = ipfsUrl.replace('ipfs://', '')
 
-  return `https://nftstorage.link/ipfs/${resLocation}`;
-};
+  return resLocation.startsWith('ipfs')
+    ? `https://nftstorage.link/${resLocation}`
+    : `https://nftstorage.link/ipfs/${resLocation}`
+}
