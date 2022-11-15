e613679bfaa6b2d6ad1a46c166405bcb03016008
Adrian Maurer <adrianmaurer+github@fastmail.com>
2022-11-15 12:10:47 -0500

    wip: private login button

---
+++ apps/passport/stories/molecules/threeid-button/ThreeIdButton.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode, useState, useEffect } from 'react'
+import {
+  Button,
+  ButtonProps,
+} from '@kubelt/design-system/src/atoms/button/Button'
+import classNames from 'classnames'
+
+import threeIdLogo from './3id.svg'
+
+export type ThreeIdButtonProps = {
+  href: string
+} & ButtonProps
+
+export function ThreeIdButton({ href }: ThreeIdButtonProps) {
+  return (
+    <Button
+      onClick={() => {
+        window.location.href = href
+      }}
+      className={'flex flex-row gap-4'}
+      size={'lg'}
+    >
+      <span className={''}>
+        <img src={threeIdLogo} />
+      </span>
+      Private Login
+    </Button>
+  )
+}
