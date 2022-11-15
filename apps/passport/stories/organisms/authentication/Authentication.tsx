eff5ca1d9272bde1b8241eab6c3f054b510f05a4
Adrian Maurer <adrianmaurer+github@fastmail.com>
2022-11-15 11:58:37 -0500

    wip: auth with wallet

--- apps/passport/stories/organisms/authentication/Authentication.tsx
+++ apps/passport/stories/organisms/authentication/Authentication.tsx
@@ -3,17 +3,32 @@ import styles from './authentication.module.scss'
 import circleLogo from './circle-logo.svg'
 
 export type AuthenticationProps = {
-  logoURL: string
+  logoURL?: string
+  connectCallback: (address: string) => void
+  errorCallback: (error: Error) => void
 }
 
-export function Authentication({ logoURL }: AuthenticationProps) {
+export function Authentication({
+  logoURL,
+  connectCallback,
+  errorCallback,
+}: AuthenticationProps) {
   const logo = logoURL || circleLogo
   return (
-    <div className={'flex flex-col items-start gap-6'}>
-      <div className={'justify-center'}>
+    <div className={'flex flex-col items-center gap-4'}>
+      <div className={''}>
         <img className={''} src={logo} alt="3ID Logo" />
       </div>
-      <ConnectButton connectCallback={() => {}} errorCallback={() => {}} />
+      <div className={'flex flex-col items-center gap-2'}>
+        <h1 className={'font-semibold text-xl'}>Welcome to the Private Web</h1>
+        <h2 style={{ color: '#6B7280' }} className={'font-medium text-base'}>
+          How would you like to continue?
+        </h2>
+      </div>
+      <ConnectButton
+        connectCallback={connectCallback}
+        errorCallback={errorCallback}
+      />
     </div>
   )
 }
