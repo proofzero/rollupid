3bc347f4aefd4b6b9c78bf7054e3cbd2262548a2
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-11-16 16:08:49 +0200

    fix(threeid): tailwind-config-relative-modules

--- apps/threeid/tailwind.config.js
+++ apps/threeid/tailwind.config.js
@@ -1,9 +1,30 @@
+const path = require('path')
+
+const getRelativeModulePath = (moduleName) => {
+  const absolutePath = require.resolve(moduleName)
+  const splitPath = absolutePath.split(path.sep)
+
+  let basePath = splitPath[0]
+  for (let i = 1; i < splitPath.length; i++) {
+    basePath = path.join(basePath, splitPath[i])
+    if (splitPath[i] === moduleName) {
+      break
+    }
+  }
+
+  return path.relative(__dirname, basePath)
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './app/**/*.{ts,tsx,jsx,js}',
-    '../../node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-    '../../node_modules/tw-elements/dist/js/**/*.js',
+    path.join(
+      getRelativeModulePath('flowbite-react'),
+      '**',
+      '*.{js,jsx,ts,tsx}'
+    ),
+    path.join(getRelativeModulePath('tw-elements'), 'dist', 'js', '**', '*.js'),
   ],
   theme: {
     extend: {
