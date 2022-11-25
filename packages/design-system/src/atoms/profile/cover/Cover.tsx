d12cd2ec1aa99920d5fae6a7065fd8d9a7f15b80
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-11-25 13:55:03 +0200

    P&L

--- packages/design-system/src/atoms/profile/cover/Cover.tsx
+++ packages/design-system/src/atoms/profile/cover/Cover.tsx
@@ -9,27 +9,34 @@ export type CoverProps = HTMLAttributes<HTMLDivElement> & {
   loaded?: boolean
 }
 
-export const Cover = ({ loaded = true, src, className, children, ...rest }: CoverProps) => {
-  return (<div
-    className={classNames(
-      'h-[300px] w-full relative rounded-b-xl',
-      className
-    )}
-    style={{
-      backgroundImage: src ? `url(${src})` : undefined,
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-    }}
-    {...rest}
-  >
-    {!loaded && (
-      <div className="absolute flex left-0 right-0 top-0 bottom-0 justify-center items-center">
-        <Spinner color="#ffffff" />
-      </div>
-    )}
+export const Cover = ({
+  loaded = true,
+  src,
+  className,
+  children,
+  ...rest
+}: CoverProps) => {
+  return (
+    <div
+      className={classNames(
+        'h-[300px] w-full relative rounded-b-xl',
+        className
+      )}
+      style={{
+        backgroundImage: src ? `url(${src})` : undefined,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+      }}
+      {...rest}
+    >
+      {!loaded && (
+        <div className="absolute flex left-0 right-0 top-0 bottom-0 justify-center items-center">
+          <Spinner color="#ffffff" />
+        </div>
+      )}
 
-    {loaded && children}
-  </div>
+      {loaded && children}
+    </div>
   )
 }
