a3676abaf4005f1ed49de133ace77a67a7cf08d1
Alex Flanagan <alex@kubelt.com>
2022-11-25 11:26:31 -0500

    wip

--- platform/galaxy/src/schema/types/index.ts
+++ platform/galaxy/src/schema/types/index.ts
@@ -1,5 +1,6 @@
 import common from "./common";
 import address from "./address";
 import profile from "./profile";
+import nfts from "./nfts";
 
-export default [common, address, profile];
+export default [common, address, profile, nfts];
