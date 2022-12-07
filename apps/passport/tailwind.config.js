59291b6159f8194d0dfc8c8cdf12ee7352b5a226
Cosmin Pârvulescu <Cosmin-Parvulescu@users.noreply.github.com>
2022-12-07 12:25:45 +0200

    fix(passport): ui regressions (#1143)

--- apps/passport/tailwind.config.js
+++ apps/passport/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{ts,tsx,jsx,js,mdx}'],
+  content: [
+    './app/**/*.{ts,tsx,jsx,js,mdx}',
+    '../../packages/design-system/src/**/*.{ts,tsx,jsx,js}',
+  ],
   theme: {
     extend: {},
   },
