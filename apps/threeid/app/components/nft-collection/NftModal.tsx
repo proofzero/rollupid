22e7ef12835e7185cbfdc9e2457450e5ea8cf3b7
emil <emil.shrft@gmail.com>
2022-12-08 12:50:34 -0500

    feat(threeid): adds accordion to nft galery

--- apps/threeid/app/components/nft-collection/NftModal.tsx
+++ apps/threeid/app/components/nft-collection/NftModal.tsx
@@ -13,7 +13,7 @@ const NftModal = ({
   handleClose: (evt: any) => void
 }) => {
   const [imgLoaded, setImgLoaded] = useState(false)
-
+  console.log(nft)
   return (
     <Modal isOpen={isOpen} handleClose={handleClose}>
       <div className="flex flex-col lg:flex-row max-w-full">
@@ -28,6 +28,7 @@ const NftModal = ({
             }`}
             src={gatewayFromIpfs(nft?.url)}
             onLoad={() => setImgLoaded(true)}
+            alt=""
           />
         </div>
 
@@ -35,7 +36,6 @@ const NftModal = ({
           <Text className="mb-2 text-gray-900" size="lg" weight="medium">
             {nft?.collectionTitle}
           </Text>
-
           <Text
             className="mb-5 lg:mb-10 text-gray-900"
             size="4xl"
@@ -44,41 +44,149 @@ const NftModal = ({
             {nft?.title}
           </Text>
 
-          <hr />
-
-          {nft?.properties?.length > 0 && (
-            <div className="mt-4 lg:mt-8">
-              <Text
-                className="ml-1 mb-4 text-gray-900"
-                size="lg"
-                weight="semibold"
-              >
-                Properties
-              </Text>
-
-              <div className="flex flex-row max-w-md flex-wrap overflow-hidden">
-                {nft?.properties.map(
-                  (p: { name: string; value: any; display: string }) => (
-                    <div
-                      key={p.name ?? `${new Date().getTime()}`}
-                      className="m-1 py-2 px-4 border rounded-md"
+          <div className="accordion accordion-flush" id="accordionDetails">
+            {typeof nft?.details === 'object' &&
+              Object.keys(nft.details).length > 0 && (
+                <div
+                  className="accordion-item 
+                border-t-0 border-l-0 border-r-0 
+                rounded-none bg-white mt-4 lg:mt-8"
+                >
+                  <Text
+                    className="accordion-header ml-1 mb-4 text-gray-900"
+                    size="lg"
+                    weight="semibold"
+                    id="flush-headingOne"
+                  >
+                    <button
+                      className={
+                        'accordion-button relative flex\
+                       items-center w-full py-4\
+                      text-left bg-white border-0 rounded-none\
+                       transition focus:outline-none'
+                      }
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseOne"
+                      aria-expanded="true"
+                      aria-controls="flush-collapseOne"
                     >
-                      <Text size="xs" weight="medium" className="text-gray-400">
-                        {p.name?.toUpperCase()}
-                      </Text>
-                      <Text
-                        size="sm"
-                        weight="semibold"
-                        className="text-gray-700"
-                      >
-                        {p.value}
-                      </Text>
+                      Details
+                    </button>
+                  </Text>
+
+                  <div
+                    id="flush-collapseOne"
+                    className="accordion-collapse border-0 collapse show
+                     flex flex-column max-w-md  overflow-hidden"
+                    aria-labelledby="flush-headingOne"
+                    data-bs-parent="#accordionDetails"
+                  >
+                    <div className="accordion-body">
+                      <div className="keys pr-8 ">
+                        <Text
+                          size="xs"
+                          weight="medium"
+                          className="text-gray-400 pb-2"
+                        >
+                          NFT Contract
+                        </Text>
+
+                        <Text
+                          size="xs"
+                          weight="medium"
+                          className="text-gray-400 pb-2"
+                        >
+                          NFT Standard
+                        </Text>
+                      </div>
+                      <div className="values">
+                        <Text
+                          size="sm"
+                          weight="semibold"
+                          className="text-gray-700 pt-2"
+                        >
+                          {nft.details.contractAddress}
+                          <Text
+                            size="sm"
+                            weight="semibold"
+                            className="text-gray-700 pt-6"
+                          >
+                            {nft.details.nftStandard}
+                          </Text>
+                        </Text>
+                      </div>
                     </div>
-                  )
-                )}
+                  </div>
+                </div>
+              )}
+
+            <hr />
+            {nft?.properties?.length > 0 && (
+              <div
+                className="accordion-item 
+              border-t-0 border-l-0 border-r-0 
+              rounded-none bg-white mt-4 lg:mt-8"
+              >
+                <Text
+                  className="accordion-header 
+                  ml-1 mb-4 text-gray-900"
+                  size="lg"
+                  weight="semibold"
+                >
+                  <button
+                    className={
+                      'accordion-button relative flex\
+                       items-center w-full py-4\
+                      text-left bg-white border-0 rounded-none\
+                       transition focus:outline-none'
+                    }
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseTwo"
+                    aria-expanded="true"
+                    aria-controls="flush-collapseTwo"
+                  >
+                    Properties
+                  </button>
+                </Text>
+
+                <div
+                  id="flush-collapseTwo"
+                  className="accordion-collapse border-0 collapse show
+                     flex flex-column max-w-md  overflow-hidden"
+                  aria-labelledby="flush-headingTwo"
+                  data-bs-parent="#accordionDetails"
+                >
+                  <div className="accordion-body">
+                    {nft?.properties.map(
+                      (p: { name: string; value: any; display: string }) => (
+                        <div
+                          key={p.name ?? `${new Date().getTime()}`}
+                          className="m-1 py-2 px-4 border rounded-md"
+                        >
+                          <Text
+                            size="xs"
+                            weight="medium"
+                            className="text-gray-400"
+                          >
+                            {p.name?.toUpperCase()}
+                          </Text>
+                          <Text
+                            size="sm"
+                            weight="semibold"
+                            className="text-gray-700"
+                          >
+                            {p.value}
+                          </Text>
+                        </div>
+                      )
+                    )}
+                  </div>
+                </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </Modal>
