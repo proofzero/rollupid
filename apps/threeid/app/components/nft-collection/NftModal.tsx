276ecf16d16bd6546162395b24bc4d4a3c7828d4
emil <emil.shrft@gmail.com>
2022-12-09 18:05:26 -0500

    no props/details handlers

--- apps/threeid/app/components/nft-collection/NftModal.tsx
+++ apps/threeid/app/components/nft-collection/NftModal.tsx
@@ -23,6 +23,10 @@ const NftModal = ({
   handleClose: (evt: any) => void
 }) => {
   const [imgLoaded, setImgLoaded] = useState(false)
+  const noProps =
+    nft?.properties?.length > 0 ? '' : 'items-center justify-center text-center'
+  const noDetails =
+    nft?.details?.length > 0 ? '' : 'items-center justify-center text-center'
   return (
     <Modal isOpen={isOpen} handleClose={handleClose}>
       <div
@@ -57,43 +61,43 @@ const NftModal = ({
             >
               {nft?.title}
             </Text>
-            {nft?.details?.length > 0 && (
-              <div
-                className="accordion-item 
+
+            <div
+              className="accordion-item 
                 border-t-0 border-l-0 border-r-0 
                 rounded-none bg-white mt-4 lg:mt-8 mb-4 lg:mb-8"
+            >
+              <Text
+                className="accordion-header ml-1 mb-4 text-gray-900"
+                size="lg"
+                weight="semibold"
+                id="flush-headingOne"
               >
-                <Text
-                  className="accordion-header ml-1 mb-4 text-gray-900"
-                  size="lg"
-                  weight="semibold"
-                  id="flush-headingOne"
-                >
-                  <button
-                    className={
-                      'accordion-button relative collapsed flex\
+                <button
+                  className={
+                    'accordion-button relative collapsed flex\
                        items-center w-full py-4\
                       text-left bg-white border-0 rounded-none\
                        transition focus:outline-none'
-                    }
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#flush-collapseOne"
-                    aria-expanded="true"
-                    aria-controls="flush-collapseOne"
-                  >
-                    <Text className="text-gray-600">Details</Text>
-                  </button>
-                </Text>
-
-                <div
-                  id="flush-collapseOne"
-                  className="accordion-collapse border-0 collapse
-                     flex flex-row max-w-md  overflow-hidden"
-                  aria-labelledby="flush-headingOne"
+                  }
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseOne"
+                  aria-expanded="true"
+                  aria-controls="flush-collapseOne"
                 >
-                  <div className="accordion-body w-[100vw]">
-                    {nft.details.map((d: { name: string; value: any }) => {
+                  <Text className="text-gray-600">Details</Text>
+                </button>
+              </Text>
+
+              <div
+                id="flush-collapseOne"
+                className={`accordion-collapse border-0 collapse flex flex-row max-w-md  overflow-hidden ${noDetails}`}
+                aria-labelledby="flush-headingOne"
+              >
+                <div className="accordion-body w-[100vw]">
+                  {nft?.details?.length ? (
+                    nft.details.map((d: { name: string; value: any }) => {
                       return (
                         <div
                           key={d.name}
@@ -115,48 +119,54 @@ const NftModal = ({
                           </Text>
                         </div>
                       )
-                    })}
-                  </div>
+                    })
+                  ) : (
+                    <div className="accordion-body items-center justify-center text-center">
+                      No details
+                    </div>
+                  )}
                 </div>
               </div>
-            )}
+            </div>
+
             <hr />
-            {nft?.properties?.length > 0 && (
-              <div
-                className="accordion-item 
+
+            <div
+              className="accordion-item 
               border-t-0 border-l-0 border-r-0
               rounded-none bg-white mt-4 lg:mt-8"
-              >
-                <Text
-                  className="accordion-header 
+            >
+              <Text
+                className="accordion-header 
                   ml-1 mb-4 text-gray-900"
-                  size="lg"
-                  weight="semibold"
-                >
-                  <button
-                    className={
-                      'accordion-button relative flex\
+                size="lg"
+                weight="semibold"
+              >
+                <button
+                  className={
+                    'accordion-button relative flex\
                        items-center w-full py-4\
                       text-left bg-white border-0 rounded-none\
                        transition focus:outline-none'
-                    }
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#flush-collapseTwo"
-                    aria-expanded="false"
-                    aria-controls="flush-collapseTwo"
-                  >
-                    <Text className="text-gray-600">Properties</Text>
-                  </button>
-                </Text>
-
-                <div
-                  id="flush-collapseTwo"
-                  className="accordion-collapse border-0 collapse show flex flex-row max-w-md  overflow-hidden"
-                  aria-labelledby="flush-headingTwo"
+                  }
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="flush-collapseTwo"
                 >
-                  <div className="accordion-body flex flex-row">
-                    {nft?.properties.map(
+                  <Text className="text-gray-600">Properties</Text>
+                </button>
+              </Text>
+
+              <div
+                id="flush-collapseTwo"
+                className={`accordion-collapse border-0 collapse show flex flex-row max-w-md  overflow-hidden ${noProps}`}
+                aria-labelledby="flush-headingTwo"
+              >
+                <div className="accordion-body flex flex-row">
+                  {nft?.properties?.length ? (
+                    nft?.properties.map(
                       (p: { name: string; value: any; display: string }) => (
                         <div
                           key={p.name ?? `${new Date().getTime()}`}
@@ -178,11 +188,15 @@ const NftModal = ({
                           </Text>
                         </div>
                       )
-                    )}
-                  </div>
+                    )
+                  ) : (
+                    <div className="accordion-body items-center justify-center text-center">
+                      No properties
+                    </div>
+                  )}
                 </div>
               </div>
-            )}
+            </div>
           </div>
         </div>
       </div>
