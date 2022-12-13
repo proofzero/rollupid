b829c57ca97d177a064eda6bf92194cc81c69f88
Betim Shahini <betimshahini@users.noreply.github.com>
2022-12-13 15:30:46 -0500

    Env var & secret cleanup and standardization for Console app.

--- apps/console/bindings.d.ts
+++ apps/console/bindings.d.ts
@@ -2,7 +2,7 @@ export const seviceBindings = true
 declare global {
   const Starbase: Fetcher
 
-  const SESSION_SECRET: string
+  const SECRET_SESSION_SALT: string
   const COOKIE_DOMAIN: string
   const PASSPORT_URL: string
   const STORAGE_NAMESPACE: string
