7b7d7acda290e645f5dba8d4300d5cc295575a29
betimshahini <betimshahini@users.noreply.github.com>
2022-12-13 18:17:31 -0500

    chore(gha): Env var and secrets cleanup for ThreeID app (#1200)
    * Env var and secrets cleanup for ThreeID app
    
    * Tweaking Alchemy API key env var name for Passport
    
    Co-authored-by: adrian <695698+maurerbot@users.noreply.github.com>

--- apps/passport/app/root.tsx
+++ apps/passport/app/root.tsx
@@ -57,7 +57,7 @@ export const loader: LoaderFunction = () => {
   return json({
     ENV: {
       THREEID_APP_URL,
-      APIKEY_ALCHEMY_THREEID: APIKEY_ALCHEMY_THREEID,
+      APIKEY_ALCHEMY_PUBLIC: APIKEY_ALCHEMY_PUBLIC,
     },
   })
 }
