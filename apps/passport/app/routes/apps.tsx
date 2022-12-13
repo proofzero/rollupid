9be92267df32da1f28af11a79cbb6ac6926f1a28
pillowboy <emil.shrft@gmail.com>
2022-12-13 16:07:20 -0500

    chore(passport): improves ui for buttons (#1201)
    * minor changes in passport
    
    * css for passport buttons

--- apps/passport/app/routes/apps.tsx
+++ apps/passport/app/routes/apps.tsx
@@ -77,7 +77,7 @@ export default function Apps() {
                   <a
                     href={!app.disabled ? app.url : '#'}
                     className={
-                      'flex flex-col-2  min-w-max basis-8/12 md:basis-5/12 md:justify-start md:items-start justify-start items-start gap-4 my-2 p-2 bg-white'
+                      'flex flex-col-2 w-[25vw] min-w-[18rem] justify-start items-start gap-4 my-2 p-2 bg-white'
                     }
                     style={{
                       border: '1px solid #D1D5DB',
