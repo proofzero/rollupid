d19b0e9f5ec2701de87909d0b9c12b147997510a
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-12-13 17:50:46 +0200

    Added menu dropdown

--- apps/console/app/components/Applications/ApplicationListItem.tsx
+++ apps/console/app/components/Applications/ApplicationListItem.tsx
@@ -1,5 +1,9 @@
+import { Menu, Transition } from '@headlessui/react'
 import { Text } from '@kubelt/design-system/src/atoms/text/Text'
+import { Link } from '@remix-run/react'
+import { Fragment } from 'react'
 import { HiDotsVertical } from 'react-icons/hi'
+import { HiCog, HiTrash } from 'react-icons/hi2'
 
 type ApplicationListItemPublishedStateProps = {
   published?: boolean
@@ -40,7 +44,6 @@ export const ApplicationListItem = ({
   created,
   iconUrl,
   published,
-  panel,
 }: ApplicationListItemProps) => (
   <article className="flex justify-center items-center border border-gray-200 shadow-sm rounded bg-white">
     <section>
@@ -60,12 +63,59 @@ export const ApplicationListItem = ({
       </Text>
     </section>
 
-    {panel && (
-      <section className="p-1.5">
-        <div className="w-8 h-8 flex justify-center items-center cursor-pointer">
-          <HiDotsVertical className="text-lg text-gray-400" />
-        </div>
-      </section>
-    )}
+    <section className="p-1.5 relative">
+      {/* Menu could be injected from outside */}
+      <Menu>
+        <Menu.Button>
+          <div className="w-8 h-8 flex justify-center items-center cursor-pointer">
+            <HiDotsVertical className="text-lg text-gray-400" />
+          </div>
+        </Menu.Button>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none divide-y divide-gray-100">
+            <div className="p-1">
+              <Menu.Item
+                as="div"
+                className="py-2 px-4 flex items-center space-x-3 cursor-pointer"
+                onClick={() => {
+                  /* Need to handle */
+                }}
+              >
+                <HiCog className="text-xl font-normal text-gray-400" />
+
+                <Text size="sm" weight="normal" className="text-gray-700">
+                  Settings
+                </Text>
+              </Menu.Item>
+            </div>
+
+            <div className="p-1">
+              <Menu.Item
+                as="div"
+                className="py-2 px-4 flex items-center space-x-3 cursor-pointer"
+                onClick={() => {
+                  /* Need to handle */
+                }}
+              >
+                <HiTrash className="text-xl font-normal text-red-500" />
+
+                <Text size="sm" weight="normal" className="text-red-500">
+                  Delete Application
+                </Text>
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </section>
   </article>
 )
