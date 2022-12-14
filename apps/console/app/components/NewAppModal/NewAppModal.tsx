c3cb166f0c6d2cbc825ce0b47e65831706e38372
Adrian Maurer <adrian@kubelt.com>
2022-12-14 09:02:06 -0500

    feat(console): app create

---
+++ apps/console/app/components/NewAppModal/NewAppModal.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@kubelt/design-system/src/atoms/buttons/Button'
+import { Modal } from '@kubelt/design-system/src/molecules/modal/Modal'
+
+export type NewAppModalProps = {
+  isOpen: boolean
+  newAppCreateCallback: (app: any) => void
+}
+
+export const NewAppModal = ({
+  isOpen,
+  newAppCreateCallback,
+}: NewAppModalProps) => {
+  return (
+    <Modal isOpen={isOpen} fixed handleClose={() => newAppCreateCallback(null)}>
+      <>
+        <h3 className="text-xl font-bold mb-6">New Application</h3>
+
+        <form method="post" action="/app/new">
+          <p>Application Name</p>
+          <input placeholder="My Application" name="client_name"></input>
+          <button onClick={() => newAppCreateCallback(null)}>Cancel</button>
+          <button type="submit">Create Application</button>
+        </form>
+      </>
+    </Modal>
+  )
+}
