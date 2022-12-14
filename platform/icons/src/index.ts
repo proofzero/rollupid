e99a7ea3ffe47ef235c9492cc22743430a5a682d
Adrian Maurer <adrian@kubelt.com>
2022-12-14 15:46:12 -0500

    fix(release): icons and access

--- platform/icons/src/index.ts
+++ platform/icons/src/index.ts
@@ -12,9 +12,9 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import { add as dateAdd, formatRFC3339 as dateFormat } from "date-fns";
+import { add as dateAdd, formatRFC3339 as dateFormat } from 'date-fns'
 
-import invariant from "tiny-invariant";
+import invariant from 'tiny-invariant'
 import checkEnv from '@kubelt/platform.commons/src/utils/checkEnv'
 
 import { required as requiredEnv } from './env'
@@ -29,19 +29,18 @@ export interface Env {
 
   // The Cloudflare account identifier that uploaded images will be
   // associated with.
-  INTERNAL_CLOUDFLARE_ACCOUNT_ID: string;
+  INTERNAL_CLOUDFLARE_ACCOUNT_ID: string
 
   // An API token with write permissions for the Images service.
-  TOKEN_CLOUDFLARE_API: string;
+  TOKEN_CLOUDFLARE_API: string
 
   // Environment variables
   // ---------------------------------------------------------------------------
 
   // Duration in seconds of the upload window, i.e. how long the
   // returned image upload URL can be used to upload an image.
-  UPLOAD_WINDOW_SECONDS: number;
-
-};
+  UPLOAD_WINDOW_SECONDS: number
+}
 
 // Service
 // -----------------------------------------------------------------------------
@@ -54,36 +53,38 @@ export default {
   ): Promise<Response> {
     checkEnv(requiredEnv, env as unknown as Record<string, unknown>)
 
-    const { headers } = request;
-    const contentType = headers.get("content-type") || "";
+    const { headers } = request
+    const contentType = headers.get('content-type') || ''
 
     // If user supplies a JSON object in POST body, use that as the image metadata.
-    let body = {};
-    if (contentType.includes("application/json")) {
-      body = await request.json();
+    let body = {}
+    if (contentType.includes('application/json')) {
+      body = await request.json()
     }
 
     // Arbitrary key/value pairs associated with the image. Can be used
     // for keeping references to another system of record.
-    const metadata = JSON.stringify(body);
+    const metadata = JSON.stringify(body)
 
     // The date after which the upload will not be accepted.
     // - minimum: now + 2 minutes
     // - maximum: now + 6 hours
     // NB: Date.now() is the number of milliseconds since the epoch.
-    const expiryDate = dateAdd(Date.now(), { seconds: env.UPLOAD_WINDOW_SECONDS });
+    const expiryDate = dateAdd(Date.now(), {
+      seconds: env.UPLOAD_WINDOW_SECONDS,
+    })
     // The API expects the expiry time to be an RFC3339-format value.
-    const expiry = dateFormat(expiryDate);
+    const expiry = dateFormat(expiryDate)
 
     // Configuration for the direct_upload Cloudflare API call:
-    const formData = new FormData();
+    const formData = new FormData()
     // Is a signature token required to access the uploaded image?
-    formData.append("requireSignedURLs", false);
-    formData.append("metadata", metadata);
-    formData.append("expiry", expiry);
+    formData.append('requireSignedURLs', false)
+    formData.append('metadata', metadata)
+    formData.append('expiry', expiry)
 
     // URL for "Create authenticated direct upload URL V2" endpoint.
-    const url = `https://api.cloudflare.com/client/v4/accounts/${env.CLOUDFLARE_ACCOUNT_ID}/images/v2/direct_upload`;
+    const url = `https://api.cloudflare.com/client/v4/accounts/${env.TOKEN_CLOUDFLARE_API}/images/v2/direct_upload`
 
     // Direct uploads allow users to upload images without API keys. A
     // common use case are web apps, client-side applications, or mobile
@@ -95,22 +96,22 @@ export default {
     // the draft: true property is not present.
 
     const uploadRequest = new Request(url, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Authorization": `Bearer ${env.TOKEN_CLOUDFLARE_API}`,
+        Authorization: `Bearer ${env.TOKEN_CLOUDFLARE_API}`,
       },
       // NB: do *not* explicitly set the Content-Type header to
       // "multipart/form-data"; this prevents the header from being set
       // with the correct boundary expression used to delimit form
       // fields in the request body.
       body: formData,
-    });
+    })
 
-    const response = await fetch(uploadRequest);
+    const response = await fetch(uploadRequest)
 
     // Check the HTTP status.
     if (!response.ok) {
-      return response;
+      return response
     }
 
     // Example response body:
@@ -123,24 +124,21 @@ export default {
     //     "id": "<image-id>"
     //   }
     // }
-    const direct_upload = await response.json();
+    const direct_upload = await response.json()
 
     if (!direct_upload.success) {
-      return new Response(
-        JSON.stringify(direct_upload), {
-          status: 500,
-        }
-      );
+      return new Response(JSON.stringify(direct_upload), {
+        status: 500,
+      })
     }
 
-    const result = JSON.stringify(direct_upload.result);
+    const result = JSON.stringify(direct_upload.result)
 
     return new Response(result, {
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       status: 200,
-    });
-
+    })
   },
-};
+}
