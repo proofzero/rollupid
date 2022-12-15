a79a712c552a160b305b4c2c745df93d89768dca
Sönmez Kartal <sonmez@kubelt.com>
2022-12-15 13:35:01 +0300

    chore(object): port to `@kubelt/openrpc` (#1236)

---
+++ packages/platform-clients/object.ts
@@ -0,0 +1,22 @@
+import type {
+  GetObjectResult,
+  ObjectValue,
+  PutObjectOptions,
+  PutObjectResult,
+} from '@kubelt/platform.object/src/types'
+
+import type { BaseApi } from './base'
+import createClient from './fetcher'
+
+export interface ObjectApi extends BaseApi {
+  kb_getObject(namespace: string, path: string): Promise<GetObjectResult>
+  kb_putObject(
+    namespace: string,
+    path: string,
+    value: ObjectValue,
+    options: PutObjectOptions
+  ): Promise<PutObjectResult>
+}
+
+export default (fetcher: Fetcher, requestInit: RequestInit | Request = {}) =>
+  createClient<ObjectApi>(fetcher, requestInit)
