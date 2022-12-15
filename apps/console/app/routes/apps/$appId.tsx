c3bbf5defe4a55c3352fba84c7f9126416a58718
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-12-15 13:15:58 +0200

    Added appId layout & dashboard skeletons

---
+++ apps/console/app/routes/apps/$appId.tsx
@@ -0,0 +1,59 @@
+import type { LoaderFunction } from '@remix-run/cloudflare'
+
+import { Outlet, useLoaderData } from '@remix-run/react'
+import { json } from '@remix-run/cloudflare'
+
+import type { Application } from '~/models/app.server'
+import { getApplicationListItems } from '~/models/app.server'
+
+import SiteMenu from '~/components/SiteMenu'
+import SiteHeader from '~/components/SiteHeader'
+
+import { requireJWT } from '~/utilities/session.server'
+import { getStarbaseClient } from '~/utilities/platform.server'
+
+type LoaderData = {
+  apps: Awaited<ReturnType<typeof getApplicationListItems>>
+  appId: string | undefined
+}
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const jwt = await requireJWT(request)
+  const starbaseClient = getStarbaseClient(jwt)
+
+  const appId = params?.appId
+
+  try {
+    const apps = await starbaseClient.kb_appList()
+    console.log({ apps })
+    return json<LoaderData>({ apps, appId })
+  } catch (error) {
+    console.error({ error })
+    return json({ error }, { status: 500 })
+  }
+}
+
+// Component
+// -----------------------------------------------------------------------------
+
+type ContextType = {
+  // The list of a user's applications.
+  apps: Array<Application>
+  appId: string
+}
+
+export default function AppDetailIndexPage() {
+  const { apps, appId } = useLoaderData<ContextType>()
+
+  return (
+    <div className="flex flex-col md:flex-row min-h-full">
+      <SiteMenu apps={apps} selected={appId} />
+
+      <main className="flex flex-col flex-initial min-h-full w-full bg-gray-300">
+        <SiteHeader />
+
+        <Outlet />
+      </main>
+    </div>
+  )
+}
