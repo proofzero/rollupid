1b849bd68ba028c35f027d1a92a8f701b161db19
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-12-21 12:29:37 +0200

    Added rolling key capability to app dashboard

--- apps/console/app/routes/apps/$clientId/index.tsx
+++ apps/console/app/routes/apps/$clientId/index.tsx
@@ -2,8 +2,8 @@
  * @file app/routes/dashboard/apps/$appId/index.tsx
  */
 
-import { json, LoaderFunction } from '@remix-run/cloudflare'
-import { useLoaderData } from '@remix-run/react'
+import { ActionFunction, json, LoaderFunction } from '@remix-run/cloudflare'
+import { useLoaderData, useSubmit } from '@remix-run/react'
 import { ApplicationDashboard } from '~/components/Applications/Dashboard/ApplicationDashboard'
 import { getStarbaseClient } from '~/utilities/platform.server'
 import { requireJWT } from '~/utilities/session.server'
@@ -49,10 +49,38 @@ export const loader: LoaderFunction = async ({ request, params }) => {
   })
 }
 
+export const action: ActionFunction = async ({ request, params }) => {
+  if (!params.clientId) {
+    throw new Error('Application client id is required for the requested route')
+  }
+
+  const jwt = await requireJWT(request)
+  const starbaseClient = getStarbaseClient(jwt)
+
+  const formData = await request.formData()
+  const op = formData.get('op')
+
+  // As part of the rolling operation
+  // we only need to remove the keys
+  // because the loader gets called again
+  // populating the values if empty
+  switch (op) {
+    case 'roll_api_key':
+      break;
+    case 'roll_app_secret':
+      await starbaseClient.kb_appClearSecret(params.clientId)
+      break;
+  }
+
+  return null
+}
+
 // Component
 // -----------------------------------------------------------------------------
 
 export default function AppDetailIndexPage() {
+  const submit = useSubmit()
+
   const { app, rotatedSecret } = useLoaderData()
 
   return (
@@ -65,7 +93,13 @@ export default function AppDetailIndexPage() {
         appId: app.appId,
         appSecret: rotatedSecret,
         createdAt: new Date(),
-        onKeyRoll: () => {},
+        onKeyRoll: () => {
+          submit({
+            op: 'roll_app_secret'
+          }, {
+            method: 'post'
+          })
+        },
       }}
     />
   )
