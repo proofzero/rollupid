22b25fcc1ee5679022c4ea5615c4441be6928fe7
emil <emil.shrft@gmail.com>
2022-12-21 14:29:36 -0500

    tokenRecord type

--- packages/platform-clients/indexer.ts
+++ packages/platform-clients/indexer.ts
@@ -3,13 +3,15 @@ import type { AccountURN } from '@kubelt/urns/account'
 import type { BaseApi } from './base'
 import createClient from './fetcher'
 
+type TokenRecord = {
+  tokenId: string
+  contract: string
+  addressURN: string
+  gallery_order: number
+}
+
 export interface IndexerApi extends BaseApi {
-  kb_setGallery(gallery: {
-    tokenId: string
-    contract: string
-    addressURN: string
-    gallery_order: number
-  }): void
+  kb_setGallery(gallery: TokenRecord[]): void
   kb_getGallery(account: AccountURN[]): object | undefined
 }
 
