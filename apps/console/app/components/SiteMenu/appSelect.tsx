046e41b7b08ad6a457e79ae7be078d999f5765b5
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2022-12-22 12:13:37 +0200

    Hooked in new app modal to the menu button

--- apps/console/app/components/SiteMenu/appSelect.tsx
+++ apps/console/app/components/SiteMenu/appSelect.tsx
@@ -10,6 +10,7 @@ import { ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import { Text } from '@kubelt/design-system/src/atoms/text/Text'
 import { HiGlobeAlt } from 'react-icons/hi'
 import { Button } from '@kubelt/design-system/src/atoms/buttons/Button'
+import { NewAppModal } from '../NewAppModal/NewAppModal'
 
 // Utility
 // -----------------------------------------------------------------------------
@@ -47,6 +48,8 @@ type AppListboxProps = {
 }
 
 function AppListbox({ apps, selectedAppIndex }: AppListboxProps) {
+  const [newAppModalOpen, setNewAppModalOpen] = useState(false)
+
   const [selected] = useState(
     apps.length !== 0
       ? selectedAppIndex < 0
@@ -78,99 +81,111 @@ function AppListbox({ apps, selectedAppIndex }: AppListboxProps) {
   }
 
   return (
-    <Listbox value={selected} onChange={setSelected}>
-      {({ open }) => (
-        <>
-          <div className="relative mt-1">
-            <Listbox.Button className="relative w-full cursor-default border border-l-0 border-r-0 border-gray-700 bg-transparent text-white py-5 pl-4 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none sm:text-sm">
-              <span className="flex items-center">
-                {/* <img src={selected.icon} alt="" className="h-6 w-6 flex-shrink-0 rounded-full" /> */}
-                {selected.clientId === 'none' && (
-                  <HiGlobeAlt
-                    className={`h-6 w-6 ${
-                      apps.length === 0 ? 'text-gray-600' : 'text-gray-300'
-                    } mr-2.5`}
-                  />
-                )}
+    <>
+      <NewAppModal
+        isOpen={newAppModalOpen}
+        newAppCreateCallback={() => {
+          setNewAppModalOpen(false)
+        }}
+      />
+      <Listbox value={selected} onChange={setSelected}>
+        {({ open }) => (
+          <>
+            <div className="relative mt-1">
+              <Listbox.Button className="relative w-full cursor-default border border-l-0 border-r-0 border-gray-700 bg-transparent text-white py-5 pl-4 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none sm:text-sm">
+                <span className="flex items-center">
+                  {/* <img src={selected.icon} alt="" className="h-6 w-6 flex-shrink-0 rounded-full" /> */}
+                  {selected.clientId === 'none' && (
+                    <HiGlobeAlt
+                      className={`h-6 w-6 ${
+                        apps.length === 0 ? 'text-gray-600' : 'text-gray-300'
+                      } mr-2.5`}
+                    />
+                  )}
 
-                {selected.clientId !== 'none' && (
-                  <div className="rounded-full w-6 h-6 flex justify-center items-center bg-gray-200 overflow-hidden mr-2.5">
-                    <Text className="text-gray-500">
-                      {selected.app.title.substring(0, 1)}
-                    </Text>
-                  </div>
-                )}
+                  {selected.clientId !== 'none' && (
+                    <div className="rounded-full w-6 h-6 flex justify-center items-center bg-gray-200 overflow-hidden mr-2.5">
+                      <Text className="text-gray-500">
+                        {selected.app.title.substring(0, 1)}
+                      </Text>
+                    </div>
+                  )}
 
-                <Text
-                  weight="medium"
-                  className={`${
-                    apps.length === 0 ? 'text-gray-600' : 'text-white'
-                  }`}
-                >
-                  {selected.app.title}
-                </Text>
-              </span>
-              <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                <ChevronUpDownIcon
-                  className="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-              </span>
-            </Listbox.Button>
-
-            <Transition
-              show={open && (apps.length > 0 || selected.clientId !== 'none')}
-              as="div"
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-              className="bg-gray-800"
-            >
-              <Listbox.Options className="w-full text-gray-300">
-                <Listbox.Option
-                  className="flex items-center px-4 py-4 border border-l-0 border-r-0 border-t-0 border-gray-700 cursor-pointer hover:bg-gray-700"
-                  value={{
-                    clientId: 'all',
-                  }}
-                >
-                  <HiGlobeAlt className={`h-6 w-6 mr-2.5`} />
-
-                  <Text size="sm" weight="medium">
-                    All Applications
+                  <Text
+                    weight="medium"
+                    className={`${
+                      apps.length === 0 ? 'text-gray-600' : 'text-white'
+                    }`}
+                  >
+                    {selected.app.title}
                   </Text>
-                </Listbox.Option>
+                </span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                  <ChevronUpDownIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </span>
+              </Listbox.Button>
 
-                {apps.map((app) => (
-                  <Listbox.Option key={app.clientId} value={app}>
-                    {({ selected, active }) => (
-                      <>
-                        <div className="flex items-center py-2 px-4 cursor-pointer hover:bg-gray-700">
-                          <div className="rounded-full w-6 h-6 flex justify-center items-center bg-gray-200 overflow-hidden mr-2.5">
-                            <Text className="text-gray-500">
-                              {app.app.title.substring(0, 1)}
+              <Transition
+                show={open && (apps.length > 0 || selected.clientId !== 'none')}
+                as="div"
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+                className="bg-gray-800"
+              >
+                <Listbox.Options className="w-full text-gray-300">
+                  <Listbox.Option
+                    className="flex items-center px-4 py-4 border border-l-0 border-r-0 border-t-0 border-gray-700 cursor-pointer hover:bg-gray-700"
+                    value={{
+                      clientId: 'all',
+                    }}
+                  >
+                    <HiGlobeAlt className={`h-6 w-6 mr-2.5`} />
+
+                    <Text size="sm" weight="medium">
+                      All Applications
+                    </Text>
+                  </Listbox.Option>
+
+                  {apps.map((app) => (
+                    <Listbox.Option key={app.clientId} value={app}>
+                      {({ selected, active }) => (
+                        <>
+                          <div className="flex items-center py-2 px-4 cursor-pointer hover:bg-gray-700">
+                            <div className="rounded-full w-6 h-6 flex justify-center items-center bg-gray-200 overflow-hidden mr-2.5">
+                              <Text className="text-gray-500">
+                                {app.app.title.substring(0, 1)}
+                              </Text>
+                            </div>
+
+                            <Text size="sm" weight="medium">
+                              {app.app.title}
                             </Text>
                           </div>
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
 
-                          <Text size="sm" weight="medium">
-                            {app.app.title}
-                          </Text>
-                        </div>
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-
-              <div className="w-full justify-center border border-l-0 border-r-0 border-gray-700 px-4 py-3">
-                <Button className="w-full" btnType="primary-alt">
-                  Create Application
-                </Button>
-              </div>
-            </Transition>
-          </div>
-        </>
-      )}
-    </Listbox>
+                <div className="w-full justify-center border border-l-0 border-r-0 border-gray-700 px-4 py-3">
+                  <Button
+                    className="w-full"
+                    btnType="primary-alt"
+                    onClick={() => setNewAppModalOpen(true)}
+                  >
+                    Create Application
+                  </Button>
+                </div>
+              </Transition>
+            </div>
+          </>
+        )}
+      </Listbox>
+    </>
   )
 }
 
