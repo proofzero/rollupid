a831783117041043dd3d2bc00dff705c489a3135
Betim Shahini <betimshahini@users.noreply.github.com>
2023-01-04 15:18:51 -0500

    Scope and domains fields are now readonly.

--- apps/console/app/components/Applications/Auth/ApplicationAuth.tsx
+++ apps/console/app/components/Applications/Auth/ApplicationAuth.tsx
@@ -132,12 +132,20 @@ export const ApplicationAuth = ({
             </div>
 
             <div className="flex-1">
-              <Input disabled id="appScopes" label="Scopes" />
+              <ReadOnlyInput id="appScopes" label="Scopes" value="User profile" />
             </div>
           </div>
 
           <div className="my-8 md:my-0">
-            <Input disabled id="appDomains" label="Domain(s)" required />
+            <ReadOnlyInput id="appDomains" label="Domain(s)" value="" required />
+            <Text
+                    type="span"
+                    size="xs"
+                    weight="medium"
+                    className="text-gray-400"
+                    ><a className="text-indigo-500" href="https://discord.gg/threeid">
+                    Contact us
+                  </a> to enable this feature</Text>
           </div>
 
           <div className="flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8 md:items-end">
