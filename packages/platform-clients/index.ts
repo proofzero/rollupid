81c24eaa3f6f80cfc5f927f5c09ae78bcd123878
Adrian Maurer <adrianmaurer+github@fastmail.com>
2023-01-04 18:11:23 -0500

    wip: add edges client

--- packages/platform-clients/index.ts
+++ packages/platform-clients/index.ts
@@ -1,6 +1,7 @@
 import * as access from './access'
 import * as account from './account'
 import * as address from './address'
+import * as edges from './edges'
 import * as starbase from './address'
 import * as ping from './ping'
 import * as ensUtils from './ens-utils'
@@ -11,6 +12,7 @@ export {
   access,
   account,
   address,
+  edges,
   starbase,
   ping,
   ensUtils,
--- packages/types/router.ts
+++ packages/types/router.ts
@@ -1,9 +1,12 @@
 import { appRouter as accountRouter } from '@kubelt/platform/account/src/jsonrpc/router'
 import { appRouter as accessRouter } from '@kubelt/platform/access/src/jsonrpc/router'
+import { appRouter as edgesRouter } from '@kubelt/platform/edges/src/jsonrpc/router'
 import { appRouter as pingRouter } from '@kubelt/platform/ping/src/jsonrpc/router'
 
 export type AccountRouter = typeof accountRouter
 
 export type AccessRouter = typeof accessRouter
+
+export type EdgesRouter = typeof edgesRouter
 
 export type PingRouter = typeof pingRouter
