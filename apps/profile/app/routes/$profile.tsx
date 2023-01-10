2e2715aa2e2d354e196daffb3c55e36399aa5915
Emil <emil@kubelt.com>
2023-01-10 17:42:38 -0500

    chore(profile): diplays links on

--- apps/profile/app/routes/$profile.tsx
+++ apps/profile/app/routes/$profile.tsx
@@ -452,20 +452,24 @@ const ProfileLayout = () => {
               </div>
               {links && (
                 <div
-                  className="flex flex-col lg:flex-row lg:space-x-2
+                  className="flex flex-col 
+                  sm:flex-row sm:flex-wrap
                 justify-start lg:items-center text-gray-500 font-size-lg"
                 >
                   {links.map((link: any, i: number) => (
                     <button
                       key={`${link.name}-${link.url}-${i}`}
-                      className="bg-gray-300 rounded-full
-                    text-gray-[#4b5563]
-                    flex
-                    justify-center
-                    items-center
-                    mt-[1.625rem]
-                    mr-[1rem]
-                    w-[131px] h-[40px]"
+                      className="
+                      bg-gray-100 hover:bg-gray-200
+                      transition-colors
+                      rounded-full
+                      text-gray-[#4b5563]
+                      flex
+                      justify-center
+                      items-center
+                      mt-[1.625rem] sm:mr-[16px]
+                      w-full sm:w-[131px] 
+                      h-[40px]"
                     >
                       <a href={link.url} className="flex flex-row items-center">
                         {link.verified && (
