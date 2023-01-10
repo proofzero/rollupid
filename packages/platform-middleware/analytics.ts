2a7c83e5added56c0754ebb6e5e817071dbb07cb
Alex Flanagan <alex@kubelt.com>
2023-01-10 16:45:55 -0500

    Hook up analytics events.

--- packages/platform-middleware/analytics.ts
+++ packages/platform-middleware/analytics.ts
@@ -6,19 +6,34 @@ export const Analytics: BaseMiddlewareFunction<{
   req?: Request
   accountURN?: AccountURN
 }> = async ({ ctx, path, type, next }) => {
-  const rayId = ctx.req?.headers.get('Ray-ID')
+  const rayId = ctx.req?.headers.get('Ray-ID') || null
+  // if (!rayId) throw new Error('No Ray-ID found in request headers')
 
-  if (!rayId) throw new Error('No Ray-ID found in request headers')
+  const accountURN: string = ctx.accountURN || 'no account URN'
 
-  // analytics?.writeDataPoint({ blobs, doubles, indexes })
+  // Pre-method call analytics.
+  const pre: AnalyticsEngineDataPoint = {
+    blobs: [ path, type, 'BEFORE', accountURN, rayId ],
+    // doubles: [],
+    indexes: [rayId],
+  }
 
-  // post your pre method call analytics
+  console.log('service precall analytics', JSON.stringify(pre))
+  ctx.Analytics?.writeDataPoint(pre)
 
   const result = await next({
     ctx,
   })
 
-  // post your post method call analytics
+  // Post-method call analytics.
+  const post: AnalyticsEngineDataPoint = {
+    blobs: [ path, type, 'AFTER', accountURN, rayId ],
+    // doubles: [],
+    indexes: [rayId],
+  }
+
+  console.log('service postcall analytics', JSON.stringify(post))
+  ctx.Analytics?.writeDataPoint(post)
 
   return result
 }
