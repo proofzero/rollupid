3d0e405f61695940aab5a5037eb66cbf66a75a25
Adrian <695698+maurerbot@users.noreply.github.com>
2023-01-11 13:42:51 -0500

    fix(clients): indexeR (#1388)
    * fix(clients): indexeR
    
    * wip

--- apps/profile/app/helpers/clients.ts
+++ apps/profile/app/helpers/clients.ts
@@ -1,6 +1,7 @@
 import { GraphQLClient } from 'graphql-request'
 import { getSdk } from '@kubelt/galaxy-client'
 import createAddressClient from '@kubelt/platform-clients/address'
+import createIndexerClient from '@kubelt/platform-clients/indexer'
 
 export async function getGalaxyClient() {
   const gqlClient = new GraphQLClient('http://127.0.0.1', {
@@ -14,4 +15,10 @@ export function getCryptoAddressClient(
   options: RequestInit<RequestInitCfProperties>
 ) {
   return createAddressClient(Address, options)
+}
+
+export function getIndexerClient(
+  options?: RequestInit<RequestInitCfProperties>
+) {
+  return createIndexerClient(Indexer, options)
 }
