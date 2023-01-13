236dcc95de31e1b8906852d67dbe59b762b67d83
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-01-13 13:17:12 +0200

    chore(ui): generic sortable list

--- packages/design-system/src/atoms/lists/SortableList.tsx
+++ packages/design-system/src/atoms/lists/SortableList.tsx
@@ -1,21 +1,102 @@
+import {
+  closestCenter,
+  DndContext,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core'
+import {
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
 import React from 'react'
 import { ReactNode } from 'react'
+import { CSS } from '@dnd-kit/utilities'
+import { RxDragHandleDots2 } from 'react-icons/rx'
 
 export type SortableListProps = {
-  items: ReactNode[]
+  items: string[]
+}
+
+const SortableListItem = ({ id, ...rest }) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  }
+
+  return (
+    <div
+      className={`
+               border border-gray-300 rounded-md
+                px-4 py-3 mb-3 truncate bg-white
+                flex flex-row items-center justify-between
+                ${isDragging ? 'shadow-inner z-100' : ''}
+                 `}
+      ref={setNodeRef}
+      style={style}
+    >
+      <div className={`flex flex-row items-center w-full truncate`}>
+        <button
+          className="text-gray-400"
+          type="button"
+          {...attributes}
+          {...listeners}
+        >
+          <RxDragHandleDots2 size={22} className="mr-[14px]" />{' '}
+        </button>
+
+        <div>Fubar</div>
+      </div>
+    </div>
+  )
 }
 
 export const SortableList = ({ items }: SortableListProps) => {
+  const sensors = useSensors(
+    useSensor(MouseSensor),
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  )
+
+  const handleDragCancel = () => {}
+
+  const handleDragStart = ({ active }: { active: any }) => {}
+
+  const handleDragEnd = (event: any) => {}
+
   return (
-    <section className="flex flex-col space-y-2">
-      {items.map((i, index) => (
-        <article
-          key={index}
-          className="flex justify-center items-center border border-gray-200 shadow-sm rounded bg-white p-4 w-full"
+    <section className="flex flex-col">
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragStart={handleDragStart}
+        onDragCancel={handleDragCancel}
+        onDragEnd={handleDragEnd}
+      >
+        <SortableContext
+          items={items.map((item, index) => item)}
+          strategy={verticalListSortingStrategy}
         >
-          {i}
-        </article>
-      ))}
+          {items.map((i) => (
+            <SortableListItem key={i} id={i} />
+          ))}
+        </SortableContext>
+      </DndContext>
     </section>
   )
 }
