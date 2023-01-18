2a246ddc22b90e931fc501a1051c831d32b4295d
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-01-18 17:43:48 +0200

    Mapped address profile to address list item props

--- apps/profile/app/routes/account/settings/connections.tsx
+++ apps/profile/app/routes/account/settings/connections.tsx
@@ -1,32 +1,62 @@
 import { Button } from '@kubelt/design-system/src/atoms/buttons/Button'
 import { Text } from '@kubelt/design-system/src/atoms/text/Text'
 import { AddressList } from '~/components/addresses/AddressList'
-import { useOutletContext } from '@remix-run/react'
+import { useLoaderData } from '@remix-run/react'
+import { CryptoAddressType, NodeType } from '@kubelt/types/address'
+import { LoaderFunction } from 'react-router-dom'
+import { getAccountAddresses, getAddressProfile } from '~/helpers/profile'
+import { requireJWT } from '~/utils/session.server'
+import { AddressURN } from '@kubelt/urns/address'
 import { AddressListItemProps } from '~/components/addresses/AddressListItem'
-import { Node } from '@kubelt/galaxy-client'
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const jwt = await requireJWT(request)
+
+  const addresses = await getAccountAddresses(jwt)
+  const cryptoAddresses =
+    addresses?.filter((e) => {
+      if (!e.rc) return
+
+      return e?.rc?.node_type === NodeType.Crypto
+    }) || []
+
+  const addressProfiles: AddressListItemProps[] = await Promise.all(
+    cryptoAddresses.map(async (cryptoAddress) => {
+      const profile = await getAddressProfile(
+        jwt,
+        cryptoAddress.urn as AddressURN
+      )
+
+      const rparams = new URLSearchParams(cryptoAddress.rc || '')
+      const qparams = new URLSearchParams(cryptoAddress.qc || '')
+
+      const addrType = rparams.get('addr_type')
+      const alias = qparams.get('alias')
+
+      let chain = undefined
+      switch (addrType) {
+        case CryptoAddressType.ETH:
+          chain = 'Ethereum'
+      }
+
+      return {
+        id: cryptoAddress.urn,
+        type: addrType as string,
+        address: alias as string,
+        chain: chain as string,
+        title: alias as string,
+        icon: profile?.pfp?.image as string,
+      }
+    })
+  )
+
+  return { addressProfiles }
+}
 
 const AccountSettingsConnections = () => {
-  const { addresses } = useOutletContext<{
-    addresses: Node[]
+  const { addressProfiles } = useLoaderData<{
+    addressProfiles: AddressListItemProps[]
   }>()
-
-  const mappedAddresses: AddressListItemProps[] = addresses.map((node) => {
-    const rparams = new URLSearchParams(node.rc || '')
-    const qparams = new URLSearchParams(node.qc || '')
-    const addrType = rparams.get('addr_type')
-    const alias = qparams.get('alias')
-
-    return {
-      id: node.urn,
-      type: addrType,
-      address: alias as string,
-      chain: 'Foo',
-      network: 'Bar',
-      title: 'Biz',
-      wallet: 'Baz',
-      icon: 'https://picsum.photos/256',
-    }
-  })
 
   return (
     <section>
@@ -39,7 +69,7 @@ const AccountSettingsConnections = () => {
           CONNECTED ACCOUNTS
         </Text>
 
-        <AddressList addresses={mappedAddresses} />
+        <AddressList addresses={addressProfiles} />
       </div>
     </section>
   )
