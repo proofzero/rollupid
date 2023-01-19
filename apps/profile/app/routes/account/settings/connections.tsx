3e88c3e8bb7571e6cde74f62ee86894a40490943
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-01-19 18:57:17 +0200

    Added dirty address list mechanism. To refactor.

--- apps/profile/app/routes/account/settings/connections.tsx
+++ apps/profile/app/routes/account/settings/connections.tsx
@@ -2,61 +2,35 @@ import { Button } from '@kubelt/design-system/src/atoms/buttons/Button'
 import { Text } from '@kubelt/design-system/src/atoms/text/Text'
 import { AddressList } from '~/components/addresses/AddressList'
 import { useLoaderData } from '@remix-run/react'
-import { CryptoAddressType, NodeType } from '@kubelt/types/address'
-import { LoaderFunction } from 'react-router-dom'
-import { getAccountAddresses, getAddressProfile } from '~/helpers/profile'
+import { getAccountAddresses, getAddressProfiles } from '~/helpers/profile'
 import { requireJWT } from '~/utils/session.server'
 import { AddressURN } from '@kubelt/urns/address'
 import { AddressListItemProps } from '~/components/addresses/AddressListItem'
+import { LoaderFunction } from '@remix-run/cloudflare'
+import { CryptoAddressProfile } from '@kubelt/galaxy-client'
 
 export const loader: LoaderFunction = async ({ request }) => {
   const jwt = await requireJWT(request)
 
-  const addresses = await getAccountAddresses(jwt)
-  const cryptoAddresses =
-    addresses?.filter((e) => {
-      if (!e.rc) return
+  const addresses = (await getAccountAddresses(jwt)) ?? []
+  const addressUrns = addresses.map((ca) => ca.urn as AddressURN)
+  const profiles = (await getAddressProfiles(jwt, addressUrns)) ?? []
 
-      return e?.rc?.node_type === NodeType.Crypto
-    }) || []
-
-  const addressProfiles: AddressListItemProps[] = await Promise.all(
-    cryptoAddresses.map(async (cryptoAddress) => {
-      const profile = await getAddressProfile(
-        jwt,
-        cryptoAddress.urn as AddressURN
-      )
-
-      const rparams = new URLSearchParams(cryptoAddress.rc || '')
-      const qparams = new URLSearchParams(cryptoAddress.qc || '')
-
-      const addrType = rparams.get('addr_type')
-      const alias = qparams.get('alias')
-
-      let chain = undefined
-      switch (addrType) {
-        case CryptoAddressType.ETH:
-          chain = 'Ethereum'
-      }
-
-      return {
-        id: cryptoAddress.urn,
-        type: addrType as string,
-        address: alias as string,
-        chain: chain as string,
-        title: alias as string,
-        icon: profile?.pfp?.image as string,
-      }
-    })
-  )
+  const addressProfiles: AddressListItemProps[] = profiles
+    ?.filter((p) => p?.__typename === 'CryptoAddressProfile')
+    .map((p) => ({
+      id: (p as CryptoAddressProfile).address,
+      address: (p as CryptoAddressProfile).address,
+      title: (p as CryptoAddressProfile).displayName as string,
+      icon: (p as CryptoAddressProfile).avatar as string,
+      chain: 'Ethereum',
+    }))
 
   return { addressProfiles }
 }
 
 const AccountSettingsConnections = () => {
-  const { addressProfiles } = useLoaderData<{
-    addressProfiles: AddressListItemProps[]
-  }>()
+  const { addressProfiles } = useLoaderData()
 
   return (
     <section>
