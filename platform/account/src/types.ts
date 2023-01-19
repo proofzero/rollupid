b7ac39c5af103921001a257a414c0d4cdfc14ef0
Alex Flanagan <alex@kubelt.com>
2023-01-19 13:47:51 -0500

    Add deployment metadata to account.

--- platform/account/src/types.ts
+++ platform/account/src/types.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod'
 import { AddressListSchema } from './jsonrpc/validators/addressList'
 import { ProfileSchema } from './jsonrpc/validators/profile'
+import { DeploymentMetadata } from '@kubelt/types'
 
 export interface Environment {
   Account: DurableObjectNamespace
   Edges: Fetcher
   Analytics: AnalyticsEngineDataset
+  ServiceDeploymentMetadata: DeploymentMetadata
 }
 
 export type AddressList = z.infer<typeof AddressListSchema>
