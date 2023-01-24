e7b80f24a59cb60926991b91d46b09ffdf2ef75c
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-01-24 16:58:04 +0200

    Improved rename address modal

--- apps/profile/app/routes/account/settings/connections.tsx
+++ apps/profile/app/routes/account/settings/connections.tsx
@@ -9,7 +9,7 @@ import { AddressListItemProps } from '~/components/addresses/AddressListItem'
 import { LoaderFunction } from '@remix-run/cloudflare'
 import { CryptoAddressProfile } from '@kubelt/galaxy-client'
 import { Modal } from '@kubelt/design-system/src/molecules/modal/Modal'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import InputText from '~/components/inputs/InputText'
 import { CryptoAddressType } from '@kubelt/types/address'
 
@@ -67,6 +67,15 @@ const AccountSettingsConnections = () => {
 
   const [renameModalOpen, setRenameModalOpen] = useState(false)
   const [actionId, setActionId] = useState<null | string>()
+  const [actionProfile, setActionProfile] = useState<any>()
+
+  useEffect(() => {
+    const selectedProfile = cryptoProfiles
+      .concat(oAuthProfiles)
+      .find((p: any) => p.id === actionId)
+
+    setActionProfile(selectedProfile)
+  }, [actionId])
 
   const submit = useSubmit()
 
@@ -81,25 +90,56 @@ const AccountSettingsConnections = () => {
           CONNECTED ACCOUNTS
         </Text>
 
-        <Modal isOpen={renameModalOpen}>
-          <Form
-            method="post"
-            action="/account/settings/connections/rename"
-            onSubmit={(e) => {
-              setRenameModalOpen(false)
-
-              const targetForm = e.currentTarget
-              submit(targetForm)
-            }}
+        <Modal
+          isOpen={renameModalOpen}
+          handleClose={() => setRenameModalOpen(false)}
+        >
+          <div
+            className={`min-w-[437px] relative transform rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:p-6 overflow-y-auto`}
           >
-            {actionId && <input type="hidden" name="id" value={actionId} />}
+            <Text size="lg" weight="semibold" className="text-gray-900 mb-8">
+              Name Your Account
+            </Text>
 
-            <InputText heading="name" name="name" />
+            <Form
+              method="post"
+              action="/account/settings/connections/rename"
+              onSubmit={(e) => {
+                const targetForm = e.currentTarget
+                submit(targetForm)
 
-            <Button type="submit" btnType="primary-alt">
-              Rename
-            </Button>
-          </Form>
+                setRenameModalOpen(false)
+                setActionId(undefined)
+              }}
+            >
+              {actionId && <input type="hidden" name="id" value={actionId} />}
+
+              <InputText
+                required
+                heading=""
+                name="name"
+                defaultValue={actionProfile?.title ?? ''}
+              />
+              {actionProfile?.address && (
+                <Text size="xs" weight="normal" className="text-gray-500 mt-2">
+                  {actionProfile?.address}
+                </Text>
+              )}
+
+              <div className="flex justify-end items-center space-x-3 mt-20">
+                <Button
+                  btnType="secondary-alt"
+                  onClick={() => setRenameModalOpen(false)}
+                >
+                  Cancel
+                </Button>
+
+                <Button type="submit" btnType="primary">
+                  Save
+                </Button>
+              </div>
+            </Form>
+          </div>
         </Modal>
 
         <AddressList
