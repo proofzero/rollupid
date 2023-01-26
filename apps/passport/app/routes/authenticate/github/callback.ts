813ee58a5076c6741a50d482140b2e313c8b228c
Sönmez Kartal <sonmez@kubelt.com>
2023-01-26 19:41:42 +0300

    chore(passport): remove excessive logs (#1480)

--- apps/passport/app/routes/authenticate/github/callback.ts
+++ apps/passport/app/routes/authenticate/github/callback.ts
@@ -18,7 +18,7 @@ export const loader: LoaderFunction = async ({
   const appData = await getConsoleParamsSession(request, context.env)
     .then((session) => JSON.parse(session.get('params')))
     .catch((err) => {
-      console.log('No console params session found', err)
+      console.log('No console params session found')
       return null
     })
 
