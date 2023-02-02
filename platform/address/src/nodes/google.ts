ee39ef8534b164dd2daee7d49826046630b7d150
Sönmez Kartal <sonmez@kubelt.com>
2023-02-02 23:15:37 +0300

    feat(address): oauth refresh token

--- platform/address/src/nodes/google.ts
+++ platform/address/src/nodes/google.ts
@@ -1,14 +1,33 @@
 import { DurableObjectStubProxy } from 'do-proxy'
+
+import type { Context } from '../context'
 import { OAuthData, OAuthGoogleProfile } from '../types'
+
 import Address from './address'
 import OAuthAddress from './oauth'
+import type { AddressNode } from '.'
+
+const TOKEN_URL = 'https://oauth2.googleapis.com/token'
 
 export default class GoogleAddress extends OAuthAddress {
+  declare clientId: string
+  declare clientSecret: string
+
+  constructor(node: AddressNode, ctx: Context) {
+    super(node)
+    this.clientId = ctx.INTERNAL_GOOGLE_OAUTH_CLIENT_ID
+    this.clientSecret = ctx.SECRET_GOOGLE_OAUTH_CLIENT_SECRET
+  }
+
   async getProfile(): Promise<OAuthGoogleProfile> {
     const data = await this.getData()
     if (!data) throw new Error('no data')
     const profile = data.profile as OAuthData['profile']
     return profile._json as OAuthGoogleProfile
+  }
+
+  getTokenURL(): string {
+    return TOKEN_URL
   }
 
   static async alarm(address: Address) {
