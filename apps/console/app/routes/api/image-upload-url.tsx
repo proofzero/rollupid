f94b11225897f781ea450da84534fbacaddb2dd8
Emil <emil@kubelt.com>
2023-02-02 16:26:43 -0500

    fix(console): jwt check back

--- apps/console/app/routes/api/image-upload-url.tsx
+++ apps/console/app/routes/api/image-upload-url.tsx
@@ -1,8 +1,10 @@
 import type { ActionFunction } from '@remix-run/cloudflare'
 import { json } from '@remix-run/cloudflare'
 import createImageClient from '@kubelt/platform-clients/image'
+import { requireJWT } from '~/utilities/session.server'
 
 export const action: ActionFunction = async ({ request }) => {
+  await requireJWT(request)
   const imageClient = createImageClient(Images)
 
   const { uploadURL } = await imageClient.upload.mutate()
