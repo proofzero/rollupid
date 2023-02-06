851eb2a4eb772ca37f28fce42754f3015d76802a
Adrian Maurer <adrian@kubelt.com>
2023-02-06 13:58:01 -0500

    fix: rm galaxy access methods

--- platform/galaxy/src/schema/types/index.ts
+++ platform/galaxy/src/schema/types/index.ts
@@ -1,8 +1,7 @@
 import common from './common'
 import edges from './edges'
 import address from './address'
-import access from './access'
 import account from './account'
 import nfts from './nfts'
 
-export default [common, edges, address, access, account, nfts]
+export default [common, edges, address, account, nfts]
