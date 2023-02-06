c8488d408748eba14bde90edc0b75c36656025bf
Emil <emil@kubelt.com>
2023-02-06 13:27:20 -0500

    fix(profile): upload OG image

--- apps/profile/app/routes/account/settings/profile/image-upload-url.tsx
+++ apps/profile/app/routes/account/settings/profile/image-upload-url.tsx
@@ -1,13 +1,19 @@
-import { ActionFunction, json, redirect } from '@remix-run/cloudflare'
+import type { ActionFunction, LoaderFunction } from '@remix-run/cloudflare'
+import { json, redirect } from '@remix-run/cloudflare'
+import { requireJWT } from '~/utils/session.server'
 
-import CFImageUploadClient from '@kubelt/platform-clients/cf-image-upload'
+import createImageClient from '@kubelt/platform-clients/image'
 
-export const loader = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireJWT(request)
   return redirect('/account/settings/profile')
 }
 
-export const action: ActionFunction = async () => {
-  const imageClient = new CFImageUploadClient(Images)
-  const imageUploadUrl = await imageClient.getImageUploadUrl()
-  return json(imageUploadUrl)
+export const action: ActionFunction = async ({ request }) => {
+  await requireJWT(request)
+
+  const imageClient = createImageClient(Images)
+  const { uploadURL } = await imageClient.upload.mutate()
+  console.log({ uploadURL })
+  return json(uploadURL)
 }
