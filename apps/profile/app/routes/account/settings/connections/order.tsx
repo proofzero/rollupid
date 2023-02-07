657c57bd16894fff5559de28a212c39168586918
Adrian Maurer <adrianmaurer+github@fastmail.com>
2023-02-07 13:33:32 -0500

    wip: prepare for update

---
+++ apps/profile/app/routes/account/settings/connections/order.tsx
@@ -0,0 +1,29 @@
+import { AddressURN } from '@kubelt/urns/address'
+import { getAuthzHeaderConditionallyFromToken } from '@kubelt/utils'
+import { ActionFunction } from '@remix-run/cloudflare'
+import { getGalaxyClient } from '~/helpers/clients'
+import { requireJWT } from '~/utils/session.server'
+
+export const action: ActionFunction = async ({ request }) => {
+  const jwt = await requireJWT(request)
+
+  const formData = await request.formData()
+
+  const connectionsText = formData.get('connections')
+  const connections = JSON.parse(connectionsText as string) as {
+    id: AddressURN
+    public?: boolean
+  }[]
+  console.debug({ connections })
+
+  // const galaxyClient = await getGalaxyClient()
+  // galaxyClient.updateAddressNickname(
+  //   {
+  //     addressURN: id,
+  //     nickname: name,
+  //   },
+  //   getAuthzHeaderConditionallyFromToken(jwt)
+  // )
+
+  return null
+}
