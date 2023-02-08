86b6b5f199a5761108051a535646d1fdbd91b86a
Adrian Maurer <adrian@kubelt.com>
2023-02-08 14:48:45 -0500

    wip: grant type

--- packages/urns/access.ts
+++ packages/urns/access.ts
@@ -1,8 +1,9 @@
 import { createRollupIdURNSpace, RollupIdURN } from './index'
+import { GrantType } from '@kubelt/types/access'
 
 export type AccessRComp = {
   client_id?: string
-  grant_type?: string
+  grant_type?: GrantType
 }
 export type AccessURN = RollupIdURN<`access/${string}`>
 export const AccessURNSpace = createRollupIdURNSpace<
