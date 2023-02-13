125e62b776fba0d03ce2ae66b2e879c786353bd2
Emil <emil@kubelt.com>
2023-02-13 10:51:50 -0500

    chore(links): anchor

--- apps/profile/app/routes/account/settings/links.tsx
+++ apps/profile/app/routes/account/settings/links.tsx
@@ -200,20 +200,15 @@ const SortableLink = ({
         <input type="hidden" name={`links[${id}][name]`} value={nameInput} />
         <input type="hidden" name={`links[${id}][url]`} value={urlInput} />
 
-        <Tooltip content="Copy" className="text-black">
-          <button
-            type="button"
-            className="bg-gray-100 hover:bg-gray-200 transition-colors
+        <a
+          href={urlInput}
+          className="bg-gray-100 hover:bg-gray-200 transition-colors
               w-[2.25rem] h-[2.25rem] mr-[14px] rounded-full
               text-gray-700
         flex items-center justify-center "
-            onClick={() => {
-              navigator.clipboard.writeText(urlInput)
-            }}
-          >
-            <TbLink size={22} />
-          </button>
-        </Tooltip>
+        >
+          <TbLink size={22} />
+        </a>
         <div className="flex flex-col flex-1">
           <Text weight="medium" className="truncate">
             {name}
