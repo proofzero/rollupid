68f3772efaf5869c76ee99ef02dca2eb89a9678a
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-02-14 15:51:46 +0200

    Renamed cookie as per the issue

--- apps/passport/app/cookies/connect.ts
+++ apps/passport/app/cookies/connect.ts
@@ -1,9 +1,10 @@
 import { createCookie } from '@remix-run/cloudflare'
 
-export const connect = createCookie('connect', {
+export const connect = createCookie('PASSPORT_CONNECT_ACCOUNT', {
+  // Is 2 minutes a good max age?
   maxAge: 120,
 })
 
-export const clearConnect = createCookie('connect', {
+export const clearConnect = createCookie('PASSPORT_CONNECT_ACCOUNT', {
   maxAge: 0,
 })
