8e664742297b6edbd018ce12bc145cf53f5eac0e
Emil <emil@kubelt.com>
2023-02-14 14:38:47 -0500

    chore(passport): avoid img existence check

--- apps/passport/app/utils/cacheImageToCF.server.ts
+++ apps/passport/app/utils/cacheImageToCF.server.ts
@@ -11,50 +11,32 @@ export default async (
   })
   const retrievedImage = await retrieveImageReq
 
-  if (!retrievedImage.ok) {
-    if (retrievedImage.status === 404) {
-      //Valid error
-      console.error('No image found in at that URL.')
-      return ''
-    } else {
-      console.error(
-        'Error retrieving the image from URL.',
-        retrievedImage.statusText
-      )
-      throw new Error('Error retrieving the image from URL')
-    }
-  }
-
-  const imgFile = await retrievedImage.blob()
-
-  const imageClient = createImageClient(env.Images)
-  const { uploadURL } = await imageClient.upload.mutate()
-
-  const formData = new FormData()
-  formData.append('file', imgFile)
-  let cfUploadRes: {
-    success: boolean
-    result: {
-      variants: string[]
-    }
-  } | null = null
-
   try {
-    cfUploadRes = await fetch(uploadURL, {
+    const imgFile = await retrievedImage.blob()
+
+    const imageClient = createImageClient(env.Images)
+    const { uploadURL } = await imageClient.upload.mutate()
+
+    const formData = new FormData()
+    formData.append('file', imgFile)
+
+    const cfUploadRes: {
+      success: boolean
+      result: {
+        variants: string[]
+      }
+    } = await fetch(uploadURL, {
       method: 'POST',
       body: formData,
     }).then((res) => res.json())
 
-    if (!cfUploadRes?.success || !cfUploadRes.result?.variants)
-      throw new Error('Upload failed')
+    const imageURL = cfUploadRes.result.variants[0]
+
+    if (!imageURL || !imageURL.length) throw new Error('Could not cache image.')
+
+    return imageURL
   } catch (ex) {
     console.error('Could not send upload image blob to Image cache.', ex)
     return ''
   }
-
-  const imageURL = cfUploadRes?.result.variants[0]
-
-  if (!imageURL || !imageURL.length) throw new Error('Could not cache image.')
-
-  return imageURL
 }
