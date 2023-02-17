5bf5abdd217c2f0d5603305838e1fb269255b437
pillowboy <emil@kubelt.com>
2023-02-16 19:13:40 -0500

    fix(galaxy): double apply urn prefix (#1697)
    * fix(galaxy): double apply urn prefix
    
    * wip
    
    * wip

--- platform/galaxy/src/schema/resolvers/nfts.ts
+++ platform/galaxy/src/schema/resolvers/nfts.ts
@@ -56,7 +56,7 @@ const nftsResolvers: Resolvers = {
       )
       let ownedNfts: any[] = []
 
-      const accountURN = AccountURNSpace.componentizedUrn(owner)
+      const accountURN = owner as AccountURN
 
       const addresses = (
         (await getConnectedAddresses({
