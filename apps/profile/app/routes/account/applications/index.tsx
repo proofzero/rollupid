5628b0cdc5af29fc73914c7d98b97371ed1d98ed
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-02-21 11:16:50 +0200

    Added modal

--- apps/profile/app/routes/account/applications/index.tsx
+++ apps/profile/app/routes/account/applications/index.tsx
@@ -2,8 +2,50 @@ import { useLoaderData } from '@remix-run/react'
 import { loader as appLoader } from '~/routes/api/apps/index'
 import { Text } from '@kubelt/design-system/src/atoms/text/Text'
 import { Button } from '@kubelt/design-system/src/atoms/buttons/Button'
+import { Modal } from '@kubelt/design-system/src/molecules/modal/Modal'
+import { useState } from 'react'
 
 export const loader = appLoader
+
+const RevocationModal = ({
+  isOpen,
+  setIsOpen,
+  icon,
+  title,
+}: {
+  isOpen: boolean
+  setIsOpen: (open: boolean) => void
+  icon: string
+  title: string
+}) => (
+  <Modal isOpen={isOpen} handleClose={() => setIsOpen(false)}>
+    <div
+      className={`min-w-[908px] relative transform rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:p-6 overflow-y-auto`}
+    >
+      <div className="flex flex-row space-x-6 items-center">
+        <img src={icon} className="object-cover w-16 h-16 rounded" />
+
+        <Text weight="semibold" className="text-gray-900">
+          {title}
+        </Text>
+      </div>
+
+      <div className="flex justify-end items-center space-x-3 mt-20">
+        <Button
+          btnType="secondary-alt"
+          onClick={() => setIsOpen(false)}
+          className="bg-gray-100"
+        >
+          Cancel
+        </Button>
+
+        <Button type="submit" btnType="dangerous-alt" disabled>
+          Remove All Access
+        </Button>
+      </div>
+    </div>
+  </Modal>
+)
 
 export default () => {
   const { apps } = useLoaderData<{
@@ -14,11 +56,33 @@ export default () => {
     }[]
   }>()
 
+  const [selectedApp, setSelectedApp] = useState<
+    | undefined
+    | {
+        icon: string
+        title: string
+        timestamp: number
+      }
+  >()
+
+  const [revocationModalOpen, setRevocationModalOpen] = useState(false)
+
   return (
     <>
       <Text size="xl" weight="semibold" className="text-gray-800 mb-5">
         Applications
       </Text>
+
+      {selectedApp && (
+        <>
+          <RevocationModal
+            icon={selectedApp.icon}
+            title={selectedApp.title}
+            isOpen={revocationModalOpen}
+            setIsOpen={setRevocationModalOpen}
+          />
+        </>
+      )}
 
       <section className="flex flex-col space-y-4">
         {apps.map((a, i) => (
@@ -35,7 +99,14 @@ export default () => {
             </div>
 
             <div className="text-right">
-              <Button btnType="secondary-alt" className="bg-gray-100 mb-4">
+              <Button
+                btnType="secondary-alt"
+                className="bg-gray-100 mb-4"
+                onClick={() => {
+                  setSelectedApp(a)
+                  setRevocationModalOpen(true)
+                }}
+              >
                 Edit Access
               </Button>
 
