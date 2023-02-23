ac7e209053c48dd5bb7024f566034ab4bd8dfcc0
Cosmin Pârvulescu <Cosmin-Parvulescu@users.noreply.github.com>
2023-02-23 20:26:55 +0200

    chore(profile): dismiss welcome prompt (#1770)
    * chore(profile): dismiss welcome prompt
    
    * Added reductive sessionStorage CTA component
    
    * Cleanup
    
    * When ignoring hide all

--- apps/profile/app/routes/account/dashboard.tsx
+++ apps/profile/app/routes/account/dashboard.tsx
@@ -1,30 +1,30 @@
 import { Link, useFetcher, useOutletContext } from '@remix-run/react'
 
 import { Text } from '@kubelt/design-system/src/atoms/text/Text'
+
 import Heading from '~/components/typography/Heading'
 import SectionTitle from '~/components/typography/SectionTitle'
 
-import type { Profile } from '@kubelt/galaxy-client'
-import { useEffect, useState } from 'react'
-import { Button } from '@kubelt/design-system/src/atoms/buttons/Button'
+import { useEffect } from 'react'
 
 import dashboardChart from '~/assets/dashboard_chart.svg'
 import { normalizeProfileToLinks } from '~/helpers'
 import { Tooltip } from 'flowbite-react'
 import { NestedErrorPage } from '@kubelt/design-system/src/pages/nested-error/NestedErrorPage'
 import { Spinner } from '@kubelt/design-system/src/atoms/spinner/Spinner'
+import { FullProfile } from '~/types'
+
+import CTA from '~/components/cta/cta'
 
 export default function Welcome() {
   const { profile, addressProfiles } = useOutletContext<{
-    profile: Profile
+    profile: FullProfile
     addressProfiles: any[]
   }>()
 
   const normalizedProfiles = addressProfiles
     .map((p) => ({ urn: p.urn, ...p?.profile }))
     .map(normalizeProfileToLinks)
-
-  const [showGetStarted, setShowGetStarted] = useState(true)
 
   const appFetcher = useFetcher()
 
@@ -76,66 +76,7 @@ export default function Welcome() {
         </Text>
       </div>
 
-      {showGetStarted && (
-        <div className="flex flex-col lg:flex-row p-4 border rounded-lg shadow space-y-6 lg:space-y-0">
-          <div className="flex flex-row items-center space-x-3">
-            <div className="p-2.5 bg-indigo-50 rounded-lg">
-              <svg
-                width="42"
-                height="42"
-                viewBox="0 0 42 42"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g clipPath="url(#clip0_7573_12929)">
-                  <path
-                    d="M21 19.25C24.866 19.25 28 16.116 28 12.25C28 8.38401 24.866 5.25 21 5.25C17.134 5.25 14 8.38401 14 12.25C14 16.116 17.134 19.25 21 19.25Z"
-                    stroke="#6366F1"
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M10.5 36.75V33.25C10.5 31.3935 11.2375 29.613 12.5503 28.3003C13.863 26.9875 15.6435 26.25 17.5 26.25H24.5C26.3565 26.25 28.137 26.9875 29.4497 28.3003C30.7625 29.613 31.5 31.3935 31.5 33.25V36.75"
-                    stroke="#6366F1"
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </g>
-                <defs>
-                  <clipPath id="clip0_7573_12929">
-                    <rect width="42" height="42" fill="white" />
-                  </clipPath>
-                </defs>
-              </svg>
-            </div>
-
-            <div className="flex-1 flex-col">
-              <Text size="lg" weight="semibold" className="text-gray-800">
-                Get started
-              </Text>
-              <Text size="sm" weight="normal" className="text-gray-500">
-                Fill out your profile information
-              </Text>
-            </div>
-          </div>
-
-          <div className="flex flex-row space-x-6 flex-1 justify-center lg:justify-end items-center">
-            <Text
-              size="xs"
-              weight="normal"
-              className="text-indigo-500 cursor-pointer"
-              onClick={() => setShowGetStarted(false)}
-            >
-              Ignore
-            </Text>
-            <Link to="/account/profile">
-              <Button btnType="primary">Go to Settings</Button>
-            </Link>
-          </div>
-        </div>
-      )}
+      <CTA profile={profile} />
 
       <div className="flex flex-col space-y-5 lg:flex-row lg:space-x-5 lg:space-y-0">
         <div className="flex-1 flex flex-col space-y-5 w-full">
