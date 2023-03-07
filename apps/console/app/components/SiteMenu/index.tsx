4361c4758c490466515106edfec444f0f84a9b6d
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-03-07 13:56:10 +0200

    feat(console): early access front end

--- apps/console/app/components/SiteMenu/index.tsx
+++ apps/console/app/components/SiteMenu/index.tsx
@@ -14,12 +14,21 @@ import consoleLogo from '~/images/console_logo.svg'
 import {
   HiOutlineChartSquareBar,
   HiOutlineCog,
+  HiOutlineColorSwatch,
   HiOutlineDocument,
   HiOutlineHome,
-  HiOutlineUsers,
 } from 'react-icons/hi'
+import {
+  TbScan,
+  TbMessage,
+  TbCrosshair,
+  TbReceipt2,
+  TbUsers,
+  TbWorld,
+} from 'react-icons/tb'
 import { Disclosure } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { IconType } from 'react-icons'
 
 // RollupLogo
 // -----------------------------------------------------------------------------
@@ -31,45 +40,6 @@ const RollupLogo = () => {
         src={consoleLogo}
         alt="console logo"
       />
-    </Link>
-  )
-}
-
-// MenuLink
-// -----------------------------------------------------------------------------
-// TODO can we replace with NavLink?
-
-type MenuLinkProps = {
-  // A link target.
-  target: string
-  // The current path being visited.
-  current: string
-  // The menu item name.
-  name: string
-  // The icon to display.
-  icon: React.ForwardedRef<Function>
-  // The text to display.
-  text: string
-}
-
-/**
- * A menu item.
- */
-const MenuLink = (props: MenuLinkProps) => {
-  const isSelected = props.target === props.current
-  const selectedStyle = isSelected ? {} : { opacity: 0.6 }
-  const selectedClass = isSelected ? 'bg-slate-700' : 'grayscale'
-  const icon = React.createElement(props.icon, {
-    className: 'inline-block w-6 h-6 mr-2',
-  })
-  return (
-    <Link
-      id={`menu-${props.name}`}
-      to={props.target}
-      className={`block p-4 text-l text-slate-50 ${selectedClass}`}
-      style={selectedStyle}
-    >
-      {icon} {props.text}
     </Link>
   )
 }
@@ -132,69 +102,128 @@ type AppMenuProps = {
   props: RollupMenuProps
 }
 
+const appSubmenuStruct: {
+  title: string
+  links: {
+    icon: IconType
+    title: string
+    subroute?: string
+    disabled?: boolean
+  }[]
+}[] = [
+  {
+    title: '',
+    links: [
+      {
+        title: 'Dashboard',
+        icon: HiOutlineHome,
+        subroute: '',
+      },
+    ],
+  },
+  {
+    title: 'Configuration',
+    links: [
+      {
+        title: 'OAuth',
+        icon: HiOutlineCog,
+        subroute: '/auth',
+      },
+      {
+        title: 'Blockchain',
+        icon: HiOutlineDocument,
+        disabled: true,
+      },
+      {
+        title: 'Designer',
+        icon: HiOutlineColorSwatch,
+        disabled: true,
+      },
+      {
+        title: 'Custom Domain',
+        icon: TbWorld,
+        disabled: true,
+      },
+    ],
+  },
+  {
+    title: 'CRM',
+    links: [
+      {
+        title: 'Users',
+        icon: HiOutlineChartSquareBar,
+        subroute: '/users',
+      },
+      {
+        title: 'KYC',
+        icon: TbScan,
+        disabled: true,
+      },
+      {
+        title: 'Messaging',
+        icon: TbMessage,
+        disabled: true,
+      },
+      {
+        title: 'Audience Builder',
+        icon: TbCrosshair,
+        disabled: true,
+      },
+    ],
+  },
+  {
+    title: 'Management',
+    links: [
+      {
+        title: 'Team & Contact',
+        icon: TbUsers,
+        disabled: true,
+      },
+      {
+        title: 'Billing',
+        icon: TbReceipt2,
+        disabled: true,
+      },
+    ],
+  },
+]
+
+const AppSubmenu = (appSubroute: string) =>
+  appSubmenuStruct.map((ass) => (
+    <div key={ass.title} className="mt-6">
+      <Text size="xs" weight="medium" className="uppercase text-gray-500">
+        {ass.title}
+      </Text>
+
+      <section>
+        {ass.links.map((al, i) => (
+          <NavLink
+            key={i}
+            to={`/apps/${appSubroute}${
+              al.disabled || al.subroute == undefined ? '/soon' : al.subroute
+            }`}
+            onClick={al.disabled ? (e) => e.preventDefault() : undefined}
+            className={({ isActive }) => menuItemClass(isActive, al.disabled)}
+            end
+          >
+            <al.icon className="w-6 h-6" />
+            <Text size="sm" weight="medium">
+              {al.title}
+            </Text>
+          </NavLink>
+        ))}
+      </section>
+    </div>
+  ))
+
 function AppMenu({ props }: AppMenuProps) {
   return (
     <div>
       <AppSelect apps={props.apps} selected={props.selected} />
 
       {props.selected && (
-        <section className="pt-5 px-2 md:flex md:flex-col">
-          <NavLink
-            to={`/apps/${props.selected}`}
-            end
-            className={({ isActive }) => menuItemClass(isActive)}
-          >
-            <HiOutlineHome className="w-6 h-6" />{' '}
-            <Text size="sm" weight="medium">
-              Dashboard
-            </Text>
-          </NavLink>
-
-          <NavLink
-            to={`/apps/${props.selected}/auth`}
-            end
-            className={({ isActive }) => menuItemClass(isActive)}
-          >
-            <HiOutlineCog className="w-6 h-6" />{' '}
-            <Text size="sm" weight="medium">
-              OAuth
-            </Text>
-          </NavLink>
-
-          <NavLink
-            to={`/apps/${props.selected}/soon`}
-            end
-            className={({ isActive }) => menuItemClass(isActive, true)}
-            onClick={(e) => e.preventDefault()}
-          >
-            <HiOutlineDocument className="w-6 h-6" />{' '}
-            <Text size="sm" weight="medium">
-              Smart Contracts
-            </Text>
-          </NavLink>
-
-          <NavLink
-            to={`/apps/${props.selected}/users`}
-            end
-            className={({ isActive }) => menuItemClass(isActive)}
-          >
-            <HiOutlineChartSquareBar className="w-6 h-6" />{' '}
-            <Text size="sm" weight="medium">
-              Users
-            </Text>
-          </NavLink>
-
-          <NavLink
-            to={`/apps/${props.selected}/soon`}
-            end
-            className={({ isActive }) => menuItemClass(isActive, true)}
-            onClick={(e) => e.preventDefault()}
-          >
-            <HiOutlineUsers className="w-6 h-6" />{' '}
-            <Text size="sm" weight="medium">
-              Team
-            </Text>
-          </NavLink>
+        <section className="px-2 md:flex md:flex-col">
+          {AppSubmenu(props.selected)}
         </section>
       )}
     </div>
