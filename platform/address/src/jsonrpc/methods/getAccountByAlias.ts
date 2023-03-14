068c64f691fd04403d46668e28f635f6eff588c1
emil <33105890+poolsar42@users.noreply.github.com>
2023-03-14 13:38:47 -0400

    wip

--- platform/address/src/jsonrpc/methods/getAccountByAlias.ts
+++ platform/address/src/jsonrpc/methods/getAccountByAlias.ts
@@ -28,6 +28,10 @@ export const getAccountByAliasMethod = async ({
     },
   })
 
+  if (edges.length > 1) {
+    throw new Error('Cannot resolve unique alias due to data conflict.')
+  }
+
   /**
    * When we have proper `hidden`-address functionality
    * we'll need to do it like this:
@@ -35,17 +39,6 @@ export const getAccountByAliasMethod = async ({
    *              .filter(edge => !edge.dst.qc.hidden)
    *              .map((edge) => edge.src.baseUrn)[0]
    */
-
-  if (edges.length > 1) {
-    throw new Error(
-      `Most likely ${input.provider}-provider allows users to change usernames.
-      It is possible that user you are looking for has changed their username in
-      past and another user obtained it. We have multiple entries of this username
-      for different users. In this case we cannot resolve profile by username for
-      this provider. 500 Internal server error`
-    )
-  }
-
   const account = edges.map((edge) => edge.src.baseUrn)[0]
 
   return account as AccountURN
