e0e0de6d51be9170c3d2f478408cd30d627e97fc
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-03-15 16:36:18 +0200

    fix(passport): Implement CSP (and other) policies on the app

--- apps/passport/app/entry.server.tsx
+++ apps/passport/app/entry.server.tsx
@@ -15,6 +15,10 @@ export default function handleRequest(
   // TODO: add service bindings to request context
 
   responseHeaders.set('Content-Type', 'text/html')
+  responseHeaders.set(
+    'Content-Security-Policy',
+    `default-src 'self' ws://localhost; img-src 'self' https://imagedelivery.net; script-src 'self' 'unsafe-inline' https://unpkg.com/flowbite@1.5.4/dist/flowbite.js https://www.googletagmanager.com/gtag/js; style-src 'self' 'unsafe-inline' https://fonts.cdnfonts.com; font-src https://fonts.cdnfonts.com; connect-src 'self' wss://*.walletconnect.org ws://localhost:*/socket https://*.alchemyapi.io;`
+  )
 
   return new Response('<!DOCTYPE html>' + markup, {
     status: responseStatusCode,
