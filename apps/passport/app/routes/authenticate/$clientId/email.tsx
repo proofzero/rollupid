66c6d1ea915031f1502eaa387feee8da2d9df590
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-03-24 17:11:28 +0200

    Added cancel handling

--- apps/passport/app/routes/authenticate/$clientId/email.tsx
+++ apps/passport/app/routes/authenticate/$clientId/email.tsx
@@ -88,6 +88,7 @@ export default () => {
             )
           }}
           goBack={() => setScreen('email')}
+          onCancel={() => navigate(`/authenticate/${clientId}`)}
         />
       )
       break
