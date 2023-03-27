204bb1f9c3b5e280c4d995e4079ffe710618c56f
Adrian Maurer <adrianmaurer+github@fastmail.com>
2023-03-27 07:51:22 -0400

    wip

--- platform/edges/sql/analytics/app_details.sql
+++ platform/edges/sql/analytics/app_details.sql
@@ -1,4 +1,3 @@
 SELECT n.nss, q.key, q.value FROM node as n
   LEFT JOIN node_qcomp as q ON n.urn = q.nodeUrn
   WHERE n.nss LIKE "application/%" AND q.key = "name" AND q.value NOT LIKE "%test%";
-  
