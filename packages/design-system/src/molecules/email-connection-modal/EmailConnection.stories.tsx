ce248cc8c1eaa7d1fbda7107718e30d8b6421d75
emil <33105890+poolsar42@users.noreply.github.com>
2023-03-29 10:23:17 -0400

    wip

--- packages/design-system/src/molecules/email-connection-modal/EmailConnection.stories.tsx
+++ packages/design-system/src/molecules/email-connection-modal/EmailConnection.stories.tsx
@@ -4,11 +4,19 @@ import { EmailConnection } from './EmailConnection'
 export default {
   title: 'Molecules/EmailConnection',
   component: EmailConnection,
+  argTypes: {
+    microsoft: true,
+    google: true,
+  },
 }
 
 const Template = (args) => {
   console.log(args)
-  return <EmailConnection />
+  return (
+    <div className="w-[409px] h-[491px] border rounded-lg p-8">
+      <EmailConnection microsoft={args.microsoft} google={args.google} />
+    </div>
+  )
 }
 
 export const Default = Template.bind({})
--- packages/design-system/src/molecules/email-connection-modal/EmailConnection.tsx
+++ packages/design-system/src/molecules/email-connection-modal/EmailConnection.tsx
@@ -7,44 +7,73 @@ import { MdOutlineEmail } from 'react-icons/md'
 import { Text } from '../../atoms/text/Text'
 import { Button } from '../../build'
 
-export const EmailConnection = () => {
+export type EmailConnectionProps = {
+  microsoft?: boolean
+  google?: boolean
+}
+
+export const EmailConnection = ({
+  microsoft = true,
+  google = true,
+}: EmailConnectionProps) => {
   return (
-    <div className="flex flex-col items-center justify-center h-[402px]">
+    <div
+      className="flex flex-col items-center justify-center
+    w-full h-full"
+    >
       <Text size="xl" weight="medium" className="pb-4">
         Connect New Email
       </Text>
-      <Button btnType="secondary-alt" className="border rounded-lg p-2">
+      <Button
+        btnType="secondary-alt"
+        className="border rounded-lg p-2
+      w-full"
+      >
         <div
           className="flex flex-row p-2
-        items-center md:w-[328px] space-x-4"
+        items-center w-full space-x-4"
         >
-          <MdOutlineEmail size={20} />
+          <MdOutlineEmail size={22} />
           <Text size="lg">Connect with Email</Text>
         </div>
       </Button>
-      <div className="my-1 w-[328px] flex flex-row items-center space-x-3">
-        <hr className="flex-1 h-px  bg-gray-500" />
-        <Text>or</Text>
-        <hr className="flex-1 h-px  bg-gray-500" />
-      </div>
-      <div className="flex flex-row w-[328px] items-center justify-center space-x-4">
-        <Button
-          btnType="secondary-alt"
-          className="flex justify-center border rounded-lg w-[164px]"
-        >
-          <img src={googleIcon} alt="google" className="py-2" />
-        </Button>
-        <Button
-          btnType="secondary-alt"
-          className="flex justify-center border rounded-lg w-[164px]"
-        >
-          <img src={microsoftIcon} alt="microsoft" className="py-2" />
-        </Button>
-      </div>
-      <Button btnType="secondary-alt" className="border rounded-lg p-2 mt-auto">
+      {microsoft || google ? (
+        <div className="w-full">
+          <div className="my-1 flex flex-row items-center space-x-3 my-1.5">
+            <hr className="flex-1 h-px bg-gray-500" />
+            <Text>or</Text>
+            <hr className="flex-1 h-px bg-gray-500" />
+          </div>
+          <div
+            className="flex flex-row items-center
+      justify-center space-x-4 mb-4"
+          >
+            {google ? (
+              <Button
+                btnType="secondary-alt"
+                className="flex justify-center border rounded-lg w-full"
+              >
+                <img src={googleIcon} alt="google" className="py-2" />
+              </Button>
+            ) : null}
+            {microsoft ? (
+              <Button
+                btnType="secondary-alt"
+                className="flex justify-center border rounded-lg w-full"
+              >
+                <img src={microsoftIcon} alt="microsoft" className="py-2" />
+              </Button>
+            ) : null}
+          </div>
+        </div>
+      ) : null}
+      <Button
+        btnType="secondary-alt"
+        className="border w-full rounded-lg p-2 mt-auto"
+      >
         <div
           className="flex flex-row p-2
-        items-center min-w-full md:w-[328px] space-x-4
+        items-center space-x-4
         justify-center"
         >
           <Text size="lg">Cancel</Text>
