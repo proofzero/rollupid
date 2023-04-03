f2151ae354a5154074c095fad136e2e360e18f08
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-04-03 12:08:34 +0300

    Added rename possibility

--- apps/passport/app/routes/settings/accounts/disconnect.tsx
+++ apps/passport/app/routes/settings/accounts/disconnect.tsx
@@ -1,10 +1,8 @@
 import { AddressURN } from '@proofzero/urns/address'
 import type { ActionFunction } from '@remix-run/cloudflare'
 import { getAddressClient } from '~/platform.server'
-import { getJWTConditionallyFromSession } from '~/session.server'
 
 export const action: ActionFunction = async ({ request, context }) => {
-  const jwt = getJWTConditionallyFromSession(request, context.env)
   const formData = await request.formData()
 
   const id = formData.get('id') as AddressURN
--- apps/passport/app/routes/settings/accounts/rename.tsx
+++ apps/passport/app/routes/settings/accounts/rename.tsx
@@ -1,27 +1,21 @@
-// import { generateTraceContextHeaders } from '@proofzero/platform-middleware/trace'
-// import { getAuthzHeaderConditionallyFromToken } from '@proofzero/utils'
-// import type { ActionFunction } from '@remix-run/cloudflare'
-// import { getGalaxyClient } from '~/helpers/clients'
-// import { requireJWT } from '~/utils/session.server'
+import type { ActionFunction } from '@remix-run/cloudflare'
+import { getAddressClient } from '~/platform.server'
 
-// export const action: ActionFunction = async ({ request, context }) => {
-//   const jwt = await requireJWT(request)
+export const action: ActionFunction = async ({ request, context }) => {
+  const formData = await request.formData()
 
-//   const formData = await request.formData()
+  const id = formData.get('id') as string
+  const name = formData.get('name') as string
 
-//   const id = formData.get('id') as string
-//   const name = formData.get('name') as string
+  const addressClient = getAddressClient(id, context.env, context.traceSpan)
 
-//   const galaxyClient = await getGalaxyClient({
-//     ...generateTraceContextHeaders(context.traceSpan),
-//   })
-//   await galaxyClient.updateAddressNickname(
-//     {
-//       addressURN: id,
-//       nickname: name,
-//     },
-//     getAuthzHeaderConditionallyFromToken(jwt)
-//   )
+  try {
+    await addressClient.setNickname.query({
+      nickname: name,
+    })
+  } catch (ex) {
+    console.error(ex)
+  }
 
-//   return null
-// }
+  return null
+}
