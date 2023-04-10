fd1172cae0af9f4360368c8a2561a94b2aebb90c
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-04-10 10:41:53 +0300

    Added separate account login screen

--- apps/passport/app/components/connect-button/AuthButton.tsx
+++ apps/passport/app/components/connect-button/AuthButton.tsx
@@ -2,7 +2,7 @@ import { Button } from '@proofzero/design-system/src/atoms/buttons/Button'
 import { Text } from '@proofzero/design-system/src/atoms/text/Text'
 
 type AuthButtonProps = {
-  Graphic: JSX.Element
+  Graphic?: JSX.Element
   Addon?: JSX.Element
   text: string
   disabled?: boolean
@@ -23,9 +23,11 @@ export default ({
     onClick={onClick}
   >
     <div className="flex flex-row items-center space-x-3 py-1.5">
-      <div className="w-6 h-6 flex justify-center items-center overflow-hidden">
-        {Graphic}
-      </div>
+      {Graphic && (
+        <div className="w-6 h-6 flex justify-center items-center overflow-hidden">
+          {Graphic}
+        </div>
+      )}
 
       <Text weight="medium" className="text-gray-800">
         {text}
