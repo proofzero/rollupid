2405a39f1da7050d86c5dd92c805fb09e60b7442
Alex Flanagan <alex@kubelt.com>
2023-04-11 14:58:03 -0400

    fix(passport): Add Keycloak CSR Rewrite Rules.

--- apps/passport/remix.config.js
+++ apps/passport/remix.config.js
@@ -4,8 +4,27 @@ module.exports = {
   server: './server.ts',
   devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ['**/.*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+
+  // Keycloak rewrites. Needed here for CSR, also done in Cloudflare for SSR.
+  routes: async (defineRoutes) => {
+    return defineRoutes((route) => {
+      route('/protocol/openid-connect/auth', 'routes/authorize/index.tsx', {
+        id: 'keycloakAuthIndex',
+        caseSensitive: true,
+        index: true
+      })
+      
+      route('/protocol/openid-connect/token', 'routes/token.tsx', {
+        id: 'keycloakToken',
+        caseSensitive: true,
+        index: true
+      })
+      
+      route('/protocol/openid-connect/userinfo', 'routes/userinfo.tsx', {
+        id: 'keycloakUserInfo',
+        caseSensitive: true,
+        index: true
+      })
+    })
+  }
 }
