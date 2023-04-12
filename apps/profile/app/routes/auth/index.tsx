bac1177176a91e9d2028a29489a07243c2864c65
Adrian Maurer <adrianmaurer+github@fastmail.com>
2023-04-12 08:07:19 -0400

    fix(profile): add back auth routes

---
+++ apps/profile/app/routes/auth/index.tsx
@@ -0,0 +1,12 @@
+import type { LoaderFunction } from '@remix-run/cloudflare'
+import { getRollupAuthenticator } from '~/utils/session.server'
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const authenticator = getRollupAuthenticator()
+  await authenticator.isAuthenticated(request, {
+    successRedirect: '/account',
+    // failureRedirect: 'https://rollup.id/profiles',
+  })
+
+  return authenticator.authenticate('rollup', request)
+}
