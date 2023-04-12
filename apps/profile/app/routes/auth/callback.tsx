5bbaa881a60544d7161cad0eaa0a673572029947
Adrian <695698+maurerbot@users.noreply.github.com>
2023-04-12 08:24:17 -0400

    fix(profile): add back auth routes (#2073)

---
+++ apps/profile/app/routes/auth/callback.tsx
@@ -0,0 +1,9 @@
+import type { LoaderFunction } from '@remix-run/cloudflare'
+import { getRollupAuthenticator } from '~/utils/session.server'
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const authenticator = getRollupAuthenticator()
+  await authenticator.authenticate('rollup', request, {
+    successRedirect: '/account',
+  })
+}
