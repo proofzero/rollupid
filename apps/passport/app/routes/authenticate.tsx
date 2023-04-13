4ef97de87c59f2ecdb96d57950536b550fc7a517
emil <33105890+poolsar42@users.noreply.github.com>
2023-04-13 08:48:28 -0400

    wip: background image alt

--- apps/passport/app/routes/authenticate.tsx
+++ apps/passport/app/routes/authenticate.tsx
@@ -35,7 +35,7 @@ export default function Index() {
           'basis-2/5 h-screen w-full hidden lg:flex justify-center items-center bg-indigo-50 overflow-hidden'
         }
       >
-        <img src={sideGraphics} alt="Not Found" />
+        <img src={sideGraphics} alt="Error loading background" />
       </div>
       <div className={'basis-full basis-full lg:basis-3/5'}>
         <Suspense fallback={''}>
