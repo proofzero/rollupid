d9beeade8dd0be930dcc97fd592470fe0b6b9c38
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-04-18 13:34:07 +0300

    Added listbox button

--- packages/design-system/src/atoms/accounts/ConnectedAccountSelect.tsx
+++ packages/design-system/src/atoms/accounts/ConnectedAccountSelect.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { Listbox } from '@headlessui/react'
 import { AccountURN } from '@proofzero/urns/account'
+import { Text } from '@proofzero/design-system/src/atoms/text/Text'
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/20/solid'
 
 type ConnectedAccountSelectListItem = {
   accountURN: AccountURN
@@ -16,9 +18,8 @@ type ConnectedAccountSelectProps = {
 export const ConnectedAccountSelect = ({
   accounts,
 }: ConnectedAccountSelectProps) => {
-  const [selectedAccounts, setSelectedAccounts] = useState<
-    Array<ConnectedAccountSelectListItem>
-  >([accounts[0], accounts[1]])
+  const [selectedAccounts, setSelectedAccounts] =
+    useState<Array<ConnectedAccountSelectListItem>>()
 
   return (
     <Listbox
@@ -27,16 +28,42 @@ export const ConnectedAccountSelect = ({
       multiple
       by="accountURN"
     >
-      <Listbox.Button>
-        {selectedAccounts.map((account) => account.title).join(', ')}
-      </Listbox.Button>
-      <Listbox.Options>
-        {accounts.map((account) => (
-          <Listbox.Option key={account.accountURN} value={account}>
-            {account.title}
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
+      {({ open }) => (
+        <>
+          <Listbox.Button className="border shadow-sm rounded-lg w-full transition-transform flex flex-row justify-between items-center py-2 px-3 hover:ring-1 hover:ring-indigo-500 focus:ring-1 focus:ring-indigo-500 bg-white">
+            {(!selectedAccounts || selectedAccounts.length === 0) && (
+              <Text size="sm" className="text-gray-800">
+                No connected account(s)
+              </Text>
+            )}
+
+            {selectedAccounts?.length === 1 && (
+              <Text size="sm" className="text-gray-800">
+                {selectedAccounts[0].address}
+              </Text>
+            )}
+            {selectedAccounts?.length > 1 && (
+              <Text size="sm" className="text-gray-800">
+                {selectedAccounts.length} accounts selected
+              </Text>
+            )}
+
+            {open ? (
+              <ChevronUpIcon className="w-5 h-5" />
+            ) : (
+              <ChevronDownIcon className="w-5 h-5" />
+            )}
+          </Listbox.Button>
+
+          <Listbox.Options>
+            {accounts.map((account) => (
+              <Listbox.Option key={account.accountURN} value={account}>
+                {account.title}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </>
+      )}
     </Listbox>
   )
 }
