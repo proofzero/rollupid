d4639a3fd72712b70cfe842f2dff89f0cd213862
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-04-18 13:56:05 +0300

    Added list items

--- packages/design-system/src/atoms/accounts/ConnectedAccountSelect.tsx
+++ packages/design-system/src/atoms/accounts/ConnectedAccountSelect.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Listbox } from '@headlessui/react'
+import React, { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
 import { AccountURN } from '@proofzero/urns/account'
 import { Text } from '@proofzero/design-system/src/atoms/text/Text'
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/20/solid'
@@ -18,8 +18,9 @@ type ConnectedAccountSelectProps = {
 export const ConnectedAccountSelect = ({
   accounts,
 }: ConnectedAccountSelectProps) => {
-  const [selectedAccounts, setSelectedAccounts] =
-    useState<Array<ConnectedAccountSelectListItem>>()
+  const [selectedAccounts, setSelectedAccounts] = useState<
+    Array<ConnectedAccountSelectListItem>
+  >([])
 
   return (
     <Listbox
@@ -29,7 +30,7 @@ export const ConnectedAccountSelect = ({
       by="accountURN"
     >
       {({ open }) => (
-        <>
+        <div className="relative">
           <Listbox.Button className="border shadow-sm rounded-lg w-full transition-transform flex flex-row justify-between items-center py-2 px-3 hover:ring-1 hover:ring-indigo-500 focus:ring-1 focus:ring-indigo-500 bg-white">
             {(!selectedAccounts || selectedAccounts.length === 0) && (
               <Text size="sm" className="text-gray-800">
@@ -55,14 +56,40 @@ export const ConnectedAccountSelect = ({
             )}
           </Listbox.Button>
 
-          <Listbox.Options>
-            {accounts.map((account) => (
-              <Listbox.Option key={account.accountURN} value={account}>
-                {account.title}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="border shadow-lg rounded-lg absolute w-full mt-1 bg-white px-4 py-3 space-y-3">
+              {accounts?.map((account) => (
+                <Listbox.Option
+                  key={account.accountURN}
+                  value={account}
+                  className="flex flex-row space-x-2 cursor-pointer"
+                >
+                  <div>
+                    <input
+                      readOnly
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 bg-gray-50 text-indigo-500 focus:ring-indigo-500"
+                      checked={selectedAccounts?.includes(account)}
+                    />
+                  </div>
+                  <div className="flex-1 flex flex-col">
+                    <Text size="sm" weight="medium" className="text-gray-900">
+                      {account.title}
+                    </Text>
+                    <Text size="xs" weight="normal" className="text-gray-500">
+                      {account.provider} - {account.address}
+                    </Text>
+                  </div>
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
       )}
     </Listbox>
   )
