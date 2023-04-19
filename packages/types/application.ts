aca3c1d8cb1eb72d3f14dbb151c3eaa83fe50603
emil <33105890+poolsar42@users.noreply.github.com>
2023-04-19 18:23:07 -0400

    feat(passport): SC wallet scope

--- packages/types/application.ts
+++ packages/types/application.ts
@@ -4,7 +4,8 @@ import { z } from 'zod'
 export const ClaimName = z.union([
   z.literal('email'),
   z.literal('openid'),
-  z.literal('connected_accounts'),
+  z.literal('connected_addresses'),
+  z.literal('smart_contract_wallet'),
 ])
 export type ClaimName = z.infer<typeof ClaimName>
 
