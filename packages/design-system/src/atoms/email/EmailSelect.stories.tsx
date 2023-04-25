9c295b177004184533545ec3cb6f47f393cf6c34
Cosmin Pârvulescu <Cosmin-Parvulescu@users.noreply.github.com>
2023-04-25 15:36:30 +0300

    fix(passport): If name of email is the same for two types of email addresses they both look selected (#2170)

--- packages/design-system/src/atoms/email/EmailSelect.stories.tsx
+++ packages/design-system/src/atoms/email/EmailSelect.stories.tsx
@@ -2,24 +2,33 @@ import React from 'react'
 import { EmailSelect } from './EmailSelect'
 
 import { OAuthAddressType, EmailAddressType } from '@proofzero/types/address'
+import { EmailSelectListItem } from '@proofzero/utils/getNormalisedConnectedEmails'
 
 export default {
   title: 'Atoms/Email/Select',
   component: EmailSelect,
 }
 
-const listItems = [
+const listItems: EmailSelectListItem[] = [
   {
     type: OAuthAddressType.Google,
     email: 'email@gmail.com',
+    addressURN: 'urn:rollupid:address/1',
   },
   {
     type: OAuthAddressType.Microsoft,
     email: 'email@microsoft.com',
+    addressURN: 'urn:rollupid:address/2',
   },
   {
     type: EmailAddressType.Email,
     email: 'email@yahoo.com',
+    addressURN: 'urn:rollupid:address/3',
+  },
+  {
+    type: EmailAddressType.Email,
+    email: 'email@gmail.com',
+    addressURN: 'urn:rollupid:address/4',
   },
 ]
 
