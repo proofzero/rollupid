d464003626ef319e01df5a294da207dc1bb5f39f
Adrian <695698+maurerbot@users.noreply.github.com>
2023-04-25 09:39:33 -0400

    chore(profile): connected accounts scope (#2174)

--- apps/profile/app/utils/session.server.tsx
+++ apps/profile/app/utils/session.server.tsx
@@ -52,7 +52,7 @@ export class RollupAuthStrategy<User> extends OAuth2Strategy<
 > {
   protected authorizationParams(params: URLSearchParams): URLSearchParams {
     const result = new URLSearchParams()
-    result.append('scope', 'openid profile')
+    result.append('scope', 'openid profile connected_accounts')
     return result
   }
 }
