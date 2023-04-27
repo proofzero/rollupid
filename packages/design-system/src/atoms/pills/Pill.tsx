2aac903a36609cdece04eb52c2b9b3c5c4e8e042
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-04-27 14:13:57 +0300

    Added user button pill

--- packages/design-system/src/atoms/pills/Pill.tsx
+++ packages/design-system/src/atoms/pills/Pill.tsx
@@ -6,7 +6,7 @@ export type PillProps = React.DetailedHTMLProps<
 >
 
 export const Pill = ({ className, children }: PillProps) => (
-  <div className={`inline-block rounded py-0.5 pl-2 pr-2.5 ${className}`}>
+  <div className={`w-fit inline-block rounded py-0.5 pl-2 pr-2.5 ${className}`}>
     {children}
   </div>
 )
