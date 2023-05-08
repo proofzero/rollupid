e79c5e5db83358c4a9bb223edb9955c2fec051fd
emil <33105890+poolsar42@users.noreply.github.com>
2023-05-08 17:23:00 -0400

    wip

--- platform/email/emailOtpTemplate.ts
+++ platform/email/emailOtpTemplate.ts
@@ -3,199 +3,204 @@ import { EmailContent } from './src/types'
 export const EmailTemplate = (passcode: string): EmailContent => {
   return {
     contentType: 'text/html',
-    subject: `Email verification - one-time passcode`,
+    subject: `Your Rollup ID one-time passcode`,
     body: `
     <!DOCTYPE html>
-<html>
-  <head>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@200;500&display=swap"
-      rel="stylesheet"
-    />
-    <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-    <style type="text/css">
-      body {
-        font-family: Inter;
-        background-color: #ffffff;
-      }
-      .container {
-        display: block;
-        width: 100%;
-        text-align: center;
-        background-color: #ffffff;
-      }
-      .content {
-        display: inline-block;
-        vertical-align: top;
-        text-align: left;
-        width: 375px;
-        border-radius: 8px;
-        padding: 40px 24px;
-        margin: 0 auto;
-        background-color: #ffffff;
-      }
-      .logo {
-        width: 170px;
-        margin-bottom: 37px;
-      }
-      .heading {
-        font-size: 36px;
-        font-weight: bold;
-        line-height: 44px;
-        margin-bottom: 16px;
-      }
-      p {
-        font-size: 16px;
-        font-weight: normal;
-        line-height: 24px;
-        color: #6b7280;
-        margin-bottom: 16px;
-      }
-      #passcode {
-        background-color: #f3f4f6;
-        width: 100%;
-        text-align: center;
-        font-size: 46px;
-        font-weight: bold;
-        border-radius: 8px;
-        margin-top: 20px;
-        margin-bottom: 20px;
-        padding: 15px 0;
-      }
-      .divider {
-        border-bottom: 1px solid #e5e7eb;
-        width: 100%;
-        margin-bottom: 10px;
-      }
-      .footer-links {
-        font-size: 12px;
-        font-weight: bold;
-        color: #6b7280;
-        text-decoration: none;
-        border-bottom: 1px solid #6b7280;
-        margin-right: 10px;
-      }
-      .vl {
-        border: 0.5px solid black;
-        display: inline;
-        margin-right: 15px;
-      }
-      .powered-by {
-        font-size: 12px;
-
-        color: #9ca3af;
-        text-decoration: none;
-      }
-      @media (prefers-color-scheme: dark) {
-        .content.apaptive {
-          background-color: #ffffff;
-        }
-        .container.apaptive {
-          background-color: #ffffff;
-        }
-        body.apaptive {
-          background-color: #ffffff;
-        }
-      }
-    </style>
-    <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-  </head>
-  <body class="adaptive">
-    <div class="container adaptive">
-      <div class="content adaptive">
-        <img
-          class="logo"
-          src="https://imagedelivery.net/VqQy1abBMHYDZwVsTbsSMw/70676dfd-2899-4556-81ef-e5f48f5eb900/public"
-          alt=""
-          style="display: inline-block"
+    <html>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@200;500&display=swap"
+          rel="stylesheet"
         />
-        <div class="heading">Confirm Your Email Address</div>
-        <p>Please copy the code below into the email verification screen.</p>
-        <div id="passcode">${passcode}</div>
-        <p>Please note: the code will be valid for the next 10 minutes.</p>
-        <p>
-          If you didn&apos;t request this email, there&apos;s nothing to worry
-          about - you can safely ignore it.
-        </p>
-        <div class="divider"></div>
-        <div style="text-align: center; width: 100%">
-          <a
-            class="footer-links"
-            href="https://rollup.id/tos"
-            target="_blank"
-            rel="noreferrer"
-            >Terms & Conditions</a
-          >
-          <div class="vl"></div>
-          <a
-            class="footer-links"
-            href="https://rollup.id/privacy-policy"
-            target="_blank"
-            rel="noreferrer"
-            >Privacy Policy</a
-          >
-          <div class="vl"></div>
-          <a
-            class="footer-links"
-            href="https://discord.com/invite/rollupid"
-            target="_blank"
-            rel="noreferrer"
-            >Contact Us</a
-          >
-        </div>
-
-        <p
-          style="
-            font-size: 12px;
-            line-height: 16px;
+        <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+        <style type="text/css">
+          body {
+            font-family: Inter;
+            background-color: #ffffff;
+          }
+          .container {
+            display: block;
+            width: 98%;
+            text-align: center;
+            background-color: #ffffff;
+          }
+          .content {
+            display: inline-block;
+            vertical-align: top;
+            text-align: left;
+            max-width: 375px;
+            border-radius: 8px;
+            background-color: #ffffff;
+          }
+          .logo {
+            width: 170px;
+            margin-bottom: 37px;
+          }
+          .heading {
+            font-size: 36px;
+            font-weight: bold;
+            line-height: 44px;
+            margin-bottom: 16px;
+          }
+          .heading-logo {
+            font-size: 36px;
+            font-weight: bold;
+            line-height: 44px;
+          }
+          p {
+            font-size: 16px;
+            font-weight: normal;
+            line-height: 24px;
             color: #6b7280;
-            margin-bottom: 4px;
-            margin-top: 10px;
-          "
-        >
-          777 Bay Street, Suite C208B Toronto, Ontario M5G 2C8 Canada
-        </p>
-        <p
-          style="
-            font-size: 12px;
-            line-height: 20px;
-            color: #6b7280;
+            margin-bottom: 16px;
+          }
+          #passcode {
+            background-color: #f3f4f6;
+            width: 100%;
+            text-align: center;
+            font-size: 46px;
+            font-weight: bold;
+            border-radius: 8px;
+            margin-top: 20px;
+            margin-bottom: 20px;
+            padding: 15px 0;
+          }
+          .divider {
+            border-bottom: 1px solid #e5e7eb;
+            width: 100%;
             margin-bottom: 10px;
-          "
-        >
-          All rights reserved.
-        </p>
-        <div style="display: inline-block; text-align: center">
-          <img
-            src="https://imagedelivery.net/VqQy1abBMHYDZwVsTbsSMw/71edc886-0478-4584-beca-a6715937f000/public"
-            alt=""
-            style="max-width: 11px; display: inline-block"
-          />
-          <p
-            style="
-              font-size: 12px;
-              line-height: 12px;
-              color: #9ca3af;
-              display: inline-block;
-              margin-bottom: 10px;
-            "
-          >
-            Powered by
-            <a
-              href="https://rollup.id"
-              target="_blank"
-              rel="noreferrer"
-              class="powered-by"
-              >rollup.id</a
+          }
+          .footer-links {
+            font-size: 12px;
+            color: #6b7280;
+            text-decoration: none;
+            border-bottom: 1px solid #6b7280;
+            margin-right: 10px;
+            width: auto;
+          }
+          .vl {
+            border: 0.5px solid black;
+            display: inline;
+            margin-right: 15px;
+          }
+          .powered-by {
+            font-size: 12px;
+
+            color: #9ca3af;
+            text-decoration: none;
+          }
+          @media (prefers-color-scheme: dark) {
+            .content.apaptive {
+              background-color: #ffffff;
+            }
+            .container.apaptive {
+              background-color: #ffffff;
+            }
+            body.apaptive {
+              background-color: #ffffff;
+            }
+          }
+        </style>
+        <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+      </head>
+      <body class="adaptive">
+        <div class="container adaptive">
+          <div class="content adaptive">
+            <div class="heading-logo">
+              <img
+                class="logo"
+                src="https://imagedelivery.net/VqQy1abBMHYDZwVsTbsSMw/70676dfd-2899-4556-81ef-e5f48f5eb900/public"
+                alt=""
+                style="display: block"
+              />
+            </div>
+            <div class="heading">Confirm Your Email Address</div>
+            <p>Please copy the code below into the email verification screen.</p>
+            <div id="passcode">${passcode}</div>
+            <p>Please note: the code will be valid for the next 10 minutes.</p>
+            <p>
+              If you didn&apos;t request this email, there&apos;s nothing to worry
+              about - you can safely ignore it.
+            </p>
+            <div class="divider"></div>
+            <div style="width: 100%">
+              <a
+                class="footer-links"
+                href="https://rollup.id/tos"
+                target="_blank"
+                rel="noreferrer"
+                >Terms & Conditions</a
+              >
+              <div class="vl"></div>
+              <a
+                class="footer-links"
+                href="https://rollup.id/privacy-policy"
+                target="_blank"
+                rel="noreferrer"
+                >Privacy Policy</a
+              >
+              <div class="vl"></div>
+              <a
+                class="footer-links"
+                href="https://discord.com/invite/rollupid"
+                target="_blank"
+                rel="noreferrer"
+                >Contact Us</a
+              >
+            </div>
+
+            <p
+              style="
+                font-size: 12px;
+                line-height: 16px;
+                color: #6b7280;
+                margin-bottom: 4px;
+                margin-top: 10px;
+              "
             >
-          </p>
+              777 Bay Street, Suite C208B Toronto, Ontario M5G 2C8 Canada
+            </p>
+            <p
+              style="
+                font-size: 12px;
+                line-height: 20px;
+                color: #6b7280;
+                margin-bottom: 10px;
+              "
+            >
+              All rights reserved.
+            </p>
+            <div style="display: inline-block; text-align: center">
+              <img
+                src="https://imagedelivery.net/VqQy1abBMHYDZwVsTbsSMw/71edc886-0478-4584-beca-a6715937f000/public"
+                alt=""
+                style="max-width: 11px; display: inline-block"
+              />
+              <p
+                style="
+                  font-size: 12px;
+                  line-height: 12px;
+                  color: #9ca3af;
+                  display: inline-block;
+                  margin-bottom: 10px;
+                "
+              >
+                Powered by
+                <a
+                  href="https://rollup.id"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="powered-by"
+                  >rollup.id</a
+                >
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </body>
-</html>
+      </body>
+    </html>
   `,
   }
 }
