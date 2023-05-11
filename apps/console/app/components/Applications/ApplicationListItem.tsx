0dbcd3194130955faf3d55491849f1087ae5fd06
emil <33105890+poolsar42@users.noreply.github.com>
2023-05-11 11:31:44 -0400

    wip

--- apps/console/app/components/Applications/ApplicationListItem.tsx
+++ apps/console/app/components/Applications/ApplicationListItem.tsx
@@ -1,6 +1,5 @@
 import { Menu, Transition } from '@headlessui/react'
 import { Text } from '@proofzero/design-system/src/atoms/text/Text'
-import { Link } from '@remix-run/react'
 import { Fragment } from 'react'
 import { HiDotsVertical, HiOutlineCog } from 'react-icons/hi'
 import { HiOutlineTrash } from 'react-icons/hi2'
