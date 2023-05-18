cfeb3d8186ad9622b95bb6f83734901e32934475
Adrian <695698+maurerbot@users.noreply.github.com>
2023-05-18 12:53:06 -0400

    fix(galaxy): header schema (#2311)

--- platform/galaxy/src/index.ts
+++ platform/galaxy/src/index.ts
@@ -30,6 +30,7 @@ const plugins = [
         bearerAuthorization: {
           type: 'http',
           scheme: 'bearer',
+          in: 'header',
           bearerFormat: 'JWT',
           name: 'Authorization',
         },
