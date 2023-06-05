e715764597c724a447ccb1eb6bd8f6520baa8b29
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-06-05 16:33:48 +0300

    Fixed secondary alt dark text

--- packages/design-system/src/atoms/buttons/common.tsx
+++ packages/design-system/src/atoms/buttons/common.tsx
@@ -28,7 +28,7 @@ export const typeToColorsDict = {
   secondary:
     'bg-[#f3f4f6] text-[#5d4aec] shadow-sm border border-solid border-[#dfdcff] hover:bg-[#e5e7eb] focus:bg-[#f3f4f6] focus:ring-2 focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-[#1F2937] focus:ring-skin-primary',
   'secondary-alt':
-    'bg-white text-[#1f2937] shadow-sm border border-solid border-[#d1d5db] hover:bg-gray-100 dark:hover:bg-gray-600 focus:bg-white dark:focus:bg-gray-600 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-[#1F2937] focus:ring-skin-primary dark:bg-[#374151] dark:border-gray-600',
+    'bg-white text-[#1f2937] shadow-sm border border-solid border-[#d1d5db] hover:bg-gray-100 dark:hover:bg-gray-600 focus:bg-white dark:focus:bg-gray-600 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-[#1F2937] focus:ring-skin-primary dark:bg-[#374151] dark:border-gray-600 dark:text-white',
   dangerous:
     'bg-red-500 text-white shadow-sm hover:bg-red-400 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-[#1F2937] focus:ring-skin-primary',
   'dangerous-alt':
