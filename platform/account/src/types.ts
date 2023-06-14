b1392b9ee5fb1eb534611168511d99596f80b41e
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-06-14 12:52:54 +0300

    Oops

--- platform/account/src/types.ts
+++ platform/account/src/types.ts
@@ -3,7 +3,6 @@ import { AddressListSchema } from './jsonrpc/validators/addressList'
 import { ProfileSchema } from './jsonrpc/validators/profile'
 import { DeploymentMetadata } from '@proofzero/types'
 import { AddressesSchema } from './jsonrpc/validators/profile'
-import { ServicePlansSchema } from './jsonrpc/validators/service-plans'
 
 export interface Environment {
   Account: DurableObjectNamespace
@@ -16,4 +15,3 @@ export interface Environment {
 export type AddressList = z.infer<typeof AddressListSchema>
 export type Profile = z.infer<typeof ProfileSchema>
 export type Addresses = z.infer<typeof AddressesSchema>
-export type ServicePlans = z.infer<typeof ServicePlansSchema>
