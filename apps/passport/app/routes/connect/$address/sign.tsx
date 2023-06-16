e842a6e33e49a4049d69ca30e9ccbfce90e56ebb
emil <33105890+poolsar42@users.noreply.github.com>
2023-06-16 19:24:06 -0400

    wip

--- apps/passport/app/routes/connect/$address/sign.tsx
+++ apps/passport/app/routes/connect/$address/sign.tsx
@@ -40,7 +40,13 @@ export const loader: LoaderFunction = getRollupReqFunctionErrorWrapper(
 
     let signTemplate = AuthenticationScreenDefaults.defaultSignMessage
 
-    const { clientId } = await getAuthzCookieParams(request, context.env)
+    let clientId: string = ''
+    try {
+      const res = await getAuthzCookieParams(request, context.env)
+      clientId = res.clientId
+    } catch (ex) {
+      throw redirect('/')
+    }
     if (clientId !== 'console' && clientId !== 'passport') {
       const sbClient = getStarbaseClient('', context.env, context.traceSpan)
       const appProps = await sbClient.getAppPublicProps.query({
