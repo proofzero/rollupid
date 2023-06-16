b2702326b0c5101f424d1f1fe45c8cc4f9a71838
Adrian Maurer <adrianmaurer+github@fastmail.com>
2023-06-16 17:26:50 -0400

    chore(passport): chains

--- apps/passport/app/web3/lazyAuth.tsx
+++ apps/passport/app/web3/lazyAuth.tsx
@@ -1,33 +1,43 @@
 import { getDefaultConfig, ConnectKitProvider } from 'connectkit'
 import { useHydrated } from 'remix-utils'
 import { createConfig, WagmiConfig } from 'wagmi'
+import {
+  mainnet,
+  polygon,
+  optimism,
+  filecoin,
+  arbitrum,
+} from '@wagmi/core/chains'
 
 export function LazyAuth({
-    autoConnect = false,
-    children
+  autoConnect = false,
+  children,
 }: {
-    context?: any
-    autoConnect?: boolean
-    children: JSX.Element,
+  context?: any
+  autoConnect?: boolean
+  children: JSX.Element
 }) {
-    const hydrated = useHydrated()
-    if (!hydrated) return null
+  const hydrated = useHydrated()
+  if (!hydrated) return null
 
-    const config = createConfig(
-        getDefaultConfig({
-            appName: 'Rollup',
-            autoConnect,
-            alchemyId:
-                window.ENV.APIKEY_ALCHEMY_PUBLIC,
-            walletConnectProjectId:
-                window.ENV.WALLET_CONNECT_PROJECT_ID,
-        })
-    )
-    return (
-        <WagmiConfig config={config}>
-            <ConnectKitProvider>
-                {children}
-            </ConnectKitProvider>
-        </WagmiConfig>
-    )
+  const config = createConfig(
+    getDefaultConfig({
+      appName: 'Rollup',
+      autoConnect,
+      alchemyId: window.ENV.APIKEY_ALCHEMY_PUBLIC,
+      walletConnectProjectId: window.ENV.WALLET_CONNECT_PROJECT_ID,
+      chains: [mainnet, polygon, optimism, filecoin, arbitrum],
+    })
+  )
+  return (
+    <WagmiConfig config={config}>
+      <ConnectKitProvider
+        options={{
+          initialChainId: 0,
+        }}
+      >
+        {children}
+      </ConnectKitProvider>
+    </WagmiConfig>
+  )
 }
