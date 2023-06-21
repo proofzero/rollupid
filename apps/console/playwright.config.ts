7418cc65af235dbc9c5abcb9b1dc4996cb3064af
emil <33105890+poolsar42@users.noreply.github.com>
2023-06-21 12:47:21 -0400

    wip

--- apps/console/playwright.config.ts
+++ apps/console/playwright.config.ts
@@ -34,6 +34,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+  timeout: 120000,
 
   // TODO: path to the global setup files.
   // globalSetup: require.resolve('./global-setup'),
