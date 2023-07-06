4f15389ade36e1f38d7a8863e00834849b95f426
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-07-06 18:33:36 +0300

    Mermelated list & list item

--- apps/console/app/routes/__layout/groups/index.tsx
+++ apps/console/app/routes/__layout/groups/index.tsx
@@ -1,11 +1,73 @@
-import { Form, NavLink, useOutletContext } from '@remix-run/react'
+import { Form, NavLink, useNavigate, useOutletContext } from '@remix-run/react'
 import { GroupRootContextData } from '../groups'
+import { Button, Text } from '@proofzero/design-system'
+import { HiDotsVertical, HiUserGroup } from 'react-icons/hi'
+import { List } from '@proofzero/design-system/src/atoms/lists/List'
+import MultiAvatar from '@proofzero/design-system/src/molecules/avatar/MultiAvatar'
 
 export default () => {
   const { groups } = useOutletContext<GroupRootContextData>()
+  const navigate = useNavigate()
 
   return (
     <>
+      <section className="flex flex-row items-center justify-between mb-5">
+        <Text size="2xl" weight="semibold">
+          Groups
+        </Text>
+
+        <Button
+          btnType="primary-alt"
+          className="flex flex-row items-center gap-2"
+        >
+          <HiUserGroup />
+          Create Group
+        </Button>
+      </section>
+
+      <section>
+        <List
+          items={groups.map((g) => ({
+            key: g.URN,
+            val: g,
+          }))}
+          itemRenderer={(item) => (
+            <article
+              key={item.key}
+              className="flex flex-row items-center justify-between w-full hover:bg-gray-50"
+            >
+              <div>
+                <Text size="base" weight="semibold" className="text-gray-800">
+                  {item.val.name}
+                </Text>
+                <Text size="sm" weight="normal" className="text-gray-500">
+                  No applications
+                </Text>
+              </div>
+
+              <div className="flex flex-row items-center gap-3">
+                <MultiAvatar
+                  avatars={item.val.members.map((m) => m.iconURL)}
+                  cutoff={3}
+                  size={32}
+                />
+
+                <button
+                  className="p-2"
+                  onClick={(e) => {
+                    e.stopPropagation()
+                  }}
+                >
+                  <HiDotsVertical />
+                </button>
+              </div>
+            </article>
+          )}
+          onItemClick={(item) => {
+            navigate(`/groups/${item.val.URN.split('/')[1]}`)
+          }}
+        ></List>
+      </section>
       <section>
         <Form method="post" action="/groups/create">
           <input type="text" name="name" />
--- packages/design-system/src/atoms/lists/List.tsx
+++ packages/design-system/src/atoms/lists/List.tsx
@@ -5,21 +5,33 @@ type ListItem = {
   key: Key
   val: any
   disabled?: boolean
+  onClick?: (id: Key) => void
 }
 
 export type ListProps<T extends ListItem> = {
   items: T[]
   itemRenderer: (item: T) => ReactNode
+  onItemClick?: (item: T) => void
 }
 
 export const List = <T extends ListItem>({
   items,
   itemRenderer,
+  onItemClick,
 }: ListProps<T>) => {
   return (
     <section className="flex flex-col">
       {items.map((item) => (
-        <ListItem key={item.key} id={item.key} disabled={item.disabled}>
+        <ListItem
+          key={item.key}
+          id={item.key}
+          disabled={item.disabled}
+          onClick={(key) =>
+            onItemClick
+              ? onItemClick(items.find((i) => i.key === key))
+              : undefined
+          }
+        >
           {itemRenderer(item)}
         </ListItem>
       ))}
