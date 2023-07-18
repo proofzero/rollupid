d81255d70c203e37282651b79040aa0d6894c7a1
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-07-18 14:43:57 +0300

    Updated feature pill icon

--- packages/design-system/src/atoms/pills/FeaturePill.tsx
+++ packages/design-system/src/atoms/pills/FeaturePill.tsx
@@ -9,8 +9,8 @@ export type FeaturePillProps = {
 }
 
 export const FeaturePill = ({ Icon, text }: FeaturePillProps) => (
-  <Pill className="bg-gray-600 flex flex-row items-center gap-2">
-    {Icon && <Icon className="text-white" />}
+  <Pill className="bg-gray-600 flex flex-row items-center gap-1">
+    {Icon && <Icon className="w-3.5 h-3.5 text-white" />}
     <Text size="xs" weight="medium" className="text-white">
       {text}
     </Text>
