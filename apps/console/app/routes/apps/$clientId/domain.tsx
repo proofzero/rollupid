a05b13caf0e08f9c237b4d2fdbfde9c14e0b7ecf
Cosmin Pârvulescu <Cosmin-Parvulescu@users.noreply.github.com>
2023-07-20 14:02:55 +0300

    fix(console): domain upgrade button (#2526)

--- apps/console/app/routes/apps/$clientId/domain.tsx
+++ apps/console/app/routes/apps/$clientId/domain.tsx
@@ -142,6 +142,7 @@ export default () => {
   if (appDetails.appPlan === ServicePlanType.FREE) {
     return (
       <EarlyAccessPanel
+        clientID={appDetails.clientId as string}
         title="Custom Domain"
         subtitle="Configure Custom Domain"
         copy="A custom domain feature in an authentication tool allows an organization to use its own domain name instead of the tool's default domain. This provides a more seamless user experience, improves brand consistency, and enhances the security of the authentication process by reducing the risk of phishing scams."
