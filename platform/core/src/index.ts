8cda6fbf52497de44622bfa2fbec608ae3a72003
Sönmez Kartal <sonmez@rollup.id>
2023-08-16 21:30:45 +0300

    Revert "chore(all): migration"
    This reverts commit cf1e71821dc003b126f11ba43ea412baaa949e2f.

--- platform/core/src/index.ts
+++ platform/core/src/index.ts
@@ -9,8 +9,6 @@ import { createContext, type Context } from './context'
 import router from './router'
 import type { Environment } from './types'
 
-import runMigration from './migration'
-
 export { Account } from '@proofzero/platform.account'
 export { Identity, IdentityGroup } from '@proofzero/platform.identity'
 export { Authorization, ExchangeCode } from '@proofzero/platform.authorization'
@@ -18,21 +16,14 @@ export { StarbaseApplication } from '@proofzero/platform.starbase'
 
 export default {
   async fetch(request: Request, env: Environment): Promise<Response> {
-    const secret = request.headers.get('X-Rollup-Secret')
-    if (secret !== 'secret')
-      return new Response("couldn't guess the secret", { status: 401 })
-
-    const limit = Number(request.headers.get('X-Migration-Limit'))
-    if (typeof limit !== 'number')
-      return new Response('no limit', { status: 400 })
-
-    const offset = Number(request.headers.get('X-Migration-Offset'))
-    if (typeof offset !== 'number')
-      return new Response('no offset', { status: 400 })
-
-    await runMigration(limit, offset, env)
-
-    return new Response('SUCCESS')
+    return fetchRequestHandler({
+      endpoint: '/trpc',
+      req: request,
+      router,
+      onError,
+      createContext: (opts: FetchCreateContextFnOptions) =>
+        createContext(opts, env),
+    })
   },
 }
 
