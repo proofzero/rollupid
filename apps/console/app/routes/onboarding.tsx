b8a7724a5abbd246e594ad78bd7979f9ff08d014
emil <33105890+poolsar42@users.noreply.github.com>
2023-08-24 12:31:35 -0400

    feat(console): profile icon on onboarding instruction

--- apps/console/app/routes/onboarding.tsx
+++ apps/console/app/routes/onboarding.tsx
@@ -5,6 +5,7 @@ import {
   Outlet,
   type ShouldRevalidateFunction,
   useLoaderData,
+  useSubmit,
 } from '@remix-run/react'
 import { getRollupReqFunctionErrorWrapper } from '@proofzero/utils/errors'
 import { getAuthzHeaderConditionallyFromToken } from '@proofzero/utils'
@@ -16,6 +17,11 @@ import createCoreClient from '@proofzero/platform-clients/core'
 import { getEmailDropdownItems } from '@proofzero/utils/getNormalisedConnectedAccounts'
 import { type DropdownSelectListItem } from '@proofzero/design-system/src/atoms/dropdown/DropdownSelectList'
 import type { Profile } from '@proofzero/platform.identity/src/types'
+import { Avatar, Text } from '@proofzero/design-system'
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import { HiOutlineBookOpen, HiOutlineLogout } from 'react-icons/hi'
+import { TbUserCog } from 'react-icons/tb'
 
 export const loader: LoaderFunction = getRollupReqFunctionErrorWrapper(
   async ({ request, context }) => {
@@ -70,8 +76,10 @@ export default function Onboarding() {
     profile: Profile
   }>()
 
+  const submit = useSubmit()
+
   return (
-    <div>
+    <div className="relative">
       <div
         className={`flex flex-row items-center justify-center h-[100dvh] bg-white dark:bg-gray-900`}
       >
@@ -90,6 +98,88 @@ export default function Onboarding() {
           />
         </div>
       </div>
+
+      <Menu as="div" className="absolute top-7 right-7">
+        {({ close }) => {
+          return (
+            <>
+              <Menu.Button className={'rounded-full bg-gray-800'}>
+                <Avatar size="xs" src={profile.pfp?.image!} />
+              </Menu.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items
+                  className="absolute right-0 z-10 mt-2 w-56
+                        origin-top-right bg-white shadow-lg
+                        ring-1 ring-black ring-opacity-5 focus:outline-none rounded-lg"
+                >
+                  <Menu.Item>
+                    <div className="pl-3 pr-2 py-2 flex flex-col w-full">
+                      <div className="flex flex-col items-center justify-center my-4 gap-3">
+                        <Avatar size="xs" src={profile.pfp?.image!} />
+                        <Text
+                          size="base"
+                          weight="medium"
+                          className="max-w-[144px] truncate"
+                        >
+                          {profile.displayName}
+                        </Text>
+                      </div>
+                    </div>
+                  </Menu.Item>
+                  <Menu.Item
+                    as="a"
+                    href="https://docs.rollup.id"
+                    target="_blank"
+                    onClick={() => {
+                      close()
+                    }}
+                    className="p-3 hover:bg-gray-100
+        w-full text-left flex gap-3 items-center border-t text-gray-700 cursor-pointer"
+                  >
+                    <HiOutlineBookOpen className="w-5 h-5 text-gray-400" />
+                    <Text size="sm">Documentation</Text>
+                  </Menu.Item>
+                  <Menu.Item
+                    as="a"
+                    href={PASSPORT_URL}
+                    target="_blank"
+                    onClick={() => {
+                      close()
+                    }}
+                    className="p-3 hover:bg-gray-100
+              w-full text-left flex gap-3 items-center border-t text-gray-700 cursor-pointer"
+                  >
+                    <TbUserCog className="w-5 h-5 text-gray-400" />
+                    <Text size="sm">User Settings</Text>
+                  </Menu.Item>
+                  <Menu.Item
+                    as="button"
+                    className="p-3 hover:bg-gray-100 rounded-b-lg
+        w-full text-left flex gap-3 items-center border-t text-red-500 cursor-pointer"
+                    onClick={() => {
+                      close()
+                      submit(null, { method: 'post', action: '/signout/' })
+                    }}
+                  >
+                    <HiOutlineLogout size={22} className="w-5 h-5" />
+                    <Text className="truncate" size="sm" weight="medium">
+                      Sign Out
+                    </Text>
+                  </Menu.Item>
+                </Menu.Items>
+              </Transition>
+            </>
+          )
+        }}
+      </Menu>
     </div>
   )
 }
