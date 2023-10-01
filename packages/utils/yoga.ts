ee1f4f1ea951acf145b7090ea45774ef46ca227e
Sönmez Kartal <sonmez@rollup.id>
2023-10-02 00:15:30 +0300

    feat(yoga): set HTTP status code for `RollupError` instance

--- packages/utils/yoga.ts
+++ packages/utils/yoga.ts
@@ -1,7 +1,20 @@
+import { RollupError, HTTP_STATUS_CODES } from '@proofzero/errors'
+
 import { getErrorCause } from './errors'
 
-export const formatError = (error: unknown): Error | undefined => {
+export const formatError = (error: unknown) => {
   if (!(error instanceof Error)) return
   if (!('originalError' in error)) return
-  return getErrorCause(error.originalError)
+  const cause = getErrorCause(error.originalError)
+  if (cause instanceof RollupError)
+    return {
+      ...cause,
+      extensions: {
+        http: {
+          status: HTTP_STATUS_CODES[cause.code],
+        },
+      },
+    }
+
+  return cause
 }
