c0b69e4e620e92b25f864467263a35ff1cbbd051
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-10-11 12:59:49 +0300

    fix(passport): "Continue with email" submission button below fold on mobile

--- apps/passport/app/routes/authenticate/$clientId/email/verify.tsx
+++ apps/passport/app/routes/authenticate/$clientId/email/verify.tsx
@@ -168,9 +168,10 @@ export default () => {
   return (
     <div
       className={
-        'flex shrink flex-col items-center justify-center gap-4 mx-auto\
-      bg-white p-6 h-[100dvh] lg:h-[580px] lg:max-h-[100dvh] w-full\
-       lg:w-[418px] lg:border-rounded-lg dark:bg-gray-800 border border-[#D1D5DB] dark:border-gray-600'
+        'flex shrink flex-col items-center\
+        gap-4 mx-auto bg-white p-6 h-[100dvh]\
+        lg:h-[580px] lg:max-h-[100dvh] w-full lg:w-[418px]\
+        lg:rounded-lg dark:bg-gray-800 border border-[#D1D5DB] dark:border-gray-600'
       }
       style={{
         boxSizing: 'border-box',
