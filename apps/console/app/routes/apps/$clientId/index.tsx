44cc5a7c3b0645dd239d6193ee634acbb3acb0d0
Cosmin Pârvulescu <Cosmin-Parvulescu@users.noreply.github.com>
2023-10-25 16:42:51 +0300

    chore(console): App Dashboard Cleanup (#2746)
    * chore(console): App Dashboard Cleanup
    
    * Removed smart wallet panel

--- apps/console/app/routes/apps/$clientId/index.tsx
+++ apps/console/app/routes/apps/$clientId/index.tsx
@@ -227,7 +227,7 @@ export default function AppDetailIndexPage() {
       <div className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5">
         <div className="flex-1 flex flex-col space-y-5">
           <Panel
-            title="Galaxy GraphQL API Key"
+            title="Galaxy API"
             titleCompanion={
               <div>
                 <Text size="xs" weight="medium" className="text-gray-400">
@@ -340,14 +340,6 @@ export default function AppDetailIndexPage() {
                   successful.
                 </Text>
               </div>
-            </div>
-          </Panel>
-
-          <Panel title="Smart Contracts">
-            <div className="flex justify-center p-8">
-              <Button btnType="secondary-alt" btnSize="xxl" disabled>
-                Connect Smart Contract
-              </Button>
             </div>
           </Panel>
         </div>
