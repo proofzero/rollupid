7ac9fb8b77aac91b967b6412bca2e0f7ba98444c
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-11-10 19:13:41 +0200

    Add input toggle and read-only input for app data storage section

--- apps/console/app/routes/apps/$clientId/storage.ostrich.tsx
+++ apps/console/app/routes/apps/$clientId/storage.ostrich.tsx
@@ -3,6 +3,9 @@ import { appDetailsProps } from '~/types'
 import { IdentityURN } from '@proofzero/urns/identity'
 import { Text } from '@proofzero/design-system'
 import { DocumentationBadge } from '~/components/DocumentationBadge'
+import { InputToggle } from '@proofzero/design-system/src/atoms/form/InputToggle'
+import { ReadOnlyInput } from '@proofzero/design-system/src/atoms/form/ReadOnlyInput'
+import { ToastType, toast } from '@proofzero/design-system/src/atoms/toast'
 
 export default () => {
   const { appDetails, identityURN } = useOutletContext<{
@@ -21,7 +24,44 @@ export default () => {
         />
       </div>
 
-      <section className="flex-1 bg-white border rounded-lg">Hello!</section>
+      <section className="flex-1 bg-white border rounded-lg px-4 pt-3 pb-6">
+        <section className="flex flex-row justify-between items-center">
+          <div className="flex flex-row gap-2 items-center">
+            <Text size="lg" weight="semibold">
+              App Data Storage
+            </Text>
+
+            <div className="w-2 h-2 rounded-full bg-gray-300"></div>
+          </div>
+
+          <InputToggle id="toggle_storage" />
+        </section>
+
+        <section className="mt-2">
+          <Text size="sm" className="text-gray-600">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam
+            rhoncus aliquam metus. Sed elit dui, pellentesque a, faucibus vel,
+            interdum nec, diam. Etiam ligula pede, sagittis quis, interdum
+            ultricies, scelerisque eu.
+          </Text>
+        </section>
+
+        <section className="mt-4">
+          <ReadOnlyInput
+            id="url"
+            value="https://rollup.id/API/EqUEbCGHGnZXDMSUbrxhX7"
+            label="API url"
+            copyable={true}
+            onCopy={() =>
+              toast(
+                ToastType.Success,
+                { message: 'Client secret copied to clipboard!' },
+                { duration: 2000 }
+              )
+            }
+          />
+        </section>
+      </section>
     </section>
   )
 }
