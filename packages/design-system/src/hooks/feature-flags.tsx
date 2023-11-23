bd387bba3d109d12753e30580e1da051fd12928f
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2023-11-23 12:10:24 +0200

    Introduced retiredFlag concept

--- packages/design-system/src/hooks/feature-flags.tsx
+++ packages/design-system/src/hooks/feature-flags.tsx
@@ -1,27 +1,43 @@
 import { useEffect } from 'react'
 
+const retiredFeatureFlags = ['seats']
+
 // Automatically called once on component mount due to useEffect with an empty dependency array.
 export const registerFeatureFlag = () => {
   useEffect(() => {
+    let lsFeatureFlags: string | Record<string, boolean> | null =
+      localStorage?.getItem('feature_flags') ?? null
+    if (lsFeatureFlags) {
+      lsFeatureFlags = JSON.parse(lsFeatureFlags) as Record<string, boolean>
+      retiredFeatureFlags.forEach((flag) => {
+        if (lsFeatureFlags[flag]) {
+          delete lsFeatureFlags[flag]
+        }
+      })
+      if (Object.keys(lsFeatureFlags).length === 0) {
+        lsFeatureFlags = null
+        localStorage?.removeItem('feature_flags')
+      } else {
+        lsFeatureFlags = JSON.stringify(lsFeatureFlags)
+        localStorage?.setItem('feature_flags', lsFeatureFlags as string)
+      }
+    }
+
     const url = new URL(window.location.href)
     const featureFlag = url.searchParams.get('feature_flag')
-
     if (featureFlag) {
-      // Retrieve existing feature flags from localStorage or initialize to null
-      let featureFlags = localStorage?.getItem('feature_flags') ?? null
-
       // Update the feature flags object and store it back in localStorage
-      if (!featureFlags) {
-        featureFlags = JSON.stringify({
+      if (!lsFeatureFlags) {
+        lsFeatureFlags = JSON.stringify({
           [featureFlag]: true,
         })
       } else {
-        featureFlags = JSON.parse(featureFlags)
-        featureFlags[featureFlag] = true
-        featureFlags = JSON.stringify(featureFlags)
+        lsFeatureFlags = JSON.parse(lsFeatureFlags as string)
+        lsFeatureFlags[featureFlag] = true
+        lsFeatureFlags = JSON.stringify(lsFeatureFlags)
       }
 
-      localStorage?.setItem('feature_flags', featureFlags)
+      localStorage?.setItem('feature_flags', lsFeatureFlags)
 
       // Clean up the URL by removing the 'feature_flag' query parameter
       url.searchParams.delete('feature_flag')
