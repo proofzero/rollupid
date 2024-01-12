c535c257b8c1e97b02c67c852844cd876821d9e6
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2024-01-12 13:57:52 +0200

    Add styling to AppDataStorageModal

--- apps/console/app/components/AppDataStorageModal/AppDataStorageModal.tsx
+++ apps/console/app/components/AppDataStorageModal/AppDataStorageModal.tsx
@@ -5,6 +5,7 @@ import { FetcherWithComponents } from '@remix-run/react'
 import ExternalAppDataPackages from '@proofzero/utils/externalAppDataPackages'
 import { ExternalAppDataPackageType } from '@proofzero/types/billing'
 import { useEffect, useState } from 'react'
+import { HiOutlineX } from 'react-icons/hi'
 
 type AppDataStorageModalProps = {
   isOpen: boolean
@@ -44,20 +45,40 @@ const AppDataStorageModal: React.FC<AppDataStorageModalProps> = ({
         <subscriptionFetcher.Form
           method="post"
           action={`/apps/${clientID}/storage/ostrich`}
+          className="flex flex-col gap-4 p-5"
         >
           {selectedPackage && <input type="hidden" name="op" value="enable" />}
           {selectedPackage && (
             <input type="hidden" name="package" value={selectedPackage} />
           )}
-          <header>
-            <Text>App Data Storage</Text>
+          <header className="flex flex-row justify-between w-full mb-3 items-center">
+            <Text
+              size="lg"
+              weight="semibold"
+              className="text-left text-gray-800"
+            >
+              Purchase Entitlement(s)
+            </Text>
+            <button
+              className="bg-white p-2 rounded-lg text-xl cursor-pointer hover:bg-[#F3F4F6]"
+              onClick={onClose}
+            >
+              <HiOutlineX />
+            </button>
           </header>
 
-          <section>
+          <section className="flex flex-col gap-4">
             <div>
-              <Text>Choose Package</Text>
-              <div>
+              <Text size="xs" className="text-gray-500 uppercase text-left">
+                Choose Package
+              </Text>
+              <div className="text-left flex flex-row items-center gap-1.5">
                 <Button
+                  btnType={
+                    selectedPackage === ExternalAppDataPackageType.STARTER
+                      ? 'secondary'
+                      : 'secondary-alt'
+                  }
                   onClick={() => {
                     setSelectedPackage(ExternalAppDataPackageType.STARTER)
                   }}
@@ -68,6 +89,11 @@ const AppDataStorageModal: React.FC<AppDataStorageModalProps> = ({
                   }
                 </Button>
                 <Button
+                  btnType={
+                    selectedPackage === ExternalAppDataPackageType.SCALE
+                      ? 'secondary'
+                      : 'secondary-alt'
+                  }
                   onClick={() => {
                     setSelectedPackage(ExternalAppDataPackageType.SCALE)
                   }}
@@ -80,23 +106,34 @@ const AppDataStorageModal: React.FC<AppDataStorageModalProps> = ({
               </div>
             </div>
 
-            {selectedPackage && (
-              <div>
-                <div>
-                  <Text>Reads:</Text>
-                  <Text>{ExternalAppDataPackages[selectedPackage].reads}</Text>
-                </div>
-                <div>
-                  <Text>Writes:</Text>
-                  <Text>{ExternalAppDataPackages[selectedPackage].writes}</Text>
-                </div>
+            <div className="flex flex-col border rounded">
+              <div className="flex flex-row items-center justify-between px-4 py-2">
+                <Text size="sm" weight="medium" className="text-gray-800">
+                  Reads:
+                </Text>
+                <Text size="sm" weight="medium" className="text-gray-500">
+                  {ExternalAppDataPackages[selectedPackage].reads}
+                </Text>
               </div>
-            )}
+              <div className="w-full h-px bg-gray-200"></div>
+              <div className="flex flex-row items-center justify-between px-4 py-2">
+                <Text size="sm" weight="medium" className="text-gray-800">
+                  Writes:
+                </Text>
+                <Text size="sm" weight="medium" className="text-gray-500">
+                  {ExternalAppDataPackages[selectedPackage].writes}
+                </Text>
+              </div>
+            </div>
           </section>
 
-          <footer>
-            <Button onClick={onClose}>Close</Button>
-            <Button type="submit">Change Subscription</Button>
+          <footer className="flex flex-row items-center justify-end gap-2">
+            <Button btnType="secondary-alt" onClick={onClose}>
+              Close
+            </Button>
+            <Button btnType="primary-alt" type="submit">
+              Change Subscription
+            </Button>
           </footer>
         </subscriptionFetcher.Form>
       </Modal>
