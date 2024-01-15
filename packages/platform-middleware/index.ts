a4796970dca15af8558a3d6ef6656a69b3a6bd4b
Alexander Flanagan <9158102+alfl@users.noreply.github.com>
2024-01-15 15:30:12 -0500

    fiix(platform-middleware): remove Cloudflare Workers Analytics Engine.

--- packages/platform-middleware/index.ts
+++ packages/platform-middleware/index.ts
@@ -3,7 +3,6 @@ import * as jwt from './jwt'
 import * as geo from './geo'
 import * as log from './log'
 import * as scopes from './scopes'
-import * as analytics from './analytics'
 import * as trace from './trace'
 
-export { inputValidators, jwt, geo, log, scopes, analytics, trace }
+export { inputValidators, jwt, geo, log, scopes, trace }
