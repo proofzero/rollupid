e45603870060492b462c4cb5a5315d90dd542de3
Alexander Flanagan <9158102+alfl@users.noreply.github.com>
2024-01-15 15:16:23 -0500

    fix(platform-clients): remove Cloudflare Workers Analytics Engine.

--- packages/platform-clients/index.ts
+++ packages/platform-clients/index.ts
@@ -1,5 +1,4 @@
-import * as analytics from './analytics'
 import * as ensUtils from './ens-utils'
 import * as image from './image'
 
-export { analytics, ensUtils, image }
+export { ensUtils, image }
