f35dcd4f0e0849eeac580371c82046bef494a393
Cosmin Pârvulescu <cosmin.parvulescu@gmail.com>
2024-02-16 11:01:27 +0200

    Update secret names for app data storage pricing

--- apps/console/app/utils/external-app-data.ts
+++ apps/console/app/utils/external-app-data.ts
@@ -2,6 +2,10 @@ import { InternalServerError } from '@proofzero/errors'
 import { ExternalAppDataPackageType } from '@proofzero/types/billing'
 
 interface IAppDataStoragePricingEnv {
+  SECRET_STRIPE_APP_DATA_STORAGE_PRICE_IDS: string
+}
+
+type AppDataStoragePricingEnvObj = {
   SECRET_STRIPE_APP_DATA_STORAGE_STARTER_PRICE_ID: string
   SECRET_STRIPE_APP_DATA_STORAGE_SCALE_PRICE_ID: string
 }
@@ -10,11 +14,15 @@ export const packageTypeToPriceID = (
   env: IAppDataStoragePricingEnv,
   packageType: ExternalAppDataPackageType
 ) => {
+  const storagePrices = JSON.parse(
+    env.SECRET_STRIPE_APP_DATA_STORAGE_PRICE_IDS
+  ) as AppDataStoragePricingEnvObj
+
   switch (packageType) {
     case ExternalAppDataPackageType.STARTER:
-      return env.SECRET_STRIPE_APP_DATA_STORAGE_STARTER_PRICE_ID
+      return storagePrices.SECRET_STRIPE_APP_DATA_STORAGE_STARTER_PRICE_ID
     case ExternalAppDataPackageType.SCALE:
-      return env.SECRET_STRIPE_APP_DATA_STORAGE_SCALE_PRICE_ID
+      return storagePrices.SECRET_STRIPE_APP_DATA_STORAGE_SCALE_PRICE_ID
     default:
       throw new InternalServerError({
         message: 'Invalid package type',
